bf55f25e7e04b1bad317a5b37c2f38801751b0e6
Chris Ohk <utilforever@gmail.com>
2017-08-11 01:43:13 +0900

    Upload Enums.h - Enumerations for card data

---
+++ Includes/Enums.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: Enums.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Enumerations for card data.
+> Created Time: 2017/08/11
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_ENUMS_H
+#define HEARTHSTONEPP_ENUMS_H
+
+namespace Hearthstonepp
+{
+	
+}
+
+#endif
