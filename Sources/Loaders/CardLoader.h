40781a008b3a927cc2eba743a8244252fb7be1de
Chris Ohk <utilforever@gmail.com>
2017-08-13 22:02:42 +0900

    Upload CardLoader.h - Card loader that loads data from cards.json

---
+++ Sources/Loaders/CardLoader.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+> File Name: Enums.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Card loader that loads data from cards.json.
+> Created Time: 2017/08/13
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_CARD_LOADER_H
+#define HEARTHSTONEPP_CARD_LOADER_H
+
+namespace Hearthstonepp
+{
+	class CardLoader
+	{
+		void Load();
+	};
+}
+
+#endif
