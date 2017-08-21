97bb8267b9ca03028ae151864193ce1d6d76589c
revsic <revsic99@gmail.com>
2017-08-21 23:16:04 +0900

    Upload Hero.h - Hero class that stores hero data

---
+++ Sources/Hero.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+> File Name: Hearo.h
+> Project Name: Hearthstone++
+> Author: Young-Joong Kim
+> Purpose: Hero class that stores hero information.
+> Created Time: 2017/08/21
+> Copyright (c) 2017, Young-Joong Kim
+*************************************************************************/
+#ifndef HEARTHSTONEPP_HERO_H
+#define HEARTHSTONEPP_HERO_H
+
+namespace Hearthstonepp 
+{
+	class Hero
+	{
+	private:
+
+	};
+}
+
+#endif
