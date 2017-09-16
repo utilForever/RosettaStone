6ae031869817c4667baf3ff1d22cfc671e3329e6
Chris Ohk <utilforever@gmail.com>
2017-09-16 22:15:57 +0900

    Upload HeroPower.h - Spell class inherits from Card class

---
+++ Sources/Models/Entities/HeroPower.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+> File Name: HeroPower.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: HeroPower class inherits from Card class.
+> Created Time: 2017/09/16
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_HERO_POWER_H
+#define HEARTHSTONEPP_HERO_POWER_H
+
+#include <Enums/Enums.h>
+#include <Models/Card.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace Hearthstonepp
+{
+	struct HeroPower : Card
+	{
+
+	};
+}
+
+#endif
