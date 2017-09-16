3b07c2beb850ef5bdee0592dceae9539f52bb7b4
Chris Ohk <utilforever@gmail.com>
2017-09-16 22:28:10 +0900

    Upload Enchantment.h - Enchantment class inherits from Card class

---
+++ Sources/Models/Entities/Enchantment.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+> File Name: Enchantment.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Enchantment class inherits from Card class.
+> Created Time: 2017/09/16
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_ENCHANTMENT_H
+#define HEARTHSTONEPP_ENCHANTMENT_H
+
+#include <Enums/Enums.h>
+#include <Models/Card.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace Hearthstonepp
+{
+	struct Enchantment : Card
+	{
+
+	};
+}
+
+#endif
