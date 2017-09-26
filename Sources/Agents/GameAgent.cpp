b93c68bb81bd3c485fe4554b302b80163c0ec148
revsic <revsic99@gmail.com>
2017-09-26 17:39:37 +0900

    Upload GameAgent - Hearthstoneapp Game Agent

---
+++ Sources/Agents/GameAgent.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+> File Name: GameAgent.cpp
+> Project Name: Hearthstone++
+> Author: Young-Joong Kim
+> Purpose: Hearthstone Game Agent
+> Created Time: 2017/09/26
+> Copyright (c) 2017, Young-Joong Kim
+*************************************************************************/
+#include <Agents/GameAgent.h>
+
+namespace Hearthstonepp
+{
+
+}
---
+++ Sources/Agents/GameAgent.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+> File Name: GameAgent.h
+> Project Name: Hearthstone++
+> Author: Young-Joong Kim
+> Purpose: Hearthstone Game Agent
+> Created Time: 2017/09/26
+> Copyright (c) 2017, Young-Joong Kim
+*************************************************************************/
+#ifndef HEARTHSTONEPP_SPELL_H
+#define HEARTHSTONEPP_SPELL_H
+
+namespace Hearthstonepp 
+{
+	class GameAgent
+	{
+
+	};
+}
+
+#endif
