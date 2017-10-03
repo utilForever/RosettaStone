bbe4f13c6e1fca3c6646e443cab7af28b6cb2a29
revsic <revsic99@gmail.com>
2017-10-03 23:29:21 +0900

    Update GameAgent.h - Add struct System and member variables

--- Sources/Agents/GameAgent.h
+++ Sources/Agents/GameAgent.h
@@ -9,11 +9,44 @@
 #ifndef HEARTHSTONEPP_GAME_AGENT_H
 #define HEARTHSTONEPP_GAME_AGENT_H
 
+#include <Models/Card.h>
+#include <Models/Entities/Hero.h>
+#include <Models/Entities/HeroPower.h>
+#include <Models/Entities/Weapon.h>
+
+#include <vector>
+
 namespace Hearthstonepp 
 {
+	struct System
+	{
+		Hero hero;
+		HeroPower heroPower;
+		Weapon weapon;
+
+		std::vector<Card*> deck;
+		std::vector<Card*> usedSpell;
+		std::vector<Card*> usedMinion;
+	};
+
 	class GameAgent
 	{
-		
+	public:
+		void InitSystems();
+		void BeginPhase();
+		void MainPhase();
+		void FinalPhase();
+
+		Card* Draw(System *system);
+
+		void LoadDeck(std::vector<Card*>& deck1, std::vector<Card*>& deck2);
+		void DecideDeckOrder();
+		void ShuffleDeck(System *system);
+		void Mulligan(System *system);
+
+	private:
+		System *systemCurrent;
+		System *systemOpponent;
 	};
 }
 
