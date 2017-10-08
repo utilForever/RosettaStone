25c580a69aced7b790fabd8387689d986d732ce6
Chris Ohk <utilforever@gmail.com>
2017-10-09 02:15:22 +0900

    Update Console - Move simulation logic to Console

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -14,52 +14,6 @@ int main()
 {
 	Console c;
 	return c.Play();
-	//Hearthstonepp::CardLoader loader;
-	//std::vector<Hearthstonepp::Card*> cards;
-
-	//loader.Load(cards);
-
-	//Deck deck1; // temporal deck
-	//Deck deck2;
-
-	//deck1.reserve(30);
-	//deck2.reserve(30);
-
-	//deck1.assign(cards.begin(), cards.begin() + 30); 
-	//deck2.assign(cards.begin() + 30, cards.begin() + 60);
-
-	//User user1(0, new Hero(), new HeroPower(), deck1); // define new user
-	//User user2(1, new Hero(), new HeroPower(), deck2);
-
-	//GameAgent agent(&user1, &user2);
-	//GameResult result;
-
-	//std::thread *at = agent.StartAgent(result);
-
-	//for (int i = 0; i < 2; ++i)
-	//{
-	//	Card *list[3] = { 0, };
-	//	int result = agent.ReadBuffer((BYTE*)list, sizeof(Card*) * 3); // get card data
-
-	//	for (auto card : list)
-	//	{
-	//		std::cout << "[" << card->name << "] ";
-	//	}
-	//	std::cout << std::endl;
-
-	//	BYTE mulligan[] = { 0, 2 }; // index of the card to be mulligan
-	//	result = agent.WriteBuffer(mulligan, 2); // send index to agent
-
-	//	result = agent.ReadBuffer((BYTE*)list, sizeof(Card*) * 3); // get new card data
-
-	//	for (auto card : list)
-	//	{
-	//		std::cout << "[" << card->name << "] ";
-	//	}
-	//	std::cout << std::endl;
-	//}
-
-	//at->join(); // join agent thread
 
 	//return 0;
 }
