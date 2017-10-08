d834572c6aabc868f4a0e94c927f8db2939e3336
Chris Ohk <utilforever@gmail.com>
2017-10-09 02:08:22 +0900

    Upload Constants.h - Constants for Hearthstone++

---
+++ Sources/Commons/Constants.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+> File Name: Constants.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Constants for Hearthstone++.
+> Created Time: 2017/10/08
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_CONSTANTS_H
+#define HEARTHSTONEPP_CONSTANTS_H
+
+namespace Hearthstonepp
+{
+    constexpr float VERSION = 0.1f;
+	constexpr size_t MENU_SIZE = 7;
+}
+
+#endif
