38d7272025e1f7fdb737f28bcf58220181e8162f
Chris Ohk <utilforever@gmail.com>
2017-10-09 22:35:34 +0900

    Upload Deck.h - Deck class that stores a list of cards

---
+++ Sources/Models/Deck.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+> File Name: Deck.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Deck class that stores a list of cards.
+> Created Time: 2017/10/09
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_DECK_H
+#define HEARTHSTONEPP_DECK_H
+
+#include <string>
+
+struct Deck
+{
+	std::string name;
+	unsigned int numOfCards = 0;
+};
+
+#endif
