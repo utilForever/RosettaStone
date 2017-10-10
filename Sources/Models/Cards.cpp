df2676f459b6f1128c3627182e4d6bc27fc4b79d
Chris Ohk <utilforever@gmail.com>
2017-10-10 13:03:00 +0900

    Upload Cards - Cards class that stores a list of cards

---
+++ Sources/Models/Cards.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+> File Name: Cards.cpp
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Cards class that stores a list of cards.
+> Created Time: 2017/10/10
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <Models/Cards.h>
+
+namespace Hearthstonepp
+{
+    Cards* Cards::m_instance = nullptr;
+
+    Cards::Cards()
+    {
+
+    }
+
+    Cards::~Cards()
+    {
+        
+    }
+
+    Cards* Cards::GetInstance()
+    {
+        if (m_instance == nullptr)
+        {
+            m_instance = new Cards();
+        }
+
+        return m_instance;
+    }
+}
---
+++ Sources/Models/Cards.h
@@ -0,0 +1,31 @@
+﻿/*************************************************************************
+> File Name: Cards.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Cards class that stores a list of cards.
+> Created Time: 2017/10/10
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_CARDS_H
+#define HEARTHSTONEPP_CARDS_H
+
+namespace Hearthstonepp
+{
+    class Cards
+    {
+    public:
+        static Cards* GetInstance();
+
+    private:
+        Cards();
+        ~Cards();
+        Cards(const Cards&) = delete;
+        Cards(Cards&&) = delete;
+        Cards& operator=(const Cards&) = delete;
+        Cards& operator=(Cards&&) = delete;
+
+        static Cards* m_instance;
+    };
+}
+
+#endif
