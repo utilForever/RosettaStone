eb9bb0b2e40f64c9631fac0302ca596ad89730a7
Chris Ohk <utilforever@gmail.com>
2017-10-11 23:18:51 +0900

    Update Weapon - Create .cpp, implement constructor, ShowInfo method

---
+++ Sources/Models/Entities/Weapon.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+> File Name: Weapon.cpp
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Weapon class inherits from Card class.
+> Created Time: 2017/10/11
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <Models/Entities/Weapon.h>
+
+namespace Hearthstonepp
+{
+	Weapon::Weapon(
+		std::string id, Rarity rarity, Faction faction, CardSet cardSet,
+		CardClass cardClass, CardType cardType, Race race,
+		std::string name, std::string text, bool collectible,
+		int cost, int attack, int health, int durability,
+		std::vector<GameTag> mechanics,
+		std::map<PlayReq, int> playRequirements,
+		std::vector<std::string> entourages) :
+		Card(
+			id, rarity, faction, cardSet, cardClass, cardType, race,
+			name, text, collectible, cost, attack, health, durability,
+			mechanics, playRequirements, entourages)
+	{
+
+	}
+
+	void Weapon::ShowInfo() const
+	{
+
+	}
+}
