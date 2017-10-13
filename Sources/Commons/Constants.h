2293a032963390676a193fc6c8f59d48ff98663f
Chris Ohk <utilforever@gmail.com>
2017-10-13 13:13:09 +0900

    Update Constants - Add PLAYER_CLASS_SIZE

--- Sources/Commons/Constants.h
+++ Sources/Commons/Constants.h
@@ -13,6 +13,7 @@ namespace Hearthstonepp
 {
     constexpr float VERSION = 0.1f;
 	constexpr size_t MENU_SIZE = 6;
+	constexpr size_t PLAYER_CLASS_SIZE = 9;
 	constexpr unsigned int MAXIMUM_NUM_CARDS_IN_DECK = 30;
 }
 
