05a791a91f06eff8587cc9e1e06c0651ae40a69d
Chris Ohk <utilforever@gmail.com>
2017-10-18 12:48:40 +0900

    Upload Player - Player class that stores a list of decks

---
+++ Sources/Models/Player.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+> File Name: Player.cpp
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Player class that stores a list of decks.
+> Created Time: 2017/10/18
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <Models/Player.h>
+
+namespace Hearthstonepp
+{
+	Player::Player() : m_name("Anonymous")
+	{
+		// Do nothing
+	}
+
+	Player::Player(std::string&& name) : m_name(std::move(name))
+	{
+		// Do nothing
+	}
+}
---
+++ Sources/Models/Player.h
@@ -0,0 +1,28 @@
+/*************************************************************************
+> File Name: Player.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Player class that stores a list of decks.
+> Created Time: 2017/10/18
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_PLAYER_H
+#define HEARTHSTONEPP_PLAYER_H
+
+#include <Models/Deck.h>
+
+namespace Hearthstonepp
+{
+	class Player
+	{
+	public:
+		Player();
+		Player(std::string&& name);
+
+	private:
+		std::string m_name;
+		std::vector<Deck*> m_decks;
+	};
+}
+
+#endif
