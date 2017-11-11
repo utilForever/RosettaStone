58ad09ff9189db4f88c0ac1a7d35c0a2e98c349a
Chris Ohk <utilforever@gmail.com>
2017-11-11 23:05:29 +0900

    Update Console project - Fix include file paths

--- Programs/Console/main.cpp
+++ Programs/Console/main.cpp
@@ -6,7 +6,7 @@
 > Created Time: 2017/08/10
 > Copyright (c) 2017, Chan-Ho Chris Ohk
 *************************************************************************/
-#include <Programs/Console.h>
+#include "Console.h"
 
 using namespace Hearthstonepp;
 
