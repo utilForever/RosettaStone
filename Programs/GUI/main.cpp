f7715a815e4de71f494ca8cf1d5fba93cdf0d73b
Chris Ohk <utilForever@gmail.com>
2018-02-05 02:15:53 +0900

    Update GUI program - Add some code of simple GUI (#34)

--- Programs/GUI/main.cpp
+++ Programs/GUI/main.cpp
@@ -6,7 +6,39 @@
 > Created Time: 2018/02/04
 > Copyright (c) 2017, Chan-Ho Chris Ohk
 *************************************************************************/
+#include <GLFW/glfw3.h>
+#include <imgui/imgui.h>
+
+#include <cstdlib>
+
+void Setup(GLFWwindow* window);
+void Render(ImDrawData* drawData);
+
 int main()
 {
+	if (!glfwInit())
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	static GLFWwindow* window{ glfwCreateWindow(800, 600, "Hearthstone++ simple GUI", nullptr, nullptr };
+
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// Close window on pressing ESC
+	glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scanCode, int action, int mods)
+	{
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+	});
+
 	return 0;
 }
