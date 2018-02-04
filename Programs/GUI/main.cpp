2f27368609512fc5f783dff7251c4a1710f47da8
Chris Ohk <utilForever@gmail.com>
2018-02-04 20:39:16 +0900

    Upload GUI program - Main entry of Hearthstone++ GUI program (#23)

---
+++ Programs/GUI/main.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+> File Name: main.cpp
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Main entry of Hearthstone++ GUI program.
+> Created Time: 2018/02/04
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+int main()
+{
+	return 0;
+}
