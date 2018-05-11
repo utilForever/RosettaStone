0cd8f8a6ddd41b40176c6813addb94d933681ba4
SeungHyun Jeon <shtowever@gmail.com>
2018-05-11 13:10:48 +0900

    Update Travis CI setting - CodeCoverage for AppleClang

--- Builds/CMake/CodeCoverage.cmake
+++ Builds/CMake/CodeCoverage.cmake
@@ -52,7 +52,7 @@ IF(NOT CMAKE_COMPILER_IS_GNUCXX)
 	# Clang version 3.0.0 and greater now supports gcov as well.
 	MESSAGE(WARNING "Compiler is not GNU gcc! Clang Version 3.0.0 and greater supports gcov as well, but older versions don't.")
 	
-	IF(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+	IF(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" AND NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
 		MESSAGE(FATAL_ERROR "Compiler is not GNU gcc! Aborting...")
 	ENDIF()
 ENDIF() # NOT CMAKE_COMPILER_IS_GNUCXX
