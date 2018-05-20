0418e86ddf4f57651301fb38db9f6d844008b315
revsic <revsic99@gmail.com>
2018-05-20 15:38:49 +0900

    [ci skip] Upload TaskSerializer - Serializer for TaskMeta and MetaData

---
+++ Includes/Tasks/TaskSerializer.h
@@ -0,0 +1,40 @@
+/*************************************************************************
+> File Name: TaskSerializer.h
+> Project Name: Hearthstonepp
+> Author: Young-Joong Kim
+> Purpose: Serializer for TaskMeta and MetaData
+> Created Time: 2018/05/20
+> Copyright (c) 2018, Young-Joong Kim
+*************************************************************************/
+#ifndef HEARTHSTONEPP_TASKSERIALIZER_H
+#define HEARTHSTONEPP_TASKSERIALIZER_H
+
+#include <Flatbuffers/MetaData_generated.h>
+
+namespace Hearthstonepp
+{
+    namespace Serializer
+    {
+        using fb = flatbuffers;
+
+        void CreateTaskMetaVector(TaskMeta& meta, const std::vector<TaskMeta>& vector)
+        {
+            fb::FlatBufferBuilder builder(1024);
+            auto flatten = std::vector<fb::Offset<MetaData::TaskMeta>>;
+
+            for (const auto& task : vector)
+            {
+                auto trait = MetaData::TaskMetaTrait(static_cast<int>(meta.id), meta.status, meta.userID);
+                auto temporal = MetaData::CreateTaskMeta(builder, trait, task.GetBuffer());
+                flatten.emplace_back(std::move(temporal));
+            }
+
+            auto integrated = MetaData::CreateTaskMetaVector(fb, flatten);
+            builder.Finish(integrated);
+
+            meta = TaskMeta(TaskMetaTrait(TaskID::TASK_TUPLE), builder.GetSize(), builder.GetBufferPoint());
+        }
+    }
+}
+
+#endif //HEARTHSTONEPP_TASKSERIALIZER_H
---
+++ Sources/Tasks/TaskSerializer.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+> File Name: TaskSerializer.cpp
+> Project Name: Hearthstonepp
+> Author: Young-Joong Kim
+> Purpose: Serializer for TaskMeta and MetaData
+> Created Time: 2018/05/20
+> Copyright (c) 2018, Young-Joong Kim
+*************************************************************************/
+#include <Tasks/TaskSerializer.h>
+
+namespace Hearthstonepp
+{
+
+}
