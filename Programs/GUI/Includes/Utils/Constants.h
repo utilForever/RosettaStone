90f6d301425a4323e6b0042e18d5be0afa049fcc
Chris Ohk <utilForever@gmail.com>
2018-06-05 21:44:07 +0900

    [ci skip] Create header file for constants

---
+++ Programs/GUI/Includes/Utils/Constants.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: Constants.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Constants of Hearthstone++ GUI program.
+> Created Time: 2018/06/05
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_GUI_CONSTANTS_H
+#define HEARTHSTONEPP_GUI_CONSTANTS_H
+
+namespace Hearthstonepp
+{
+
+}
+
+#endif
