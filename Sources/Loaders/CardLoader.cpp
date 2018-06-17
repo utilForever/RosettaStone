ea1a26320904234be7326d8b7dc9cfa3d394e7f3
Chris Ohk <utilForever@gmail.com>
2018-06-17 23:56:04 +0900

    [ci skip] Refactor card loader code

--- Sources/Loaders/CardLoader.cpp
+++ Sources/Loaders/CardLoader.cpp
@@ -18,109 +18,165 @@
 
 namespace Hearthstonepp
 {
-	std::vector<Card*> CardLoader::Load() const
-	{
-		// Read card data from JSON file
-		std::ifstream cardFile(RESOURCES_DIR "cards.json");
-		json j;
+std::vector<Card*> CardLoader::Load() const
+{
+    // Read card data from JSON file
+    std::ifstream cardFile(RESOURCES_DIR "cards.json");
+    json j;
 
-		if (!cardFile.is_open())
-		{
-			throw std::runtime_error("Can't open cards.json");
-		}
+    if (!cardFile.is_open())
+    {
+        throw std::runtime_error("Can't open cards.json");
+    }
 
-		cardFile >> j;
+    cardFile >> j;
 
-		std::vector<Card*> cards;
-		cards.reserve(j.size());
+    std::vector<Card*> cards;
+    cards.reserve(j.size());
 
-		for (auto& card : j)
-		{
-			const std::string id = card["id"].get<std::string>();
-			const Rarity rarity = card["rarity"].is_null() ? +Rarity::FREE : Rarity::_from_string(card["rarity"].get<std::string>().c_str());
-			const Faction faction = card["faction"].is_null() ? +Faction::NEUTRAL : Faction::_from_string(card["faction"].get<std::string>().c_str());
-			const CardSet cardSet = card["set"].is_null() ? +CardSet::NONE : CardSet::_from_string(card["set"].get<std::string>().c_str());
-			const CardClass cardClass = card["cardClass"].is_null() ? +CardClass::NEUTRAL : CardClass::_from_string(card["cardClass"].get<std::string>().c_str());
-			const CardType cardType = card["type"].is_null() ? +CardType::INVALID : CardType::_from_string(card["type"].get<std::string>().c_str());
-			const Race race = card["race"].is_null() ? +Race::INVALID : Race::_from_string(card["race"].get<std::string>().c_str());
+    for (auto& cardData : j)
+    {
+        const std::string id = cardData["id"].get<std::string>();
+        const Rarity rarity =
+            cardData["rarity"].is_null()
+                ? +Rarity::FREE
+                : Rarity::_from_string(
+                      cardData["rarity"].get<std::string>().c_str());
+        const Faction faction =
+            cardData["faction"].is_null()
+                ? +Faction::NEUTRAL
+                : Faction::_from_string(
+                      cardData["faction"].get<std::string>().c_str());
+        const CardSet cardSet =
+            cardData["set"].is_null()
+                ? +CardSet::NONE
+                : CardSet::_from_string(
+                      cardData["set"].get<std::string>().c_str());
+        const CardClass cardClass =
+            cardData["cardClass"].is_null()
+                ? +CardClass::NEUTRAL
+                : CardClass::_from_string(
+                      cardData["cardClass"].get<std::string>().c_str());
+        const CardType cardType =
+            cardData["type"].is_null()
+                ? +CardType::INVALID
+                : CardType::_from_string(
+                      cardData["type"].get<std::string>().c_str());
+        const Race race =
+            cardData["race"].is_null()
+                ? +Race::INVALID
+                : Race::_from_string(
+                      cardData["race"].get<std::string>().c_str());
 
-			const std::string name = card["name"].is_null() ? "" : card["name"]["enUS"].get<std::string>();
-			const std::string text = card["text"].is_null() ? "" : card["text"]["enUS"].get<std::string>();
+        const std::string name =
+            cardData["name"].is_null()
+                ? ""
+                : cardData["name"]["enUS"].get<std::string>();
+        const std::string text =
+            cardData["text"].is_null()
+                ? ""
+                : cardData["text"]["enUS"].get<std::string>();
 
-			const bool collectible = card["collectible"].is_null() ? false : card["collectible"].get<bool>();
-			const int cost = card["cost"].is_null() ? -1 : card["cost"].get<int>();
-			const int attack = card["attack"].is_null() ? -1 : card["attack"].get<int>();
-			const int health = card["health"].is_null() ? -1 : card["health"].get<int>();
-			const int durability = card["durability"].is_null() ? -1 : card["durability"].get<int>();
+        const bool collectible = cardData["collectible"].is_null()
+                                     ? false
+                                     : cardData["collectible"].get<bool>();
+        const int cost =
+            cardData["cost"].is_null() ? -1 : cardData["cost"].get<int>();
+        const int durability = cardData["durability"].is_null()
+                                   ? -1
+                                   : cardData["durability"].get<int>();
 
-			std::vector<GameTag> mechanics;
-			for (auto& mechanic : card["mechanics"])
-			{
-				mechanics.emplace_back(GameTag::_from_string(mechanic.get<std::string>().c_str()));
-			}
+        const int attack =
+            cardData["attack"].is_null() ? -1 : cardData["attack"].get<int>();
 
-			std::map<PlayReq, int> playRequirements;
-			for (auto iter = card["playRequirements"].begin(); iter != card["playRequirements"].end(); ++iter)
-			{
-				playRequirements.try_emplace(PlayReq::_from_string(iter.key().c_str())), iter.value().get<int>();
-			}
+        std::vector<GameTag> mechanics;
+        for (auto& mechanic : cardData["mechanics"])
+        {
+            mechanics.emplace_back(
+                GameTag::_from_string(mechanic.get<std::string>().c_str()));
+        }
 
-			std::vector<std::string> entourages;
-			for (auto& entourage : card["entourage"])
-			{
-				entourages.emplace_back(entourage.get<std::string>());
-			}
+        std::map<PlayReq, int> playRequirements;
+        for (auto iter = cardData["playRequirements"].begin();
+             iter != cardData["playRequirements"].end(); ++iter)
+        {
+            playRequirements.try_emplace(
+                PlayReq::_from_string(iter.key().c_str())),
+                iter.value().get<int>();
+        }
 
-			Card* c = nullptr;
-			switch (cardType)
-			{
-			case CardType::HERO:
-				c = new Hero(
-					id, rarity, faction, cardSet, cardClass, cardType, race,
-					name, text, collectible, cost, attack, health, durability,
-					mechanics, playRequirements, entourages);
-				break;
-			case CardType::MINION:
-				c = new Minion(
-					id, rarity, faction, cardSet, cardClass, cardType, race,
-					name, text, collectible, cost, attack, health, durability,
-					mechanics, playRequirements, entourages);
-				break;
-			case CardType::SPELL:
-				c = new Spell(
-					id, rarity, faction, cardSet, cardClass, cardType, race,
-					name, text, collectible, cost, attack, health, durability,
-					mechanics, playRequirements, entourages);
-				break;
-			case CardType::ENCHANTMENT:
-				c = new Enchantment(
-					id, rarity, faction, cardSet, cardClass, cardType, race,
-					name, text, collectible, cost, attack, health, durability,
-					mechanics, playRequirements, entourages);
-				break;
-			case CardType::WEAPON:
-				c = new Weapon(
-					id, rarity, faction, cardSet, cardClass, cardType, race,
-					name, text, collectible, cost, attack, health, durability,
-					mechanics, playRequirements, entourages);
-				break;
-			case CardType::HERO_POWER:
-				c = new HeroPower(
-					id, rarity, faction, cardSet, cardClass, cardType, race,
-					name, text, collectible, cost, attack, health, durability,
-					mechanics, playRequirements, entourages);
-				break;
-			default:
-				// TODO: Log invalid card type
-				break;
-			}
+        std::vector<std::string> entourages;
+        for (auto& entourage : cardData["entourage"])
+        {
+            entourages.emplace_back(entourage.get<std::string>());
+        }
 
-			if(c != nullptr)
-				cards.emplace_back(c);
-		}
+        // Check invalid card type for 'Placeholder'
+        // See https://hearthstone.gamepedia.com/Placeholder_Card
+        if (cardType == +CardType::INVALID)
+        {
+            continue;
+        }
 
-		cardFile.close();
+        Card* card = nullptr;
+        switch (cardType)
+        {
+            case +CardType::ENCHANTMENT:
+                card = new Enchantment();
+                break;
+            case +CardType::HERO:
+                card = new Hero();
+                break;
+            case +CardType::HERO_POWER:
+                card = new HeroPower();
+                break;
+            case +CardType::MINION:
+                card = new Minion();
+                break;
+            case +CardType::SPELL:
+                card = new Spell();
+                break;
+            case +CardType::WEAPON:
+                card = new Weapon();
+                break;
+            default:
+                // TODO: Handle invalid card type
+                break;
+        }
 
-		return cards;
-	}
+        if (card == nullptr)
+        {
+            continue;
+        }
+
+        card->id = id;
+        card->rarity = rarity;
+        card->faction = faction;
+        card->cardSet = cardSet;
+        card->cardClass = cardClass;
+        card->cardType = cardType;
+        card->race = race;
+        card->name = name;
+        card->text = text;
+        card->isCollectible = collectible;
+        card->cost = cost;
+        card->mechanics = mechanics;
+        card->playRequirements = playRequirements;
+        card->entourages = entourages;
+        card->Initialize();
+
+        if (cardType == +CardType::WEAPON)
+        {
+            const auto weapon = dynamic_cast<Weapon*>(card);
+            weapon->attack = attack;
+            weapon->durability = durability;
+        }
+
+        cards.emplace_back(card);
+    }
+
+    cardFile.close();
+
+    return cards;
+}
 }
