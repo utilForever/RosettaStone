89b919200d90ccaed437cfd2f3c3dbdb75cdc9bc
Chris Ohk <utilForever@gmail.com>
2018-06-17 23:54:40 +0900

    [ci skip] Add card structure: Character

---
+++ Sources/Models/Entities/Character.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+> File Name: Character.cpp
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Character class inherits from Card class.
+> Created Time: 2018/06/17
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <Models/Entities/Character.h>
+
+namespace Hearthstonepp
+{
+void Character::ShowInfo() const
+{
+}
+}
