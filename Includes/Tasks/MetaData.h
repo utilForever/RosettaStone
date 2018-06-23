abd8597c3e1706f81415dd95460c233368fce129
revsic <revsic99@gmail.com>
2018-06-23 16:04:05 +0900

    [ci skip] Update MetaData - Update status_t to enum class

--- Includes/Tasks/MetaData.h
+++ Includes/Tasks/MetaData.h
@@ -9,59 +9,54 @@
 #ifndef HEARTHSTONEPP_METADATA_H
 #define HEARTHSTONEPP_METADATA_H
 
-#include <Tasks/TaskMeta.h>
-
-namespace Hearthstonepp::MetaData
+namespace Hearthstonepp
 {
-    enum class TaskMetaStatus : TaskMeta::status_t
-    {
-        INVALID,
+using status_t = unsigned int;
+enum class MetaData : status_t
+{
+    INVALID,
 
+    SWAP,
+    SWAP_SUCCESS,
 
-    };
-constexpr TaskMeta::status_t INVALID = 0;
+    DRAW,
+    DRAW_SUCCESS,
+    DRAW_EXHAUST,
+    DRAW_OVERDRAW,
+    DRAW_EXHAUST_OVERDRAW,
 
-constexpr TaskMeta::status_t SWAP = 1;
-constexpr TaskMeta::status_t SWAP_SUCCESS = 2;
+    SHUFFLE,
+    SHUFFLE_SUCCESS,
 
-constexpr TaskMeta::status_t DRAW = 3;
-constexpr TaskMeta::status_t DRAW_SUCCESS = 4;
-constexpr TaskMeta::status_t DRAW_EXHAUST = 5;
-constexpr TaskMeta::status_t DRAW_OVERDRAW = 6;
-constexpr TaskMeta::status_t DRAW_EXHAUST_OVERDRAW = 7;
+    BRIEF,
 
-constexpr TaskMeta::status_t SHUFFLE = 8;
-constexpr TaskMeta::status_t SHUFFLE_SUCCESS = 9;
+    MULLIGAN,
+    MULLIGAN_SUCCESS,
+    MULLIGAN_INDEX_OUT_OF_RANGE,
+    MULLIGAN_DUPLICATED_INDEX,
 
-constexpr TaskMeta::status_t BRIEF = 10;
+    MODIFY_MANA,
+    MODIFY_MANA_SUCCESS,
 
-constexpr TaskMeta::status_t MULLIGAN = 11;
-constexpr TaskMeta::status_t MULLIGAN_SUCCESS = 12;
-constexpr TaskMeta::status_t MULLIGAN_INDEX_OUT_OF_RANGE = 13;
-constexpr TaskMeta::status_t MULLIGAN_DUPLICATED_INDEX = 14;
+    SELECT_CARD_MINION,
 
-constexpr TaskMeta::status_t MODIFY_MANA = 15;
-constexpr TaskMeta::status_t MODIFY_MANA_SUCCESS = 16;
+    SUMMON,
+    SUMMON_SUCCESS,
+    SUMMON_CARD_IDX_OUT_OF_RANGE,
+    SUMMON_POSITION_OUT_OF_RANGE,
+    SUMMON_NOT_ENOUGH_MANA,
 
-constexpr TaskMeta::status_t SELECT_CARD_MINION = 17;
+    MODIFY_HEALTH,
+    MODIFY_HEALTH_SUCCESS,
 
-constexpr TaskMeta::status_t SUMMON = 18;
-constexpr TaskMeta::status_t SUMMON_SUCCESS = 19;
-constexpr TaskMeta::status_t SUMMON_CARD_IDX_OUT_OF_RANGE = 20;
-constexpr TaskMeta::status_t SUMMON_POSITION_OUT_OF_RANGE = 21;
-constexpr TaskMeta::status_t SUMMON_NOT_ENOUGH_MANA = 22;
+    COMBAT,
+    COMBAT_SUCCESS,
+    COMBAT_SRC_IDX_OUT_OF_RANGE,
+    COMBAT_DST_IDX_OUT_OF_RANGE,
+    COMBAT_ALREADY_ATTACKED,
 
-constexpr TaskMeta::status_t MODIFY_HEALTH = 23;
-constexpr TaskMeta::status_t MODIFY_HEALTH_SUCCESS = 24;
-
-constexpr TaskMeta::status_t COMBAT = 25;
-constexpr TaskMeta::status_t COMBAT_SUCCESS = 26;
-constexpr TaskMeta::status_t COMBAT_SRC_IDX_OUT_OF_RANGE = 27;
-constexpr TaskMeta::status_t COMBAT_DST_IDX_OUT_OF_RANGE = 28;
-constexpr TaskMeta::status_t COMBAT_ALREADY_ATTACKED = 29;
-
-constexpr TaskMeta::status_t GAME_END =
-    std::numeric_limits<TaskMeta::status_t>::max();
-}  // namespace Hearthstonepp::MetaData
+    GAME_END
+};
+}  // namespace Hearthstonepp
 
 #endif  // HEARTHSTONEPP_METADATA_H
