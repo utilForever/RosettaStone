3f164881e77f2c5d8cffec81d5c481d54402070d
Chris Ohk <utilForever@gmail.com>
2018-06-23 17:30:46 +0900

    [ci skip] Create 'BasicCardsGen' class

---
+++ Sources/Enchants/BasicCardsGen.cpp
@@ -0,0 +1,104 @@
+/*************************************************************************
+> File Name: BasicCardsGen.cpp
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: BasicCardsGen class that stores the power of basic cards.
+> Created Time: 2018/06/23
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <CardSets/BasicCardsGen.h>
+
+namespace Hearthstonepp
+{
+void BasicCardsGen::AddHeroes(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddHeroPowers(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddDruid(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddDruidNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddHunter(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddHunterNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddMage(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddMageNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddPaladin(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddPaladinNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddPriest(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddPriestNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddRogue(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddRogueNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddShaman(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddShamanNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddWarlock(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddWarlockNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddWarrior(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddWarriorNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddNeutral(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddNeutralNonCollect(std::map<std::string, Power> cards)
+{
+}
+
+void BasicCardsGen::AddAll(std::map<std::string, Power> cards)
+{
+}
+}
