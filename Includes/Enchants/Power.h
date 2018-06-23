7a52773c3fde696f4cdf3e8774a3d681b5fcb03b
Chris Ohk <utilForever@gmail.com>
2018-06-23 17:29:59 +0900

    [ci skip] Create 'Power' struct

---
+++ Includes/Enchants/Power.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+> File Name: Power.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Power class that stores aura, enchant and so on.
+> Created Time: 2018/06/23
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_POWER_H
+#define HEARTHSTONEPP_POWER_H
+
+namespace Hearthstonepp
+{
+struct Power
+{
+    void AddPowerTask();
+};
+}
+
+#endif
