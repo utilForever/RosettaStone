6f672d76084268ac3007c0ef1d4dcc7aa220f74e
Chris Ohk <utilForever@gmail.com>
2018-06-26 23:50:36 +0900

    [ci skip] Create PowerTask class
    PowerTask that processes the power of card

---
+++ Includes/Tasks/PowerTask.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+> File Name: PowerTask.h
+> Project Name: Hearthstonepp
+> Author: Chan-Ho Chris Ohk
+> Purpose: PowerTask that processes the power of card.
+> Created Time: 2018/06/26
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+namespace Hearthstonepp::PowerTask
+{
+
+}
---
+++ Sources/Tasks/PowerTask.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+> File Name: PowerTask.cpp
+> Project Name: Hearthstonepp
+> Author: Chan-Ho Chris Ohk
+> Purpose: PowerTask that processes the power of card.
+> Created Time: 2018/06/26
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
