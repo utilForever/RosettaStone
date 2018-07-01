05566010d67acd7e25a5baac0d7797c342327339
Chris Ohk <utilForever@gmail.com>
2018-07-01 23:55:17 +0900

    [ci skip] Create TaskEnums

---
+++ Includes/Enums/TaskEnums.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+> File Name: TaskEnums.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Enumerations for task.
+> Created Time: 2018/07/01
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_TASK_ENUMS_H
+#define HEARTHSTONEPP_TASK_ENUMS_H
+
+#ifdef _MSC_VER
+#define HEARTHSTONEPP_EXPAND_LARGE_ENUMS
+#else  // _MSC_VER
+#undef BETTER_ENUMS_MACRO_FILE
+#define BETTER_ENUMS_MACRO_FILE "../../Includes/Enums/EnumMacros.h"
+#endif  // _MSC_VER
+
+#include <better-enums/enum.h>
+
+namespace Hearthstonepp
+{
+BETTER_ENUM(PowerTaskType, int, EMPTY = 0, DESTROY_WEAPON = 1)
+}
+
+#endif
