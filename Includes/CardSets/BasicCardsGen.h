7338acf07961c0fd6f7e6e06202f926ef73d2493
Chris Ohk <utilForever@gmail.com>
2018-07-02 02:01:28 +0900

    [ci skip] Change BasicCardsGen method's argument type

--- Includes/CardSets/BasicCardsGen.h
+++ Includes/CardSets/BasicCardsGen.h
@@ -19,40 +19,40 @@ namespace Hearthstonepp
 class BasicCardsGen
 {
  public:
-    static void AddHeroes(std::map<std::string, Power> cards);
-    static void AddHeroPowers(std::map<std::string, Power> cards);
+    static void AddHeroes(std::map<std::string, Power>& cards);
+    static void AddHeroPowers(std::map<std::string, Power>& cards);
 
-    static void AddDruid(std::map<std::string, Power> cards);
-    static void AddDruidNonCollect(std::map<std::string, Power> cards);
+    static void AddDruid(std::map<std::string, Power>& cards);
+    static void AddDruidNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddHunter(std::map<std::string, Power> cards);
-    static void AddHunterNonCollect(std::map<std::string, Power> cards);
+    static void AddHunter(std::map<std::string, Power>& cards);
+    static void AddHunterNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddMage(std::map<std::string, Power> cards);
-    static void AddMageNonCollect(std::map<std::string, Power> cards);
+    static void AddMage(std::map<std::string, Power>& cards);
+    static void AddMageNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddPaladin(std::map<std::string, Power> cards);
-    static void AddPaladinNonCollect(std::map<std::string, Power> cards);
+    static void AddPaladin(std::map<std::string, Power>& cards);
+    static void AddPaladinNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddPriest(std::map<std::string, Power> cards);
-    static void AddPriestNonCollect(std::map<std::string, Power> cards);
+    static void AddPriest(std::map<std::string, Power>& cards);
+    static void AddPriestNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddRogue(std::map<std::string, Power> cards);
-    static void AddRogueNonCollect(std::map<std::string, Power> cards);
+    static void AddRogue(std::map<std::string, Power>& cards);
+    static void AddRogueNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddShaman(std::map<std::string, Power> cards);
-    static void AddShamanNonCollect(std::map<std::string, Power> cards);
+    static void AddShaman(std::map<std::string, Power>& cards);
+    static void AddShamanNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddWarlock(std::map<std::string, Power> cards);
-    static void AddWarlockNonCollect(std::map<std::string, Power> cards);
+    static void AddWarlock(std::map<std::string, Power>& cards);
+    static void AddWarlockNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddWarrior(std::map<std::string, Power> cards);
-    static void AddWarriorNonCollect(std::map<std::string, Power> cards);
+    static void AddWarrior(std::map<std::string, Power>& cards);
+    static void AddWarriorNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddNeutral(std::map<std::string, Power> cards);
-    static void AddNeutralNonCollect(std::map<std::string, Power> cards);
+    static void AddNeutral(std::map<std::string, Power>& cards);
+    static void AddNeutralNonCollect(std::map<std::string, Power>& cards);
 
-    static void AddAll(std::map<std::string, Power> cards);
+    static void AddAll(std::map<std::string, Power>& cards);
 };
 }
 
--- Sources/CardSets/BasicCardsGen.cpp
+++ Sources/CardSets/BasicCardsGen.cpp
@@ -11,87 +11,107 @@
 
 namespace Hearthstonepp
 {
-void BasicCardsGen::AddHeroes(std::map<std::string, Power> cards)
+void BasicCardsGen::AddHeroes(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddHeroPowers(std::map<std::string, Power> cards)
+void BasicCardsGen::AddHeroPowers(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddDruid(std::map<std::string, Power> cards)
+void BasicCardsGen::AddDruid(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddDruidNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddDruidNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddHunter(std::map<std::string, Power> cards)
+void BasicCardsGen::AddHunter(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddHunterNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddHunterNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddMage(std::map<std::string, Power> cards)
+void BasicCardsGen::AddMage(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddMageNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddMageNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddPaladin(std::map<std::string, Power> cards)
+void BasicCardsGen::AddPaladin(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddPaladinNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddPaladinNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddPriest(std::map<std::string, Power> cards)
+void BasicCardsGen::AddPriest(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddPriestNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddPriestNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddRogue(std::map<std::string, Power> cards)
+void BasicCardsGen::AddRogue(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddRogueNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddRogueNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddShaman(std::map<std::string, Power> cards)
+void BasicCardsGen::AddShaman(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddShamanNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddShamanNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddWarlock(std::map<std::string, Power> cards)
+void BasicCardsGen::AddWarlock(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddWarlockNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddWarlockNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddWarrior(std::map<std::string, Power> cards)
+void BasicCardsGen::AddWarrior(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddWarriorNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddWarriorNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddNeutral(std::map<std::string, Power> cards)
+void BasicCardsGen::AddNeutral(std::map<std::string, Power>& cards)
 {
     // --------------------------------------- MINION - NEUTRAL
     // [EX1_066] Acidic Swamp Ooze - COST:2 [ATK:3/HP:2]
@@ -107,11 +127,13 @@ void BasicCardsGen::AddNeutral(std::map<std::string, Power> cards)
     cards.emplace("EX1_066", p);
 }
 
-void BasicCardsGen::AddNeutralNonCollect(std::map<std::string, Power> cards)
+void BasicCardsGen::AddNeutralNonCollect(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 
-void BasicCardsGen::AddAll(std::map<std::string, Power> cards)
+void BasicCardsGen::AddAll(std::map<std::string, Power>& cards)
 {
+    (void)cards;
 }
 }
