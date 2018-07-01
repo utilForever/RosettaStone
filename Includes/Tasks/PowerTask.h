b849b4794fadfb6f9eb1540ca5881da5554693e6
Chris Ohk <utilForever@gmail.com>
2018-07-01 23:59:01 +0900

    [ci skip] Add DestoryWeapon function

--- Includes/Tasks/PowerTask.h
+++ Includes/Tasks/PowerTask.h
@@ -6,7 +6,15 @@
 > Created Time: 2018/06/26
 > Copyright (c) 2018, Chan-Ho Chris Ohk
 *************************************************************************/
+#ifndef HEARTHSTONEPP_POWER_TASK_H
+#define HEARTHSTONEPP_POWER_TASK_H
+
+#include <Syncs/AgentStructures.h>
+#include <Tasks/TaskMeta.h>
+
 namespace Hearthstonepp::PowerTask
 {
-
+TaskMeta DestroyWeapon(Player& player);
 }
+
+#endif
--- Sources/Tasks/PowerTask.cpp
+++ Sources/Tasks/PowerTask.cpp
@@ -6,3 +6,16 @@
 > Created Time: 2018/06/26
 > Copyright (c) 2018, Chan-Ho Chris Ohk
 *************************************************************************/
+#include <Tasks/PowerTask.h>
+#include <Tasks/TaskSerializer.h>
+
+namespace Hearthstonepp::PowerTask
+{
+TaskMeta DestroyWeapon(Player& player)
+{
+    delete player.hero->weapon;
+
+    std::vector<TaskMeta> vector;
+    return Serializer::CreateTaskMetaVector(vector);
+}
+}
