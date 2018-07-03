dfad56e5f3fa56acb85d1cdcde8c7bc4988c2894
Chris Ohk <utilForever@gmail.com>
2018-07-03 23:06:50 +0900

    [ci skip] Add tests for CS2_041 card

--- Tests/UnitTests/BasicCardTests.cpp
+++ Tests/UnitTests/BasicCardTests.cpp
@@ -34,3 +34,27 @@ TEST(BasicCard, EX1_066)
         BasicTask::PlayCardTask(0, 0));
     EXPECT_EQ(agent.GetPlayer1().hero->weapon != nullptr, false);
 }
+
+TEST(BasicCard, CS2_041)
+{
+    GameAgent agent(
+        Player(new Account("Player 1", ""), new Deck("", CardClass::SHAMAN)),
+        Player(new Account("Player 2", ""), new Deck("", CardClass::MAGE)));
+    agent.GetPlayer1().totalMana = agent.GetPlayer1().existMana = 10;
+    agent.GetPlayer2().totalMana = agent.GetPlayer2().existMana = 10;
+
+    agent.Process(agent.GetPlayer1(),
+                  BasicTask::DrawTask(Cards::GetInstance()->FindCardByName(
+                      "Acidic Swamp Ooze")));
+    agent.Process(agent.GetPlayer1(),
+                  BasicTask::DrawTask(Cards::GetInstance()->FindCardByName(
+                      "Ancestral Healing")));
+    EXPECT_EQ(agent.GetPlayer1().hand.size(), static_cast<size_t>(2));
+
+    agent.Process(agent.GetPlayer1(), BasicTask::PlayCardTask(0));
+    dynamic_cast<Character*>(agent.GetPlayer1().field.at(0))->health = 1;
+    EXPECT_EQ(dynamic_cast<Character*>(agent.GetPlayer1().field.at(0))->health, 1);
+
+    agent.Process(agent.GetPlayer1(),
+                  BasicTask::PlayCardTask(0, -1, TargetType::MY_FIELD, 1));
+}
