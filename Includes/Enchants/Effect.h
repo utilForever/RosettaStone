3fee77485a7fed2bf2ac48a3506bb18288177822
Chris Ohk <utilForever@gmail.com>
2018-07-07 15:32:09 +0900

    [ci skip] Create Effect class

---
+++ Includes/Enchants/Effect.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: Effect.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Represents an effect of auras or enchantment cards.
+> Created Time: 2018/07/07
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_EFFECT_H
+#define HEARTHSTONEPP_EFFECT_H
+
+struct Effect
+{
+
+};
+
+#endif
