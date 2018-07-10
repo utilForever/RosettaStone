f59c7af18fa18d2b905095469a22907ccd24e687
Chris Ohk <utilForever@gmail.com>
2018-07-10 18:21:45 +0900

    [ci skip] Add Entity header file

---
+++ Includes/Cards/Entity.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+> File Name: Entity.h
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: The base class of all data-holding/action-performing/
+           visible or invisible objects in a Hearthstone++.
+           An entity is defined as a collection of properties, called Tags.
+> Created Time: 2018/07/10
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef HEARTHSTONEPP_ENTITY_H
+#define HEARTHSTONEPP_ENTITY_H
+
+
+
+#endif
