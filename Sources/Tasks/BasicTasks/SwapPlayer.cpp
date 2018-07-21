6d9c1f70ded408a117bbb4c354bf9e41cb265b7c
revsic <revsic99@gmail.com>
2018-07-22 05:29:01 +0900

    [ci skip] Update SwapPlayer - Fix Invalid Variable Name

--- Sources/Tasks/BasicTasks/SwapPlayer.cpp
+++ Sources/Tasks/BasicTasks/SwapPlayer.cpp
@@ -17,7 +17,7 @@ TaskID SwapPlayerTask::GetTaskID() const
     return TaskID::SWAP;
 }
 
-MetaData SwapPlayerTask::Impl(Player& player1, Player& player1) const
+MetaData SwapPlayerTask::Impl(Player& player1, Player& player2) const
 {
     std::swap(player1, player2);
     return MetaData::SWAP_SUCCESS;
