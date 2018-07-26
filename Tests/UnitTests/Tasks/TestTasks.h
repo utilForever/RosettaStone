b63a76215c5c76564c26b9ecb5625b44f2660769
revsic <revsic99@gmail.com>
2018-07-26 23:30:21 +0900

    [ci skip] Upload TestTasks - Tasks for Test

---
+++ Tests/UnitTests/Tasks/TestTasks.h
@@ -0,0 +1,28 @@
+#ifndef UNITTEST_TEST_TASKS_H
+#define UNITTEST_TEST_TASKS_H
+
+#include <Enums/TaskEnums.h>
+#include <Managers/Player.h>
+#include <Tasks/MetaData.h>
+#include <Tasks/Tasks.h>
+
+#include <functional>
+
+using namespace Hearthstonepp;
+
+class TestTask : public ITask
+{
+public:
+	TestTask(TaskID id, std::function<MetaData(Player &, Player &)> &&impl);
+	
+	TaskID GetTaskID() const override;
+
+private:
+	TaskID m_id;
+	std::function<MetaData(Player &, Player &)> m_impl;
+
+	MetaData Impl(Player &player1, Player &player2) const override;
+};
+
+
+#endif  // UNITTEST_TEST_TASKS_H
