f9f15afa799ea56fdaadbc07ae54c1c0f0607cb2
Sungkuk Park <sungkukpark@maverickgames.co>
2018-08-15 16:05:26 +0900

    Add implement discard task and succubus minion

--- Tests/UnitTests/CardSets/BasicCardTests.cpp
+++ Tests/UnitTests/CardSets/BasicCardTests.cpp
@@ -296,3 +296,58 @@ TEST(BasicCard, CS2_179)
     EXPECT_EQ(agent.GetPlayer1().field[0]->health, static_cast<size_t>(4));
     EXPECT_EQ(agent.GetPlayer2().field.size(), static_cast<size_t>(0));
 }
+
+TEST(BasicCard, EX1_306)
+{
+    GameAgent agent(
+        Player(new Account("Player 1", ""), new Deck("", CardClass::WARRIOR)),
+        Player(new Account("Player 2", ""), new Deck("", CardClass::MAGE)));
+
+    TaskAgent& taskAgent = agent.GetTaskAgent();
+    TestUtils::AutoResponder response(agent);
+
+    Player& player1 = agent.GetPlayer1();
+    Player& player2 = agent.GetPlayer2();
+
+    player1.totalMana = player1.existMana = 10;
+    player2.totalMana = player2.existMana = 10;
+
+	// Each players draw 2 cards.
+    agent.RunTask(BasicTasks::DrawCardTask(
+                      Cards::GetInstance()->FindCardByName("Succubus")),
+                  player1, player2);
+    EXPECT_EQ(agent.GetPlayer1().hand.size(), static_cast<size_t>(1));
+    EXPECT_EQ(agent.GetPlayer1().hand[0]->card->name, "Succubus");
+
+    agent.RunTask(BasicTasks::DrawCardTask(
+                      Cards::GetInstance()->FindCardByName("Fiery War Axe")),
+                  player1, player2);
+    EXPECT_EQ(agent.GetPlayer1().hand.size(), static_cast<size_t>(2));
+    EXPECT_EQ(agent.GetPlayer1().hand[1]->card->name, "Fiery War Axe");
+
+    agent.RunTask(BasicTasks::DrawCardTask(Cards::GetInstance()->FindCardByName(
+                      "Acidic Swamp Ooze")),
+                  player2, player1);
+    EXPECT_EQ(agent.GetPlayer2().hand.size(), static_cast<size_t>(1));
+    EXPECT_EQ(agent.GetPlayer2().hand[0]->card->name, "Acidic Swamp Ooze");
+
+    agent.RunTask(BasicTasks::DrawCardTask(
+                      Cards::GetInstance()->FindCardByName("Stonetusk Boar")),
+                  player2, player1);
+    EXPECT_EQ(agent.GetPlayer2().hand.size(), static_cast<size_t>(2));
+    EXPECT_EQ(agent.GetPlayer2().hand[1]->card->name, "Stonetusk Boar");
+
+	// Player 1 plays Succubus, and now the player 1's hand is empty
+    auto respAutoMinion = response.AutoMinion(0, 0);
+    MetaData result =
+        agent.RunTask(BasicTasks::PlayCardTask(taskAgent), player1, player2);
+    EXPECT_EQ(result, MetaData::PLAY_MINION_SUCCESS);
+    EXPECT_EQ(agent.GetPlayer1().hand.size(), 0);
+
+	// This task doesn't affect player 2's hand
+    respAutoMinion = response.AutoMinion(0, 0);
+    result =
+        agent.RunTask(BasicTasks::PlayCardTask(taskAgent), player2, player1);
+    EXPECT_EQ(result, MetaData::PLAY_MINION_SUCCESS);
+    EXPECT_EQ(agent.GetPlayer2().hand.size(), 1);
+}
