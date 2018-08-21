58ce8c41f206d7dcb6292faedd84a0ceb7205603
Chris Ohk <utilForever@gmail.com>
2018-08-21 12:18:17 +0900

    [ci skip] Initial commit for HSppTool project

---
+++ Tools/main.cpp
@@ -0,0 +1,13 @@
+
+/*************************************************************************
+> File Name: main.cpp
+> Project Name: Hearthstone++
+> Author: Chan-Ho Chris Ohk
+> Purpose: Main entry of Hearthstone++ tool.
+> Created Time: 2018/08/21
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+int main()
+{
+    return 0;
+}
