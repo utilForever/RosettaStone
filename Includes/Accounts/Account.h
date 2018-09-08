13356b843e7fde96dd985529f0d11c02ba154ad0
Chris Ohk <utilForever@gmail.com>
2018-09-08 21:21:43 +0900

    [ci skip] docs: Add comments for doxygen

--- Includes/Accounts/Account.h
+++ Includes/Accounts/Account.h
@@ -1,11 +1,3 @@
-/*************************************************************************
-> File Name: Account.h
-> Project Name: Hearthstone++
-> Author: Chan-Ho Chris Ohk
-> Purpose: Account class that stores a list of decks.
-> Created Time: 2017/10/18
-> Copyright (c) 2017, Chan-Ho Chris Ohk
-*************************************************************************/
 #ifndef HEARTHSTONEPP_PLAYER_H
 #define HEARTHSTONEPP_PLAYER_H
 
@@ -15,27 +7,67 @@
 
 namespace Hearthstonepp
 {
+//!
+//! \brief Account class.
+//!
+//! This class stores user information such as e-mail address and nickname.
+//! Also, it stores a list of decks. A user can construct several decks.
+//!
 class Account
 {
  public:
+    //! Constructs anonymous account.
     Account();
-    Account(std::string&& email, std::string&& nickname);
-    Account(std::string&& email, std::string&& nickname, std::vector<Deck*> decks);
 
+    //! Constructs account with given \p email and \p nickname.
+    //! \param email E-mail address of user.
+    //! \param nickname Nickname of user.
+    Account(std::string&& email, std::string&& nickname);
+
+    //! Constructs account with given \p email and \p nickname
+    //! and fill deck list with \p decks.
+    //! \param email E-mail address of user.
+    //! \param nickname Nickname of user.
+    //! \param decks A list of decks.
+    Account(std::string&& email, std::string&& nickname,
+            std::vector<Deck*> decks);
+
+    //! Returns e-mail address of user.
+    //! \return E-mail address of user.
     std::string GetEmail() const;
+
+    //! Returns nickname of user.
+    //! \return Nickname of user.
     std::string GetNickname() const;
+
+    //! Returns the number of decks.
+    //! \return Number of decks.
     size_t GetNumOfDeck() const;
+
+    //! Returns deck at given index.
+    //! \param idx Index of decks.
+    //! \return Pointer to deck at given index.
     Deck* GetDeck(size_t idx) const;
 
+    //! Prints a list of decks.
     void ShowDeckList() const;
-    bool CreateDeck(std::string name, CardClass playerClass);
-    bool DeleteDeck(size_t selectedDeck);
+
+    //! Creates a deck with given \p name and \p deckClass.
+    //! \param name The name of deck.
+    //! \param deckClass The class of deck.
+    //! \return true if deck is created successfully, and false otherwise.
+    bool CreateDeck(std::string name, CardClass deckClass);
+
+    //! Deletes a deck with given \p idx.
+    //! \param idx Index of decks.
+    //! \return true if deck is deleted successfully, and false otherwise.
+    bool DeleteDeck(size_t idx);
 
  private:
     std::string m_email;
     std::string m_nickname;
     std::vector<Deck*> m_decks;
 };
-}
+}  // namespace Hearthstonepp
 
 #endif
