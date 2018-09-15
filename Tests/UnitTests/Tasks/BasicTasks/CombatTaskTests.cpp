579d489973b1dfa16556e803b779e9fbfa275eee
revsic <revsic99@gmail.com>
2018-09-15 20:20:04 +0900

    Update CombatTaskTests - Implement CombatDefault

--- Tests/UnitTests/Tasks/BasicTasks/CombatTaskTests.cpp
+++ Tests/UnitTests/Tasks/BasicTasks/CombatTaskTests.cpp
@@ -3,6 +3,7 @@
 #include <Utils/TestUtils.h>
 
 #include <Tasks/BasicTasks/CombatTask.h>
+#include <Tasks/BasicTasks/InitAttackCountTask.h>
 
 using namespace Hearthstonepp;
 
@@ -16,12 +17,40 @@ TEST(CombatTask, GetTaskID)
 TEST(CombatTask, CombatDefault)
 {
     TestUtils::PlayerGenerator gen(CardClass::DRUID, CardClass::ROGUE);
-
     GameAgent agent(gen.player1, gen.player2);
     TestUtils::AutoResponder resp(agent);
+
+    BasicTasks::InitAttackCountTask init;
     BasicTasks::CombatTask combat(agent.GetTaskAgent());
 
-    
+    auto attack = [&](size_t src, size_t dst) {
+        init.Run(gen.player1, gen.player2);
+        init.Run(gen.player2, gen.player1);
+
+        auto target = resp.Target(1, 1);
+        MetaData result = combat.Run(gen.player1, gen.player2);
+        EXPECT_EQ(result, MetaData::COMBAT_SUCCESS);
+
+        TaskMeta meta = target.get();
+        EXPECT_EQ(meta.id, +TaskID::REQUIRE);
+    };
+
+    auto card1 = TestUtils::GenerateMinion("minion1", 3, 6);
+    auto card2 = TestUtils::GenerateMinion("minion2", 5, 4);
+
+    Character minion1(card1.get());
+    Character minion2(card2.get());
+
+    gen.player1.field.emplace_back(&minion1);
+    gen.player2.field.emplace_back(&minion2);
+
+    attack(1, 1);
+    EXPECT_EQ(gen.player1.field[0]->health, static_cast<size_t>(1));
+    EXPECT_EQ(gen.player2.field[0]->health, static_cast<size_t>(1));
+
+    attack(1, 1);
+    EXPECT_EQ(gen.player1.field.size(), static_cast<size_t>(0));
+    EXPECT_EQ(gen.player2.field.size(), static_cast<size_t>(0));
 }
 
 TEST(CombatTask, CombatTaunt)
