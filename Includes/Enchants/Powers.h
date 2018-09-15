e648b004321e88a1fa7e13eb39a90ed2b2330756
Chris Ohk <utilForever@gmail.com>
2018-09-15 20:29:25 +0900

    [ci skip] refactor: Change type (struct -> class)

--- Includes/Enchants/Powers.h
+++ Includes/Enchants/Powers.h
@@ -7,12 +7,13 @@
 namespace Hearthstonepp
 {
 //!
-//! \brief Powers structure.
+//! \brief Powers class.
 //!
-//! This structure stores a list of power.
+//! This class stores a list of power.
 //!
-struct Powers
+class Powers
 {
+ public:
     //! Deleted copy constructor.
     Powers(const Powers& other) = delete;
 
@@ -28,7 +29,7 @@ struct Powers
     //! \return A pointer to card that matches \p cardID.
     Power* FindPowerByCardID(std::string cardID) const;
 
-private:
+ private:
     //! Constructor that loads power data.
     Powers();
 
@@ -36,6 +37,6 @@ private:
 
     std::map<std::string, Power*> m_powers;
 };
-}
+}  // namespace Hearthstonepp
 
 #endif
