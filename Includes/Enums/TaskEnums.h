c157e7d4d3e91274898c18c996d67051ca26eee5
Chris Ohk <utilForever@gmail.com>
2018-09-16 15:58:46 +0900

    [ci skip] docs: Add comments for doxygen
    - Remove header comments

--- Includes/Enums/TaskEnums.h
+++ Includes/Enums/TaskEnums.h
@@ -1,11 +1,3 @@
-/*************************************************************************
-> File Name: TaskEnums.h
-> Project Name: Hearthstone++
-> Author: Chan-Ho Chris Ohk
-> Purpose: Enumerations for task.
-> Created Time: 2018/07/01
-> Copyright (c) 2018, Chan-Ho Chris Ohk
-*************************************************************************/
 #ifndef HEARTHSTONEPP_TASK_ENUMS_H
 #define HEARTHSTONEPP_TASK_ENUMS_H
 
@@ -20,18 +12,82 @@
 
 namespace Hearthstonepp
 {
+//! \brief An enumerator for identifying each task.
+#ifndef HEARTHSTONEPP_DOXYGEN
 BETTER_ENUM(TaskID, int, INVALID = 0, TASK_VECTOR = 1, REQUIRE = 2,
             PLAYER_SETTING = 3, SWAP = 4, SHUFFLE = 5, DRAW = 6, OVER_DRAW = 7,
             MODIFY_MANA = 8, MODIFY_HEALTH = 9, BRIEF = 10, SELECT_MENU = 11,
             SELECT_CARD = 12, SELECT_TARGET = 13, SELECT_POSITION = 14,
             MULLIGAN = 15, COMBAT = 16, PLAY_CARD = 17, PLAY_MINION = 18,
-            PLAY_WEAPON = 19, PLAY_SPELL = 20, GAME_END = 21, ADD_ENCHANTMENT = 22,
-            DESTROY = 23, DISCARD = 24, HEAL_FULL = 25, POISONOUS = 26, FREEZE = 27, INIT_ATTACK_COUNT = 28, HEAL = 29)
+            PLAY_WEAPON = 19, PLAY_SPELL = 20, GAME_END = 21,
+            ADD_ENCHANTMENT = 22, DESTROY = 23, DISCARD = 24, HEAL_FULL = 25,
+            POISONOUS = 26, FREEZE = 27, INIT_ATTACK_COUNT = 28, HEAL = 29);
+#else
+enum class TaskID
+{
+    INVALID = 0,
+    TASK_VECTOR = 1,
+    REQUIRE = 2,
+    PLAYER_SETTING = 3,
+    SWAP = 4,
+    SHUFFLE = 5,
+    DRAW = 6,
+    OVER_DRAW = 7,
+    MODIFY_MANA = 8,
+    MODIFY_HEALTH = 9,
+    BRIEF = 10,
+    SELECT_MENU = 11,
+    SELECT_CARD = 12,
+    SELECT_TARGET = 13,
+    SELECT_POSITION = 14,
+    MULLIGAN = 15,
+    COMBAT = 16,
+    PLAY_CARD = 17,
+    PLAY_MINION = 18,
+    PLAY_WEAPON = 19,
+    PLAY_SPELL = 20,
+    GAME_END = 21,
+    ADD_ENCHANTMENT = 22,
+    DESTROY = 23,
+    DISCARD = 24,
+    HEAL_FULL = 25,
+    POISONOUS = 26,
+    FREEZE = 27,
+    INIT_ATTACK_COUNT = 28,
+    HEAL = 29
+};
+#endif
 
-BETTER_ENUM(EntityType, int, EMPTY = 0, SOURCE = 1, TARGET = 2, OPPONENT_WEAPON = 3, MY_HAND = 4, MY_HERO = 5)
+//! \brief An enumerator for identifying entity type.
+#ifndef HEARTHSTONEPP_DOXYGEN
+BETTER_ENUM(EntityType, int, EMPTY = 0, SOURCE = 1, TARGET = 2,
+            OPPONENT_WEAPON = 3, MY_HAND = 4, MY_HERO = 5)
+#else
+enum class EntityType
+{
+    EMPTY = 0,
+    SOURCE = 1,
+    TARGET = 2,
+    OPPONENT_WEAPON = 3,
+    MY_HAND = 4,
+    MY_HERO = 5
+};
+#endif
 
+//! \brief An enumerator for identifying target type.
+#ifndef HEARTHSTONEPP_DOXYGEN
 BETTER_ENUM(TargetType, int, INVALID = 0, MY_HERO = 1, MY_FIELD = 2,
-            OPPONENT_HERO = 3, OPPONENT_FIELD = 4);
+            OPPONENT_HERO = 3, OPPONENT_FIELD = 4)
+#else
+enum class TargetType
+{
+    INVALID = 0,
+    MY_HERO = 1,
+    MY_FIELD = 2,
+    OPPONENT_HERO = 3,
+    OPPONENT_FIELD = 4
+};
+#endif
 }  // namespace Hearthstonepp
 
 #endif
