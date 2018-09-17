dea2e83dcdb81d24c59a98eee731d68e6b267be6
Chris Ohk <utilForever@gmail.com>
2018-09-17 18:19:13 +0900

    [ci skip] docs: Remove header comments

--- Includes/Managers/GameAgent.h
+++ Includes/Managers/GameAgent.h
@@ -1,11 +1,3 @@
-/*************************************************************************
-> File Name: GameAgent.h
-> Project Name: Hearthstone++
-> Author: Young-Joong Kim
-> Purpose: Hearthstone Game Agent
-> Created Time: 2017/09/26
-> Copyright (c) 2017, Young-Joong Kim
-*************************************************************************/
 #ifndef HEARTHSTONEPP_GAME_AGENT_H
 #define HEARTHSTONEPP_GAME_AGENT_H
 
