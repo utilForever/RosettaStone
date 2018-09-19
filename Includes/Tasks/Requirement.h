eec52d4977c9b3300d9ca005d637df338f13dc8d
Chris Ohk <utilForever@gmail.com>
2018-09-20 02:17:52 +0900

    [ci skip] docs: Add comments for doxygen
    - Remove header comments
    - Change reference mark position

--- Includes/Tasks/Requirement.h
+++ Includes/Tasks/Requirement.h
@@ -1,11 +1,3 @@
-/*************************************************************************
-> File Name: Requirement.h
-> Project Name: Hearthstonepp
-> Author: Young-Joong Kim
-> Purpose: Implement Requirement, Interact with GameInterface
-> Created Time: 2018/07/21
-> Copyright (c) 2018, Young-Joong Kim
-*************************************************************************/
 #ifndef HEARTHSTONEPP_REQUIREMENT_H
 #define HEARTHSTONEPP_REQUIREMENT_H
 
@@ -14,12 +6,23 @@
 
 namespace Hearthstonepp::BasicTasks
 {
-// Send Requirements to GameInterface and Return Response Data
+//!
+//! \brief Requirement class.
+//!
+//! This class sends requirements to game interface and returns meta data that
+//! the result of the task.
+//!
 class Requirement
 {
  public:
+    //! Constructs requirement with given \p request and \p agent.
+    //! \param request A task ID to request.
+    //! \param agent A task agent.
     Requirement(TaskID request, TaskAgent& agent);
 
+    //! Notifies task meta to main channel reads task meta from side channel.
+    //! \param userID The unique user ID.
+    //! \param meta The meta data that the result of the task.
     void Interact(BYTE userID, TaskMeta& meta) const;
 
  private:
--- Sources/Tasks/Requirement.cpp
+++ Sources/Tasks/Requirement.cpp
@@ -1,22 +1,14 @@
-/*************************************************************************
-> File Name: Requirement.cpp
-> Project Name: Hearthstonepp
-> Author: Young-Joong Kim
-> Purpose: Implement Requirement, Interact with GameInterface
-> Created Time: 2018/07/21
-> Copyright (c) 2018, Young-Joong Kim
-*************************************************************************/
 #include <Tasks/Requirement.h>
 
 namespace Hearthstonepp::BasicTasks
 {
-Requirement::Requirement(TaskID request, TaskAgent &agent)
+Requirement::Requirement(TaskID request, TaskAgent& agent)
     : m_request(request), m_agent(agent)
 {
     // Do Nothing
 }
 
-void Requirement::Interact(BYTE userID, TaskMeta &meta) const
+void Requirement::Interact(BYTE userID, TaskMeta& meta) const
 {
     meta = Serializer::CreateRequire(m_request, userID);
 
