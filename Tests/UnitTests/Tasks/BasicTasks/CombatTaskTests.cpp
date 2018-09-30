79f2d6592dd4542e18ea2c6c51c49db5ecf02f8e
revsic <revsic99@gmail.com>
2018-09-30 14:53:51 +0900

    Update CombatTaskTests - Fix test error from CombatDefault

--- Tests/UnitTests/Tasks/BasicTasks/CombatTaskTests.cpp
+++ Tests/UnitTests/Tasks/BasicTasks/CombatTaskTests.cpp
@@ -38,8 +38,8 @@ TEST(CombatTask, CombatDefault)
     auto card1 = TestUtils::GenerateMinion("minion1", 3, 6);
     auto card2 = TestUtils::GenerateMinion("minion2", 5, 4);
 
-    Character minion1(card1.get());
-    Character minion2(card2.get());
+    Minion minion1(card1.get());
+    Minion minion2(card2.get());
 
     gen.player1.field.emplace_back(&minion1);
     gen.player2.field.emplace_back(&minion2);
@@ -56,22 +56,48 @@ TEST(CombatTask, CombatDefault)
     EXPECT_EQ(gen.player1.field.size(), static_cast<size_t>(0));
     EXPECT_EQ(gen.player2.field.size(), static_cast<size_t>(0));
 
-    card1->attack = 5;
-    Character minion3(card1.get());
+    auto card3 = TestUtils::GenerateMinion("minion3", 5, 6);
+    auto card4 = TestUtils::GenerateMinion("minion4", 5, 4);
+
+    Minion minion3(card3.get());
+    Minion minion4(card4.get());
 
     gen.player1.field.emplace_back(&minion3);
-    gen.player2.field.emplace_back(&minion2);
+    gen.player2.field.emplace_back(&minion4);
 
     attack(1, 1);
     EXPECT_EQ(gen.player1.field[0]->health, static_cast<size_t>(1));
     EXPECT_EQ(gen.player2.field.size(), static_cast<size_t>(0));
 
+    auto card5 = TestUtils::GenerateMinion("minion5", 5, 4);
+
+    Minion minion5(card5.get());
+
     gen.player1.field[0]->attack = 1;
-    gen.player2.field.emplace_back(&minion2);
+    gen.player2.field.emplace_back(&minion5);
 
     attack(1, 1);
     EXPECT_EQ(gen.player1.field.size(), static_cast<size_t>(0));
     EXPECT_EQ(gen.player2.field[0]->health, static_cast<size_t>(3));
+}
+
+TEST(CombatTask, CombatAlreadyAttacked)
+{
+    TestUtils::PlayerGenerator gen(CardClass::DRUID, CardClass::ROGUE);
+    GameAgent agent(gen.player1, gen.player2);
+    TestUtils::AutoResponder resp(agent);
+
+    BasicTasks::CombatTask combat(agent.GetTaskAgent());
+    auto attack = [&](size_t src, size_t dst) {
+        auto target = resp.Target(src, dst);
+        MetaData result = combat.Run(gen.player1, gen.player2);
+        EXPECT_EQ(result, MetaData::COMBAT_SUCCESS);
+
+        TaskMeta meta = target.get();
+        EXPECT_EQ(meta.id, +TaskID::REQUIRE);
+    };
+
+    auto card = TestUtils::GenerateMinion("minion1", 1, 10);
 }
 
 TEST(CombatTask, CombatTaunt)
