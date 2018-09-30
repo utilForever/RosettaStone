e1fe33610bc97105822585a1d24630c145c9b76b
Chris Ohk <utilForever@gmail.com>
2018-09-30 15:42:00 +0900

    feat(docs): Apply header generation script to Hearthstonepp

--- Scripts/header_gen.py
+++ Scripts/header_gen.py
@@ -10,10 +10,10 @@ dirname = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 
 def main():
-    include_dir = os.path.join(dirname, "../Includes")
+    include_dir = os.path.join(dirname, "../Includes/hspp")
     filenames = utils.get_all_files(include_dir, ["*.h"])
     filenames.sort()
-    header = os.path.join(dirname, "../Includes/Hearthstonepp.h")
+    header = os.path.join(dirname, "../Includes/hspp/hspp.h")
     header_tmp = header + ".tmp"
     with open(header_tmp, "w") as header_file:
         header_file.write("""// Copyright (c) 2018 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
@@ -22,12 +22,12 @@ def main():
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.\n
 """)
-        header_file.write("#ifndef HEARTHSTONEPP_HEARTHSTONEPP_H\n")
-        header_file.write("#define HEARTHSTONEPP_HEARTHSTONEPP_H\n")
+        header_file.write("#ifndef HEARTHSTONEPP_HSPP_H\n")
+        header_file.write("#define HEARTHSTONEPP_HSPP_H\n\n")
         for filename in filenames:
-            line = "#include <%s>\n" % os.path.basename(filename)
+            line = "#include <hspp/%s>\n" % filename
             header_file.write(line)
-        header_file.write("#endif  // HEARTHSTONEPP_HEARTHSTONEPP_H\n")
+        header_file.write("\n#endif  // HEARTHSTONEPP_HSPP_H\n")
     if not filecmp.cmp(header, header_tmp):
         shutil.move(header_tmp, header)
     else:
