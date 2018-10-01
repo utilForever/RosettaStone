47bc4be8e7e7605af4913f59b6bb6cdd70b47a29
revsic <revsic99@gmail.com>
2018-10-01 15:53:32 +0900

    feat: implement destroy weapon task tests

---
+++ Tests/UnitTests/Tasks/BasicTasks/DestroyWeaponTaskTests.cpp
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+#include <Utils/TestUtils.h>
+
+#include <Tasks/BasicTasks/DestroyWeaponTask.h>
+
+using namespace Hearthstonepp;
+
+TEST(DestroyWeaponTask, GetTaskID)
+{
+    BasicTasks::DestroyWeaponTask destroy;
+    EXPECT_EQ(destroy.GetTaskID(), +TaskID::DESTROY);
+}
+
+TEST(DestroyWeaponTask, Run)
+{
+    BasicTasks::DestroyWeaponTask destroy;
+    TestUtils::PlayerGenerator gen(CardClass::DRUID, CardClass::ROGUE);
+
+    Card card;
+    card.id = "weapon1";
+
+    Weapon weapon(&card);
+    gen.player1.hero->weapon = &weapon;
+
+    MetaData result = destroy.Run(gen.player1, gen.player2);
+    EXPECT_EQ(result, MetaData::DESTROY_WEAPON_SUCCESS);
+    EXPECT_EQ(gen.player1.hero->weapon, nullptr);
+}
