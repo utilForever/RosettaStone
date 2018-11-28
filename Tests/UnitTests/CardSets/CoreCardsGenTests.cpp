e29131e7c62b9796ec5991542f15d75815ca758c
Chris Ohk <utilForever@gmail.com>
2018-11-29 01:23:28 +0900

    test: Clean-up unnecessary codes and change incorrect player class

--- Tests/UnitTests/CardSets/CoreCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/CoreCardsGenTests.cpp
@@ -15,7 +15,7 @@ using namespace SimpleTasks;
 
 TEST(CoreCardsGen, EX1_066)
 {
-    GameAgent agent(CardClass::DRUID, CardClass::ROGUE, PlayerType::PLAYER1);
+    GameAgent agent(CardClass::WARRIOR, CardClass::ROGUE, PlayerType::PLAYER1);
     TaskAgent& taskAgent = agent.GetTaskAgent();
 
     Player& currentPlayer = agent.GetCurrentPlayer();
@@ -27,14 +27,9 @@ TEST(CoreCardsGen, EX1_066)
 
     const auto card1 = Generic::DrawCard(
         currentPlayer, Cards::GetInstance().FindCardByName("Fiery War Axe"));
-    EXPECT_EQ(currentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(currentPlayer.GetHand()[0]->card->name, "Fiery War Axe");
-
     const auto card2 = Generic::DrawCard(
         opponentPlayer,
         Cards::GetInstance().FindCardByName("Acidic Swamp Ooze"));
-    EXPECT_EQ(opponentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(opponentPlayer.GetHand()[0]->card->name, "Acidic Swamp Ooze");
 
     GameAgent::RunTask(currentPlayer, PlayCardTask(taskAgent, card1));
     EXPECT_EQ(currentPlayer.GetHero()->HasWeapon(), true);
@@ -45,7 +40,8 @@ TEST(CoreCardsGen, EX1_066)
 
 TEST(CoreCardsGen, EX1_306)
 {
-    GameAgent agent(CardClass::DRUID, CardClass::ROGUE, PlayerType::PLAYER1);
+    GameAgent agent(CardClass::WARLOCK, CardClass::WARRIOR,
+                    PlayerType::PLAYER1);
     TaskAgent& taskAgent = agent.GetTaskAgent();
 
     Player& currentPlayer = agent.GetCurrentPlayer();
@@ -57,24 +53,13 @@ TEST(CoreCardsGen, EX1_306)
 
     const auto card1 = Generic::DrawCard(
         currentPlayer, Cards::GetInstance().FindCardByName("Succubus"));
-    EXPECT_EQ(currentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(currentPlayer.GetHand()[0]->card->name, "Succubus");
-
     Generic::DrawCard(currentPlayer,
-                      Cards::GetInstance().FindCardByName("Fiery War Axe"));
-    EXPECT_EQ(currentPlayer.GetHand().size(), 2u);
-    EXPECT_EQ(currentPlayer.GetHand()[1]->card->name, "Fiery War Axe");
-
+                      Cards::GetInstance().FindCardByName("Stonetusk Boar"));
     const auto card2 = Generic::DrawCard(
         opponentPlayer,
         Cards::GetInstance().FindCardByName("Acidic Swamp Ooze"));
-    EXPECT_EQ(opponentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(opponentPlayer.GetHand()[0]->card->name, "Acidic Swamp Ooze");
-
     Generic::DrawCard(opponentPlayer,
-                      Cards::GetInstance().FindCardByName("Stonetusk Boar"));
-    EXPECT_EQ(opponentPlayer.GetHand().size(), 2);
-    EXPECT_EQ(opponentPlayer.GetHand()[1]->card->name, "Stonetusk Boar");
+                      Cards::GetInstance().FindCardByName("Fiery War Axe"));
 
     GameAgent::RunTask(currentPlayer, PlayCardTask(taskAgent, card1));
     EXPECT_EQ(currentPlayer.GetHand().size(), 0u);
@@ -85,7 +70,7 @@ TEST(CoreCardsGen, EX1_306)
 
 TEST(CoreCardsGen, CS2_041)
 {
-    GameAgent agent(CardClass::DRUID, CardClass::ROGUE, PlayerType::PLAYER1);
+    GameAgent agent(CardClass::SHAMAN, CardClass::ROGUE, PlayerType::PLAYER1);
     TaskAgent& taskAgent = agent.GetTaskAgent();
 
     Player& currentPlayer = agent.GetCurrentPlayer();
@@ -98,25 +83,14 @@ TEST(CoreCardsGen, CS2_041)
     const auto card1 = Generic::DrawCard(
         currentPlayer,
         Cards::GetInstance().FindCardByName("Acidic Swamp Ooze"));
-    EXPECT_EQ(currentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(currentPlayer.GetHand()[0]->card->name, "Acidic Swamp Ooze");
-
     const auto card2 = Generic::DrawCard(
         currentPlayer,
         Cards::GetInstance().FindCardByName("Ancestral Healing"));
-    EXPECT_EQ(currentPlayer.GetHand().size(), 2u);
-    EXPECT_EQ(currentPlayer.GetHand()[1]->card->name, "Ancestral Healing");
-
     const auto card3 = Generic::DrawCard(
         opponentPlayer, Cards::GetInstance().FindCardByName("Stonetusk Boar"));
-    EXPECT_EQ(opponentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(opponentPlayer.GetHand()[0]->card->name, "Stonetusk Boar");
 
     GameAgent::RunTask(currentPlayer, PlayCardTask(taskAgent, card1));
-    EXPECT_EQ(currentPlayer.GetField()[0]->card->name, "Acidic Swamp Ooze");
-
     GameAgent::RunTask(opponentPlayer, PlayCardTask(taskAgent, card3));
-    EXPECT_EQ(opponentPlayer.GetField()[0]->card->name, "Stonetusk Boar");
 
     GameAgent::RunTask(opponentPlayer, CombatTask(taskAgent, card3, card1));
     EXPECT_EQ(currentPlayer.GetField()[0]->health, 1);
@@ -130,7 +104,7 @@ TEST(CoreCardsGen, CS2_041)
 
 TEST(CoreCardsGen, CS2_088)
 {
-    GameAgent agent(CardClass::DRUID, CardClass::ROGUE, PlayerType::PLAYER1);
+    GameAgent agent(CardClass::DRUID, CardClass::PALADIN, PlayerType::PLAYER1);
     TaskAgent& taskAgent = agent.GetTaskAgent();
 
     Player& currentPlayer = agent.GetCurrentPlayer();
@@ -142,22 +116,10 @@ TEST(CoreCardsGen, CS2_088)
     opponentPlayer.GetHero()->health = 24;
 
     const auto card1 = Generic::DrawCard(
-        currentPlayer,
-        Cards::GetInstance().FindCardByName("Acidic Swamp Ooze"));
-    EXPECT_EQ(currentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(currentPlayer.GetHand()[0]->card->name, "Acidic Swamp Ooze");
-
-    const auto card2 = Generic::DrawCard(
         opponentPlayer,
         Cards::GetInstance().FindCardByName("Guardian of Kings"));
-    EXPECT_EQ(opponentPlayer.GetHand().size(), 1u);
-    EXPECT_EQ(opponentPlayer.GetHand()[0]->card->name, "Guardian of Kings");
 
-    GameAgent::RunTask(currentPlayer, PlayCardTask(taskAgent, card1));
-    EXPECT_EQ(currentPlayer.GetField()[0]->card->name, "Acidic Swamp Ooze");
-
-    GameAgent::RunTask(opponentPlayer, PlayCardTask(taskAgent, card2));
-    EXPECT_EQ(opponentPlayer.GetField()[0]->card->name, "Guardian of Kings");
+    GameAgent::RunTask(opponentPlayer, PlayCardTask(taskAgent, card1));
     EXPECT_EQ(opponentPlayer.GetHero()->maxHealth,
               opponentPlayer.GetHero()->health);
 }
