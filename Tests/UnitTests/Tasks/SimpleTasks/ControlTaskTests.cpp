3becdd205f2ec24554bfb61633eb49a54af92793
Chris Ohk <utilForever@gmail.com>
2018-12-26 17:23:05 +0900

    [WIP] test: Add unit test for ControlTask

--- Tests/UnitTests/Tasks/SimpleTasks/ControlTaskTests.cpp
+++ Tests/UnitTests/Tasks/SimpleTasks/ControlTaskTests.cpp
@@ -20,3 +20,30 @@ TEST(ControlTask, GetTaskID)
     const ControlTask control(EntityType::TARGET);
     EXPECT_EQ(control.GetTaskID(), +TaskID::CONTROL);
 }
+
+TEST(ControlTask, Run)
+{
+    GameAgent agent(CardClass::ROGUE, CardClass::DRUID, PlayerType::PLAYER1);
+    Player& player1 = agent.GetPlayer1();
+    Player& player2 = agent.GetPlayer2();
+
+    std::vector<Card> cards;
+    cards.reserve(5);
+
+    const std::string name = "test";
+    for (size_t i = 0; i < 5; ++i)
+    {
+        const auto id = static_cast<char>(i + 0x30);
+        cards.emplace_back(GenerateMinionCard(name + id, 1, 1));
+        PlayMinionCard(player1, cards[i]);
+        PlayMinionCard(player2, cards[i]);
+    }
+
+    ControlTask control(EntityType::TARGET);
+    control.SetTarget(player2.GetField()[0]);
+    MetaData result = control.Run(player1);
+
+    EXPECT_EQ(result, MetaData::CONTROL_SUCCESS);
+    EXPECT_EQ(player1.GetField().size(), 6u);
+    EXPECT_EQ(player2.GetField().size(), 4u);
+}
