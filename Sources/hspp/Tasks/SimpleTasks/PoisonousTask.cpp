a60a0392111f8ea29a184723989a0b9c98e61e9e
Chris Ohk <utilForever@gmail.com>
2019-01-16 23:56:56 +0900

    [ci skip] fix: Change the logic to destroy minion

--- Sources/hspp/Tasks/SimpleTasks/PoisonousTask.cpp
+++ Sources/hspp/Tasks/SimpleTasks/PoisonousTask.cpp
@@ -19,7 +19,7 @@ TaskID PoisonousTask::GetTaskID() const
 
 MetaData PoisonousTask::Impl(Player&)
 {
-    dynamic_cast<Character*>(m_target)->health = 0;
+    dynamic_cast<Character*>(m_target)->Destroy();
 
     return MetaData::POISONOUS_SUCCESS;
 }
