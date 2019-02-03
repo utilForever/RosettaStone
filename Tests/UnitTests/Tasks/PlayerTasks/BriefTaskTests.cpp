2e0fc5cc285412c31c6597e9295cfcdeca7d59b1
revsic <revsic99@gmail.com>
2019-02-04 00:27:02 +0900

    refactor: Replace GameAgent to Game

--- Tests/UnitTests/Tasks/PlayerTasks/BriefTaskTests.cpp
+++ Tests/UnitTests/Tasks/PlayerTasks/BriefTaskTests.cpp
@@ -6,7 +6,7 @@
 
 #include "gtest/gtest.h"
 
-#include <hspp/Managers/GameAgent.hpp>
+#include <hspp/Game/Game.hpp>
 #include <hspp/Tasks/PlayerTasks/BriefTask.hpp>
 
 using namespace Hearthstonepp;
@@ -21,17 +21,10 @@ TEST(BriefTask, GetTaskID)
 TEST(BriefTask, Run)
 {
     BriefTask brief;
-    GameAgent agent(CardClass::ROGUE, CardClass::DRUID, PlayerType::PLAYER1);
+    Game game(CardClass::ROGUE, CardClass::DRUID, PlayerType::PLAYER1);
 
-    agent.GetPlayer1().SetID(100);
+    game.GetPlayer1().SetID(100);
 
-    MetaData result = brief.Run(agent.GetPlayer1());
+    MetaData result = brief.Run(game.GetPlayer1());
     EXPECT_EQ(result, MetaData::BRIEF);
-
-    TaskMeta meta;
-    result = brief.Run(agent.GetPlayer1(), meta);
-    EXPECT_EQ(result, MetaData::BRIEF);
-    EXPECT_EQ(meta.GetID(), +TaskID::BRIEF);
-    EXPECT_EQ(meta.GetStatus(), MetaData::BRIEF);
-    EXPECT_EQ(meta.GetUserID(), agent.GetPlayer1().GetID());
 }
