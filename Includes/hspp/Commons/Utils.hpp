da723cd19fea0dfe1e3b262e59010ad398c22801
revsic <revsic99@gmail.com>
2019-02-05 08:35:37 +0900

    docs: Add doxygen comments for Utils::Box

--- Includes/hspp/Commons/Utils.hpp
+++ Includes/hspp/Commons/Utils.hpp
@@ -21,6 +21,13 @@ constexpr bool AllCondIsTrue(const T& t, Others const&... args)
     return (t == true) && AllCondIsTrue(args...);
 }
 
+//!
+//! \brief Box class.
+//!
+//! This class is unique_ptr like pointer wrapper.
+//! For std::any, base class required copy constructible, it supports both deep
+//! copy and move operations.
+//!
 template <typename T>
 class Box
 {
@@ -37,26 +44,34 @@ class Box
     using iterator = pointer;
     using const_iterator = const_pointer;
 
+    //! Default constructor.
     Box() : m_size(0), m_ptr(nullptr)
     {
         // Do nothing
     }
 
+    //! Constructs box with given /p size.
+    //! \param size The size of buffer.
     explicit Box(size_t size) : m_size(size), m_ptr(new T[m_size])
     {
         // Do nothing
     }
 
+    //! Constructs box with given \p ptr and \p size.
+    //! \param ptr The pointer that box managing.
+    //! \param size The size of buffer.
     Box(T* ptr, size_t size) : m_size(size), m_ptr(ptr)
     {
         // Do nothing
     }
 
+    //! Default destructor.
     ~Box()
     {
         reset();
     }
 
+    //! Copy constructor, deep copying buffer.
     Box(const Box& box) : m_size(box.size()), m_ptr(new T[m_size])
     {
         for (size_t i = 0; i < m_size; ++i)
@@ -65,12 +80,14 @@ class Box
         }
     }
 
+    //! Move constructor.
     Box(Box&& box) noexcept : m_size(box.size()), m_ptr(box.get())
     {
         box.m_ptr = nullptr;
         box.m_size = 0;
     }
 
+    //! Copy assignment operator, deep copying buffer.
     Box& operator=(const Box& box)
     {
         reset(new T[box.size()], box.size());
@@ -81,6 +98,7 @@ class Box
         return *this;
     }
 
+    //! Move assignment operator.
     Box& operator=(Box&& box) noexcept
     {
         reset(box.get(), box.size());
@@ -91,6 +109,7 @@ class Box
         return *this;
     }
 
+    //! Resets buffer.
     void reset() noexcept
     {
         if (m_ptr != nullptr)
@@ -102,6 +121,9 @@ class Box
         m_size = 0;
     }
 
+    //! Resets buffer with given \p other and \p size.
+    //! \param other The pointer that box managing.
+    //! \param size The size of buffer.
     void reset(T* other, size_t size) noexcept
     {
         if (m_ptr != nullptr)
@@ -113,46 +135,66 @@ class Box
         m_size = size;
     }
 
+    //! Queries that object has buffer.
     operator bool()
     {
         return m_ptr != nullptr;
     }
 
+    //! Gets buffer pointer.
+    //! \return The pointer of buffer.
     T* get()
     {
         return m_ptr;
     }
 
+    //! Gets buffer pointer.
+    //! \return The pointer of buffer.
     const T* get() const
     {
         return m_ptr;
     }
 
+    //! Gets size of buffer.
+    //! \return The size of buffer.
     size_t size() const
     {
         return m_size;
     }
 
+    //! Gets the first element of buffer.
+    //! \return The first element of buffer.
     T& operator*()
     {
         return *m_ptr;
     }
 
+    //! Gets the first element of buffer.
+    //! \return The first element of buffer.
     const T& operator*() const
     {
         return *m_ptr;
     }
 
+    //! Gets the element with given \p idx from buffer.
+    //! \param idx The index of buffer.
+    //! \return The index'th element of buffer.
     T& operator[](size_t idx)
     {
         return m_ptr[idx];
     }
 
+    //! Gets the element with given \p idx from buffer.
+    //! \param idx The index of buffer.
+    //! \return The index'th element of buffer.
     const T& operator[](size_t idx) const
     {
         return m_ptr[idx];
     }
 
+    //! Check the equality of two buffers.
+    //! \param other Comparable object.
+    //! \return Equality about two buffers.
     bool operator==(const Box& other) const
     {
         if (m_size != other.size())
@@ -171,46 +213,65 @@ class Box
         return true;
     }
 
+    //! Check the inequality of two buffers.
+    //! \param other Comparable object.
+    //! \return Inequality about two buffers.
     bool operator!=(const Box& other) const
     {
         return !operator==(other);
     }
 
+    //! Check if the buffer is empty.
+    //! \return true if buffer is empty, false otherwise.
     bool operator==(std::nullptr_t) const
     {
         return m_ptr == nullptr;
     }
 
+    //! Check if the buffer is not empty.
+    //! \return true if buffer is not empty, false otherwise.
     bool operator!=(std::nullptr_t) const
     {
         return m_ptr != nullptr;
     }
 
+    //! Gets begining iterator of the buffer.
+    //! \return The begining iterator.
     iterator begin()
     {
         return m_ptr;
     }
 
+    //! Gets constant begining iterator of the buffer.
+    //! \return The constant begining iterator.
     const_iterator begin() const
     {
         return m_ptr;
     }
 
+    //! Gets constant begining iterator of the buffer.
+    //! \return The constant begining iterator.
     const_iterator cbegin() const
     {
         return m_ptr;
     }
 
+    //! Gets ending iterator of the buffer.
+    //! \return The ending iterator.
     iterator end()
     {
         return m_ptr + m_size;
     }
 
+    //! Gets ending iterator of the buffer.
+    //! \return The ending iterator.
     const_iterator end() const
     {
         return m_ptr + m_size;
     }
 
+    //! Gets ending iterator of the buffer.
+    //! \return The ending iterator.
     const_iterator cend() const
     {
         return m_ptr + m_size;
