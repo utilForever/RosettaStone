ce19b5c1556b89a4d8b5b28e56960fb9807d7184
revsic <revsic99@gmail.com>
2019-02-05 08:51:36 +0900

    docs: Add doxygen type documents

--- Includes/hspp/Policy/BasicPolicy.hpp
+++ Includes/hspp/Policy/BasicPolicy.hpp
@@ -13,26 +13,51 @@
 
 namespace Hearthstonepp
 {
+//!
+//! \brief BasicPolicy class.
+//!
+//! The purpose of this class is easier implementation of IPolicy for GameAgent.
+//!
 class BasicPolicy : public IPolicy
 {
  public:
+    //! Default constructor.
     BasicPolicy() = default;
 
+    //! Default destructor.
     ~BasicPolicy() = default;
 
+    //! Gets next behavior from given \p game.
+    //! \param game The current game status.
+    //! \return The behavior serialized as TaskMeta.
     TaskMeta Next(const Game& game) override;
 
+    //! Gets proper requirement with given \p player and \p id.
+    //! \param player The player who requests the requirement.
+    //! \param id The requested TaskID.
     TaskMeta Require(Player& player, TaskID id) override;
 
+    //! Notify serialized data to IPolicy.
+    //! \param meta The serialized data.
     void Notify(const TaskMeta& meta) override;
 
  private:
+    //! Virtual method for MulliganTask requirement.
     virtual TaskMeta RequireMulligan(Player& player);
+
+    //! Virtual method for PlaySpellTask requirement.
     virtual TaskMeta RequirePlaySpell(Player& player);
+
+    //! Virtual method for PlayCardTask requirement.
     virtual TaskMeta RequirePlayCard(Player& player);
+
+    //! Virtual method for PlayMinionTask requirement.
     virtual TaskMeta RequirePlayMinion(Player& player);
+
+    //! Virtual method for CombatTask requirement.
     virtual TaskMeta RequireCombat(Player& player);
 
+    //! Virtual method for OverDraw notifying.
     virtual void NotifyOverDraw(const TaskMeta& meta);
 
     std::map<TaskID, std::function<TaskMeta(BasicPolicy&, Player&)>> m_require =
