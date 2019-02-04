503dde258d9d94d3e10f74fdac76e3e80386ea4a
revsic <revsic99@gmail.com>
2019-02-05 08:45:59 +0900

    docs: Add doxygen format docs for IPolicy

--- Includes/hspp/Policy/Policy.hpp
+++ Includes/hspp/Policy/Policy.hpp
@@ -14,17 +14,32 @@ namespace Hearthstonepp
 {
 class Game;
 
+//!
+//! \brief IPolicy class.
+//!
+//! This class is for abstracting game playing behavior.
+//!
 class IPolicy
 {
  public:
+    //! Default constructor.
     IPolicy() = default;
 
+    //! Default virtual destructor.
     virtual ~IPolicy() = default;
 
+    //! Gets next behavior from given \p game.
+    //! \param game The current game status.
+    //! \return The behavior serialized as TaskMeta.
     virtual TaskMeta Next(const Game& game) = 0;
 
+    //! Gets proper requirement with given \p player and \p id.
+    //! \param player The player who requests the requirement.
+    //! \param id The requested TaskID.
     virtual TaskMeta Require(Player& player, TaskID id) = 0;
 
+    //! Notify serialized data to IPolicy.
+    //! \param meta The serialized data.
     virtual void Notify(const TaskMeta& meta) = 0;
 };
 }  // namespace Hearthstonepp
