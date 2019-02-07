ccd82b0df3a08f3d609b7c25fd567b0e6b5192f5
Chris Ohk <utilForever@gmail.com>
2019-02-07 16:30:45 +0900

    [ci skip] [WIP] feat(dev_policy): Update branch from 'dev_policy'

---
+++ Includes/hspp/Models/Game.hpp
@@ -0,0 +1,85 @@
+// Copyright (c) 2018 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef HEARTHSTONEPP_GAME_HPP
+#define HEARTHSTONEPP_GAME_HPP
+
+#include <hspp/Accounts/Player.hpp>
+#include <hspp/Commons/Constants.hpp>
+#include <hspp/Enums/CardEnums.hpp>
+#include <hspp/Policy/Policy.hpp>
+
+namespace Hearthstonepp
+{
+//!
+//! \brief Game class.
+//!
+//! This class stores Hearthstone game states which consists of informations of
+//! both players.
+//!
+class Game
+{
+ public:
+    //! Constructs account with given \p p1Class, \p p2Class and \p firstPlayer.
+    //! \param p1Class The class of player 1.
+    //! \param p2Class The class of player 2.
+    //! \param firstPlayer The first player who starts turn first.
+    Game(CardClass p1Class, CardClass p2Class,
+         PlayerType firstPlayer = PlayerType::PLAYER1);
+
+    //! Copy Constructor
+    Game(const Game&) = delete;
+
+    //! Move Constructor
+    Game(Game&&) = delete;
+
+    //! Copy assignment
+    Game& operator=(const Game&) = delete;
+
+    //! Move assignment
+    Game& operator=(Game&&) = delete;
+
+    //! Returns the first player.
+    //! \return The first player.
+    Player& GetPlayer1();
+
+    //! Returns the second player.
+    //! \return The second player.
+    Player& GetPlayer2();
+
+    //! Returns the first player.
+    //! \return The first player.
+    Player& GetFirstPlayer();
+
+    //! Sets the first player.
+    //! \param playerType The first player who starts turn first.
+    void SetFirstPlayer(PlayerType playerType);
+
+    //! Returns the player controlling the current turn.
+    //! \return The player controlling the current turn.
+    Player& GetCurrentPlayer();
+
+    //! Sets the player controlling the current turn.
+    //! \param playerType The player controlling the current turn.
+    void SetCurrentPlayer(PlayerType playerType);
+
+    //! Returns the opponent player.
+    //! \return The opponent player.
+    Player& GetOpponentPlayer();
+
+    //! Flip current player, player1 to player2 or player2 to player1.
+    void FlipCurrentPlayer();
+
+ private:
+    Player m_player1;
+    Player m_player2;
+
+    PlayerType m_firstPlayer;
+    PlayerType m_currentPlayer;
+};
+}  // namespace Hearthstonepp
+
+#endif  // HEARTHSTONEPP_GAME_HPP
---
+++ Includes/hspp/Models/GameAgent.hpp
@@ -0,0 +1,94 @@
+// Copyright (c) 2018 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef HEARTHSTONEPP_GAME_AGENT_HPP
+#define HEARTHSTONEPP_GAME_AGENT_HPP
+
+#include <hspp/Accounts/Player.hpp>
+#include <hspp/Commons/Constants.hpp>
+#include <hspp/Models/Game.hpp>
+#include <hspp/Tasks/Tasks.hpp>
+
+#include <array>
+#include <functional>
+#include <thread>
+
+namespace Hearthstonepp
+{
+struct GameResult
+{
+};
+
+//!
+//! \brief GameAgent class.
+//!
+//! This class manages Hearthstone game and saves information of both players
+//! playing the game. Also, it stores task agent manages the task.
+//!
+class GameAgent
+{
+ public:
+    //! Constructs game with given \p p1Class, \p p2Class and \p firstPlayer.
+    //! \param p1Class The class of player 1.
+    //! \param p2Class The class of player 2.
+    //! \param p1Policy The game playing policy for player1.
+    //! \param p2Policy The game playing policy for player2.
+    //! \param firstPlayer The first player who starts turn first.
+    GameAgent(CardClass p1Class, CardClass p2Class, IPolicy* p1Policy,
+              IPolicy* p2Policy, PlayerType firstPlayer = PlayerType::PLAYER1);
+
+    //! Starts the game agent.
+    //! \return The thread that plays the game.
+    GameResult Start();
+
+    //! Get game structure.
+    //! \return The game structure.
+    Game& GetGame();
+
+    //! Get game structure.
+    //! \return The game structure.
+    const Game& GetGame() const;
+
+ private:
+    //! Returns whether the game is over.
+    //! \return true if the game is over, and false otherwise.
+    bool IsGameOver();
+
+    //! Processes the begin phase of the game.
+    void BeginPhase();
+
+    //! Processes the main phase of the game.
+    //! \return true if the game is over, and false otherwise.
+    bool MainPhase();
+
+    //! Processes the final phase of the game.
+    void FinalPhase();
+
+    //! Prepares to handle the main phase: draw card, modify mana and clear
+    //! attacked characters.
+    void PrepareMainPhase();
+
+    //! Processes command in main menu and calls action.
+    //! \return return true if the game is over, and false otherwise.
+    bool ProcessMainMenu();
+
+    //! Plays card such as minion, weapon and spell.
+    void PlayCard();
+
+    //! Combats with other minion or hero.
+    void Combat();
+
+    Game m_game;
+
+    std::array<std::function<void(GameAgent&)>, GAME_MAIN_MENU_SIZE - 1>
+        m_mainMenuFuncs = {
+            &GameAgent::PlayCard,
+            &GameAgent::Combat,
+        };
+};
+}  // namespace Hearthstonepp
+
+#endif  // HEARTHSTONEPP_GAME_AGENT_HPP
