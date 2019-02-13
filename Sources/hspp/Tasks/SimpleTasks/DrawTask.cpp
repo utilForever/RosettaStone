160c4baf81d45fea6177d9e131cff83764cd06bb
Chris Ohk <utilForever@gmail.com>
2019-02-13 21:48:58 +0900

    [ci skip] refactor: Change logic to call Draw::DrawCard() method

--- Sources/hspp/Tasks/SimpleTasks/DrawTask.cpp
+++ Sources/hspp/Tasks/SimpleTasks/DrawTask.cpp
@@ -3,12 +3,8 @@
 // Hearthstone++ is hearthstone simulator using C++ with reinforcement learning.
 // Copyright (c) 2018 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
 
-#include <hspp/Commons/Constants.hpp>
-#include <hspp/Models/Entity.hpp>
-#include <hspp/Policies/Policy.hpp>
+#include <hspp/Actions/Draw.hpp>
 #include <hspp/Tasks/SimpleTasks/DrawTask.hpp>
-
-#include <utility>
 
 namespace Hearthstonepp::SimpleTasks
 {
@@ -24,72 +20,74 @@ TaskID DrawTask::GetTaskID() const
 
 TaskStatus DrawTask::Impl(Player& player)
 {
-    size_t num = m_num;
-    TaskStatus result = TaskStatus::DRAW_SUCCESS;
-
-    auto& deck = player.GetDeck();
-    auto& hand = player.GetHand();
-
-    // After reaching fatigue
-    if (deck.GetNumOfCards() < num)
+    for (std::size_t i = 0; i < m_num; ++i)
     {
-        const size_t numDrawAfterFatigue = num - deck.GetNumOfCards();
-
-        // Sigma (i = 1 to numDrawAfterFatigue) { current.exhausted + i }
-        const auto fatigueDamage = static_cast<int>(
-            player.GetNumCardAfterExhaust() * numDrawAfterFatigue +
-            numDrawAfterFatigue * (numDrawAfterFatigue + 1) / 2);
-
-        player.GetHero()->health = player.GetHero()->health - fatigueDamage;
-        player.SetNumCardAfterExhaust(static_cast<BYTE>(
-            player.GetNumCardAfterExhaust() + numDrawAfterFatigue));
-
-        num = deck.GetNumOfCards();
-        result = TaskStatus::DRAW_EXHAUST;
+        Draw::DrawCard(player, Card{});
     }
 
-    // When hand size over HAND_SIZE, overdraw
-    if (hand.GetNumOfCards() + num > HAND_SIZE)
-    {
-        // The number of overdraw
-        const size_t over = hand.GetNumOfCards() + num - HAND_SIZE;
+    // auto& deck = player.GetDeck();
+    // auto& hand = player.GetHand();
 
-        std::vector<Entity*> burnt;
-        burnt.reserve(over);
+    //// After reaching fatigue
+    // if (deck.GetNumOfCards() < num)
+    //{
+    //    const size_t numDrawAfterFatigue = num - deck.GetNumOfCards();
 
-        // Draw burnt card
-        for (size_t i = 0; i < over; ++i)
-        {
-            //burnt[i] = deck.back();
-            //deck.pop_back();
-        }
+    //    // Sigma (i = 1 to numDrawAfterFatigue) { current.exhausted + i }
+    //    const auto fatigueDamage = static_cast<int>(
+    //        player.GetNumCardAfterExhaust() * numDrawAfterFatigue +
+    //        numDrawAfterFatigue * (numDrawAfterFatigue + 1) / 2);
 
-        num = HAND_SIZE - hand.GetNumOfCards();
+    //    player.GetHero()->health = player.GetHero()->health - fatigueDamage;
+    //    player.SetNumCardAfterExhaust(static_cast<BYTE>(
+    //        player.GetNumCardAfterExhaust() + numDrawAfterFatigue));
 
-        if (result == TaskStatus::DRAW_EXHAUST)
-        {
-            result = TaskStatus::DRAW_EXHAUST_OVERDRAW;
-        }
-        else
-        {
-            result = TaskStatus::DRAW_OVERDRAW;
-        }
+    //    num = deck.GetNumOfCards();
+    //    result = TaskStatus::DRAW_EXHAUST;
+    //}
 
-        // Send burnt cards to policy
-        TaskMetaTrait trait(TaskID::OVERDRAW, TaskStatus::DRAW_OVERDRAW,
-                            player.GetID());
-        player.GetPolicy().Notify(TaskMeta(trait, std::move(burnt)));
-    }
+    //// When hand size over HAND_SIZE, overdraw
+    // if (hand.GetNumOfCards() + num > HAND_SIZE)
+    //{
+    //    // The number of overdraw
+    //    const size_t over = hand.GetNumOfCards() + num - HAND_SIZE;
 
-    // Draw success
-    for (size_t i = 0; i < num; ++i)
-    {
-        //const auto entity = deck.back();
-        //const auto entityClone = new Entity(*entity);
-        //hand.AddCard(*entityClone);
-        //deck.pop_back();
-    }
+    //    std::vector<Entity*> burnt;
+    //    burnt.reserve(over);
 
-    return result;
+    //    // Draw burnt card
+    //    for (size_t i = 0; i < over; ++i)
+    //    {
+    //        //burnt[i] = deck.back();
+    //        //deck.pop_back();
+    //    }
+
+    //    num = HAND_SIZE - hand.GetNumOfCards();
+
+    //    if (result == TaskStatus::DRAW_EXHAUST)
+    //    {
+    //        result = TaskStatus::DRAW_EXHAUST_OVERDRAW;
+    //    }
+    //    else
+    //    {
+    //        result = TaskStatus::DRAW_OVERDRAW;
+    //    }
+
+    //    // Send burnt cards to policy
+    //    TaskMetaTrait trait(TaskID::OVERDRAW, TaskStatus::DRAW_OVERDRAW,
+    //                        player.GetID());
+    //    player.GetPolicy().Notify(TaskMeta(trait, std::move(burnt)));
+    //}
+
+    //// Draw success
+    // for (size_t i = 0; i < num; ++i)
+    //{
+    //    //const auto entity = deck.back();
+    //    //const auto entityClone = new Entity(*entity);
+    //    //hand.AddCard(*entityClone);
+    //    //deck.pop_back();
+    //}
+
+    return TaskStatus::DRAW_SUCCESS;
 }
 }  // namespace Hearthstonepp::SimpleTasks
