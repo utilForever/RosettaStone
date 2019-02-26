64d7188ddcc3935960db8477e1edd05aaccb0eeb
Chris Ohk <utilForever@gmail.com>
2019-02-27 02:02:37 +0900

    fix: Add missing brew commands (bash) and change incorrect remove path

--- Scripts/travis_build_codecov.sh
+++ Scripts/travis_build_codecov.sh
@@ -5,6 +5,7 @@ set -e
 export NUM_JOBS=1
 
 brew update;
+brew install bash;
 brew install lcov;
 
 mkdir build
@@ -15,7 +16,7 @@ lcov -c -i -d Tests/UnitTests -o base.info
 bin/UnitTests
 lcov -c -d Tests/UnitTests -o test.info
 lcov -a base.info -a test.info -o coverage.info
-lcov -r coverage.info '/usr/*' -o coverage.info
+lcov -r coverage.info '*/Toolchains/*' -o coverage.info
 lcov -r coverage.info '*/Libraries/*' -o coverage.info
 lcov -r coverage.info '*/Programs/*' -o coverage.info
 lcov -r coverage.info '*/Tests/*' -o coverage.info
