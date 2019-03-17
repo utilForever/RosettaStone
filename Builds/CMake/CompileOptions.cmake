cdf0f0da2209a19b6de6464febb014e412b120e9
revsic <revsic99@gmail.com>
2019-03-18 02:23:57 +0900

    fix: Fix compile option cubbyflow to hearthstonepp

--- Builds/CMake/CompileOptions.cmake
+++ Builds/CMake/CompileOptions.cmake
@@ -3,8 +3,8 @@
 #
 
 # Set warnings as errors flag
-option(CUBBYFLOW_WARNINGS_AS_ERRORS "Treat all warnings as errors" ON)
-if(CUBBYFLOW_WARNINGS_AS_ERRORS)
+option(HEARTHSTONEPP_WARNINGS_AS_ERRORS "Treat all warnings as errors" ON)
+if(HEARTHSTONEPP_WARNINGS_AS_ERRORS)
 	if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 		set(WARN_AS_ERROR_FLAGS	"/WX")
 	else()
