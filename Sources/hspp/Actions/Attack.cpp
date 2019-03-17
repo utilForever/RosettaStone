61b0ba24f6b2c30f24267cb1f41bbcc0fe0ac869
Chris Ohk <utilForever@gmail.com>
2019-03-18 01:42:16 +0900

    [ci skip] feat(game-state): Copy code from CombatTask

--- Sources/hspp/Actions/Attack.cpp
+++ Sources/hspp/Actions/Attack.cpp
@@ -4,13 +4,96 @@
 // Copyright (c) 2018 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
 
 #include <hspp/Actions/Attack.hpp>
+#include <hspp/Tasks/SimpleTasks/DestroyTask.hpp>
+#include <hspp/Tasks/SimpleTasks/FreezeTask.hpp>
+#include <hspp/Tasks/SimpleTasks/PoisonousTask.hpp>
 
 namespace Hearthstonepp::Generic
 {
 void Attack(Player& player, Character* source, Character* target)
 {
-    (void)player;
-    (void)source;
-    (void)target;
+    if (!source->CanAttack() ||
+        !source->IsValidCombatTarget(player.GetOpponent(), target))
+    {
+        return;
+    }
+
+    const std::size_t targetAttack = target->GetAttack();
+    const std::size_t sourceAttack = source->GetAttack();
+
+    const std::size_t targetDamage = target->TakeDamage(*source, sourceAttack);
+    const bool isTargetDamaged = targetDamage > 0;
+
+    // Destroy target if attacker is poisonous
+    if (isTargetDamaged && source->GetGameTag(GameTag::POISONOUS) == 1)
+    {
+        SimpleTasks::PoisonousTask(target).Run(player);
+    }
+
+    // Freeze target if attacker is freezer
+    if (isTargetDamaged && source->GetGameTag(GameTag::FREEZE) == 1)
+    {
+        SimpleTasks::FreezeTask(EntityType::TARGET, target).Run(player);
+    }
+
+    // Ignore damage from defenders with 0 attack
+    if (targetAttack > 0)
+    {
+        const std::size_t sourceDamage =
+            source->TakeDamage(*target, targetAttack);
+        const bool isSourceDamaged = sourceDamage > 0;
+
+        // Destroy source if defender is poisonous
+        if (isSourceDamaged && target->GetGameTag(GameTag::POISONOUS) == 1)
+        {
+            SimpleTasks::PoisonousTask(source).Run(player);
+        }
+
+        // Freeze source if defender is freezer
+        if (isSourceDamaged && target->GetGameTag(GameTag::FREEZE) == 1)
+        {
+            SimpleTasks::FreezeTask(EntityType::SOURCE, source).Run(player);
+        }
+    }
+
+    // Remove stealth ability if attacker has it
+    if (source->GetGameTag(GameTag::STEALTH) == 1)
+    {
+        source->SetGameTag(GameTag::STEALTH, 0);
+    }
+
+    // Remove durability from weapon if hero attack
+    const Hero* hero = dynamic_cast<Hero*>(source);
+    if (hero != nullptr && hero->weapon != nullptr &&
+        hero->weapon->GetGameTag(GameTag::IMMUNE) == 0)
+    {
+        hero->weapon->durability -= 1;
+
+        // Destroy weapon if durability is 0
+        if (hero->weapon->durability == 0)
+        {
+            SimpleTasks::DestroyTask(EntityType::WEAPON).Run(player);
+        }
+    }
+
+    source->numAttacked++;
+
+    if ((source->numAttacked >= 1 &&
+         source->GetGameTag(GameTag::WINDFURY) == 0) ||
+        (source->numAttacked >= 2 &&
+         source->GetGameTag(GameTag::WINDFURY) == 1))
+    {
+        source->SetGameTag(GameTag::EXHAUSTED, 1);
+    }
+
+    if (source->isDestroyed)
+    {
+        source->Destroy();
+    }
+
+    if (target->isDestroyed)
+    {
+        target->Destroy();
+    }
 }
 }  // namespace Hearthstonepp::Generic
