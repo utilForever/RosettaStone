f4025b8990103e1247983770dd216d80c28ac0e5
Chris Ohk <utilForever@gmail.com>
2019-03-17 23:53:48 +0900

    [ci skip] feat(game-state): Add ChoiceMulligan() function
    - Choices cards in mulligan state

--- Includes/hspp/Actions/Choose.hpp
+++ Includes/hspp/Actions/Choose.hpp
@@ -12,6 +12,11 @@
 
 namespace Hearthstonepp::Generic
 {
+//! Choices cards in mulligan state.
+//! \param player The player to create a choice cards.
+//! \param choices A container of card to choose.
+void ChoiceMulligan(Player& player, std::vector<std::size_t> choices);
+
 //! Creates a choice for player.
 //! \param player The player to create a choice.
 //! \param type A choice type.
