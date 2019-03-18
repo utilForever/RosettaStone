1a36f3453814e72971316c2ef2404ca5dbc55149
revsic <revsic99@gmail.com>
2019-03-18 16:57:44 +0900

    feat(ModuleBase): Add torch module base for hspp policy

---
+++ Extensions/HSppTorch/Includes/hspp_torch/ModuleBase.hpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2018 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef HSPP_TORCH_MODULE_BASE_HPP
+#define HSPP_TORCH_MODULE_BASE_HPP
+
+#include <torch/torch.h>
+
+namespace HSppTorch
+{
+class ModuleBase : public torch::nn::Module
+{
+ public:
+ private:
+};
+}  // namespace HSppTorch
+
+#endif
