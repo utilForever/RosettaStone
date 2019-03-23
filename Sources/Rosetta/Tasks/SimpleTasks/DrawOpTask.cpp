d6fd73677e08192599291ffb846dbd8a8861981c
Hankyul <hyunhankyul@gmail.com>
2019-03-23 23:50:15 +0900

    feat(card-impl): Create DrawOp Task class

---
+++ Sources/Rosetta/Tasks/SimpleTasks/DrawOpTask.cpp
@@ -0,0 +1,30 @@
+// This code is based on Sabberstone project.
+// Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+// Hearthstone++ is hearthstone simulator using C++ with reinforcement learning.
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+#include <Rosetta/Actions/Draw.hpp>
+#include <Rosetta/Tasks/SimpleTasks/DrawOpTask.hpp>
+
+namespace RosettaStone::SimpleTasks
+{
+DrawOpTask::DrawOpTask(std::size_t num) : m_num(num)
+{
+    // Do nothing
+}
+
+TaskID DrawOpTask::GetTaskID() const
+{
+    return TaskID::DRAW;
+}
+
+TaskStatus DrawOpTask::Impl(Player& player)
+{
+    for (std::size_t i = 0; i < m_num; ++i)
+    {
+        Generic::Draw(player.GetOpponent(), std::nullopt);
+    }
+
+    return TaskStatus::COMPLETE;
+}
+}  // namespace RosettaStone::SimpleTasks
