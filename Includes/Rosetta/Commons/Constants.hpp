2758da84f5efb09bf330df5d11d1bf57d0573b45
Chris Ohk <utilForever@gmail.com>
2019-03-24 21:23:55 +0900

    refactor: Update project version (0.3 -> 0.4)

--- Includes/Rosetta/Commons/Constants.hpp
+++ Includes/Rosetta/Commons/Constants.hpp
@@ -12,7 +12,7 @@
 namespace RosettaStone
 {
 //! RosettaStone version.
-constexpr float VERSION = 0.3f;
+constexpr float VERSION = 0.4f;
 
 //! The number of player class.
 //! \note Druid, Hunter, Mage, Paladin, Priest, Rogue, Shaman, Warlock, Warrior
