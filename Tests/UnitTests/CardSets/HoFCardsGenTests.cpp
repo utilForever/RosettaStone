23b4f67316406c8626a96a826c2740a066e4894a
Junhyeong Bae <bjh6681@outlook.kr>
2019-04-11 03:23:11 +0900

    feat(card_impl):Add implement card 'Sylvanas Windrunner' (EX1_016)

--- Tests/UnitTests/CardSets/HoFCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/HoFCardsGenTests.cpp
@@ -8,6 +8,8 @@
 
 #include <Rosetta/Actions/Draw.hpp>
 #include <Rosetta/Cards/Cards.hpp>
+#include <Rosetta/Models/Entity.hpp>
+#include <effolkronium/random.hpp>
 
 using namespace RosettaStone;
 using namespace PlayerTasks;
@@ -40,6 +42,39 @@ TEST(HoFCardsGen, EX1_050)
     Task::Run(curPlayer, PlayCardTask::Minion(curPlayer, card));
     EXPECT_EQ(curPlayer.GetHand().GetNumOfCards(), 6u);
     EXPECT_EQ(opPlayer.GetHand().GetNumOfCards(), 7u);
+}
+
+TEST(HoFCardsGen, EX1_016)
+{
+    GameConfig config;
+    config.player1Class = CardClass::DRUID;
+    config.player2Class = CardClass::MAGE;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+
+	Game game(config);
+    game.StartGame();
+
+	Player& player1 = game.GetCurrentPlayer();
+    Player& player2 = game.GetCurrentPlayer().GetOpponent();
+    player1.maximumMana = 6;
+    player1.currentMana = 6;
+    player2.maximumMana = 10;
+    player2.currentMana = 10;
+
+    const auto sylvanas = Generic::DrawCard(
+        player1, Cards::GetInstance().FindCardByName("Sylvanas Windrunner"));
+    Task::Run(player1, PlayCardTask::Minion(player1,sylvanas));
+    EXPECT_FALSE(player1.currentMana);
+    Task::Run(player1, EndTurnTask());
+    const auto attackerCard =
+        Generic::DrawCard(player2, Cards::GetInstance().FindCardByName("Magma Rager"));
+    const auto beEffectedBySylvanas = Generic::DrawCard(
+        player2, Cards::GetInstance().FindCardByName("Oasis Snapjaw"));
+    Task::Run(player2, PlayCardTask::Minion(player2, attackerCard));
+    Task::Run(player2, AttackTask(attackerCard, sylvanas));
+	
+	EXPECT_EQ(beEffectedBySylvanas->GetOwner().GetID(), player1.GetID());
 }
 
 TEST(HoFCardsGen, EX1_310)
