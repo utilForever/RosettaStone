e086c94d68af68e66efe5e730d3338d89172bb67
revsic <revsic99@gmail.com>
2019-04-12 00:32:01 +0900

    fix: Remove action encoder

--- Extensions/RosettaTorch/Includes/RosettaTorch/TorchPolicy.hpp
+++ Extensions/RosettaTorch/Includes/RosettaTorch/TorchPolicy.hpp
@@ -14,51 +14,6 @@
 
 namespace RosettaTorch
 {
-using namespace RosettaStone;
-
-//!
-//! \brief ActionEncoder struct.
-//!
-//! This struct generates torch tensor from ActionEncode.
-//!
-struct ActionEncoder
-{
-    static constexpr size_t TaskIDSize = 4;
-
-    static constexpr TaskID TaskIDs[TaskIDSize] = {
-        TaskID::MULLIGAN, TaskID::END_TURN, TaskID::ATTACK, TaskID::PLAY_CARD
-    };
-
-    /////////////////////////////////////////////
-    // My hand, my field, your field
-    static constexpr size_t TargetPlaceSize = 3;
-
-    static constexpr size_t CurrentHandOffset = 0;
-
-    static constexpr size_t CurrentFieldOffset = 1;
-
-    static constexpr size_t OpponentFieldOffset = 2;
-
-    /////////////////////////////////////////////
-    static constexpr size_t MaximumFieldSize = 10;
-
-    static constexpr size_t EntityEncodeSize =
-        TargetPlaceSize + MaximumFieldSize;
-
-    /////////////////////////////////////////////
-    static constexpr size_t NumSource = 1;
-
-    static constexpr size_t NumTarget = Generic::ActionEncode::NumTarget;
-
-    /////////////////////////////////////////////
-    static constexpr size_t ActionTensorSize =
-        TaskIDSize + EntityEncodeSize * (NumSource + NumTarget);
-
-    //! Convert ActionEncode to torch::Tensor.
-    static torch::Tensor ActionToTensor(const Game& game,
-                                        const Generic::ActionEncode& action);
-};
-
 //!
 //! \brief TorchPolicy class.
 //!
--- Extensions/RosettaTorch/Sources/RosettaTorch/TorchPolicy.cpp
+++ Extensions/RosettaTorch/Sources/RosettaTorch/TorchPolicy.cpp
@@ -6,74 +6,11 @@
 
 #include <Rosetta/Commons/Constants.hpp>
 #include <Rosetta/Commons/Utils.hpp>
+#include <RosettaTorch/ActionEncoder.hpp>
 #include <RosettaTorch/TorchPolicy.hpp>
 
 namespace RosettaTorch
 {
-torch::Tensor ActionEncoder::ActionToTensor(const Game& game,
-                                            const Generic::ActionEncode& action)
-{
-    torch::Tensor tensor = torch::empty(ActionTensorSize, torch::kInt8);
-    for (size_t i = 0; i < TaskIDSize; ++i)
-    {
-        if (action.taskID == TaskIDs[i])
-        {
-            tensor[i] = 1;
-        }
-        else
-        {
-            tensor[i] = 0;
-        }
-    }
-
-    auto fill_zero = [&](size_t start) {
-        for (size_t i = 0; i < EntityEncodeSize; ++i)
-        {
-            tensor[start + i] = 0;
-        }
-    };
-
-    auto write = [&](size_t start, Entity* entity) {
-        fill_zero(start);
-        if (entity == nullptr)
-        {
-            return;
-        }
-
-        Player& player = game.GetCurrentPlayer();
-        if (auto pos = player.GetHand().FindCardPos(*entity); pos.has_value())
-        {
-            tensor[start + CurrentHandOffset] = 1;
-            tensor[start + TargetPlaceSize + pos.value()] = 1;
-        }
-        else if (Minion* minion = dynamic_cast<Minion*>(entity);
-                 minion != nullptr)
-        {
-            if (auto pos = player.GetField().FindMinionPos(*minion);
-                pos.has_value())
-            {
-                tensor[start + CurrentFieldOffset] = 1;
-                tensor[start + TargetPlaceSize + pos.value()] = 1;
-            }
-            else if (auto pos =
-                         game.GetOpponentPlayer().GetField().FindMinionPos(
-                             *minion);
-                     pos.has_value())
-            {
-                tensor[start + OpponentFieldOffset] = 1;
-                tensor[start + TargetPlaceSize + pos.value()] = 1;
-            }
-        }
-    };
-
-    write(TaskIDSize, action.source);
-    for (size_t i = 0; i < NumTarget; ++i)
-    {
-        write(TaskIDSize + (i + 1) * EntityEncodeSize, action.target[i]);
-    }
-    return tensor;
-}
-
 TorchPolicy::TorchPolicy(std::shared_ptr<ModuleBase> module,
                          GameToVec* gameToVec)
     : m_module(module), m_gameToVec(gameToVec)
