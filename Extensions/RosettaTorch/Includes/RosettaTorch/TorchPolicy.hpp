b85433668ad9966329cf1171b5d1968e9fd35e93
revsic <revsic99@gmail.com>
2019-04-11 22:05:50 +0900

    feat(TorchPolicy): Add action encoder

--- Extensions/RosettaTorch/Includes/RosettaTorch/TorchPolicy.hpp
+++ Extensions/RosettaTorch/Includes/RosettaTorch/TorchPolicy.hpp
@@ -7,12 +7,57 @@
 #ifndef ROSETTASTONE_TORCH_TORCH_POLICY_HPP
 #define ROSETTASTONE_TORCH_TORCH_POLICY_HPP
 
+#include <Rosetta/Actions/AvailableActions.hpp>
 #include <Rosetta/Policies/BasicPolicy.hpp>
+#include <RosettaTorch/GameToVec.hpp>
 #include <RosettaTorch/ModuleBase.hpp>
 
 namespace RosettaTorch
 {
 using namespace RosettaStone;
+
+//!
+//! \brief ActionEncoder struct.
+//!
+//! This struct generates torch tensor from ActionEncode.
+//!
+struct ActionEncoder
+{
+    static constexpr size_t TaskIDSize = 4;
+
+    static constexpr TaskID TaskIDs[TaskIDSize] = {
+        TaskID::MULLIGAN, TaskID::END_TURN, TaskID::ATTACK, TaskID::PLAY_CARD
+    };
+
+    /////////////////////////////////////////////
+    // My hand, my field, your field
+    static constexpr size_t TargetPlaceSize = 3;
+
+    static constexpr size_t CurrentHandOffset = 0;
+
+    static constexpr size_t CurrentFieldOffset = 1;
+
+    static constexpr size_t OpponentFieldOffset = 2;
+
+    /////////////////////////////////////////////
+    static constexpr size_t MaximumFieldSize = 10;
+
+    static constexpr size_t EntityEncodeSize =
+        TargetPlaceSize + MaximumFieldSize;
+
+    /////////////////////////////////////////////
+    static constexpr size_t NumSource = 1;
+
+    static constexpr size_t NumTarget = 4;
+
+    /////////////////////////////////////////////
+    static constexpr size_t ActionTensorSize =
+        TaskIDSize + EntityEncodeSize * (NumSource + NumTarget);
+
+    //! Convert ActionEncode to torch::Tensor.
+    static torch::Tensor ActionToTensor(const Game& game,
+                                        const Generic::ActionEncode& action);
+};
 
 //!
 //! \brief TorchPolicy class.
@@ -24,10 +69,24 @@ class TorchPolicy : public BasicPolicy
  public:
     //! Constructs torch based policy with given \p module.
     //! \param module The module base for torch deep learning policy.
-    TorchPolicy(std::shared_ptr<ModuleBase> module);
+    //! \param gameToVec The generator for converting game to numeric vector.
+    TorchPolicy(std::shared_ptr<ModuleBase> module, GameToVec* gameToVec);
+
+    //! Gets next behavior from given \p game.
+    //! \param game The current game status.
+    //! \return The behavior serialized as TaskMeta.
+    TaskMeta Next(const Game& game) override;
 
  private:
     torch::nn::ModuleHolder<ModuleBase> m_module;
+    GameToVec* m_gameToVec;
+
+    Generic::ActionEncode m_action;
+
+    //! Convert ActionEncode to TaskMeta.
+    TaskMeta ActionToTaskMeta() const;
+
+    TaskMeta Inference(const Game& game, const std::vector<Generic::ActionEncode>& actions);
 
     //! Method for MulliganTask requirement.
     TaskMeta RequireMulligan(Player& player) override;
@@ -37,10 +96,7 @@ class TorchPolicy : public BasicPolicy
 
     //! Method for CombatTask requirement.
     TaskMeta RequireAttack(Player& player) override;
-
-    //! Method for OverDraw notifying.
-    void NotifyOverDraw(const TaskMeta& meta) override;
 };
-}  // namespace HSppTorch
+}  // namespace RosettaTorch
 
 #endif
--- Extensions/RosettaTorch/Sources/RosettaTorch/TorchPolicy.cpp
+++ Extensions/RosettaTorch/Sources/RosettaTorch/TorchPolicy.cpp
@@ -4,38 +4,142 @@
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
+#include <Rosetta/Commons/Constants.hpp>
 #include <Rosetta/Commons/Utils.hpp>
 #include <RosettaTorch/TorchPolicy.hpp>
 
 namespace RosettaTorch
 {
-TorchPolicy::TorchPolicy(std::shared_ptr<ModuleBase> module) : m_module(module)
+torch::Tensor ActionEncoder::ActionToTensor(const Game& game,
+                                            const Generic::ActionEncode& action)
+{
+    torch::Tensor tensor = torch::empty(ActionTensorSize, torch::kInt8);
+    for (size_t i = 0; i < TaskIDSize; ++i)
+    {
+        if (action.taskID == TaskIDs[i])
+        {
+            tensor[i] = 1;
+        }
+        else
+        {
+            tensor[i] = 0;
+        }
+    }
+
+    auto fill_zero = [&](size_t start) {
+        for (size_t i = 0; i < EntityEncodeSize; ++i)
+        {
+            tensor[start + i] = 0;
+        }
+    };
+
+    auto write = [&](size_t start, Entity* entity) {
+        fill_zero(start);
+        if (entity == nullptr)
+        {
+            return;
+        }
+
+        Player& player = game.GetCurrentPlayer();
+        if (auto pos = player.GetHand().FindCardPos(*entity); pos.has_value())
+        {
+            tensor[start + CurrentHandOffset] = 1;
+            tensor[start + TargetPlaceSize + pos.value()] = 1;
+        }
+        else if (Minion* minion = dynamic_cast<Minion*>(entity);
+                 minion != nullptr)
+        {
+            if (auto pos = player.GetField().FindMinionPos(*minion);
+                pos.has_value())
+            {
+                tensor[start + CurrentFieldOffset] = 1;
+                tensor[start + TargetPlaceSize + pos.value()] = 1;
+            }
+            else if (auto pos =
+                         game.GetOpponentPlayer().GetField().FindMinionPos(
+                             *minion);
+                     pos.has_value())
+            {
+                tensor[start + OpponentFieldOffset] = 1;
+                tensor[start + TargetPlaceSize + pos.value()] = 1;
+            }
+        }
+    };
+
+    write(TaskIDSize, action.source);
+    for (size_t i = 0; i < NumTarget; ++i)
+    {
+        write(TaskIDSize + (i + 1) * EntityEncodeSize, action.target[i]);
+    }
+    return tensor;
+}
+
+TorchPolicy::TorchPolicy(std::shared_ptr<ModuleBase> module,
+                         GameToVec* gameToVec)
+    : m_module(module), m_gameToVec(gameToVec)
 {
     // Do nothing
 }
 
+TaskMeta TorchPolicy::Next(const Game& game)
+{
+    std::vector<Generic::ActionEncode> actions =
+        Generic::AvailableActions(game);
+    return Inference(game, actions);
+}
+
+TaskMeta TorchPolicy::ActionToTaskMeta() const
+{
+    SizedPtr<Entity*> entities(m_action.numTarget);
+    for (size_t i = 0; i < m_action.numTarget; ++i)
+    {
+        entities[i] = m_action.target[i];
+    }
+    return TaskMeta({ m_action.taskID }, std::move(entities));
+}
+
+TaskMeta TorchPolicy::Inference(
+    const Game& game, const std::vector<Generic::ActionEncode>& actions)
+{
+    torch::Tensor context = m_gameToVec->GenerateTensor(game);
+    torch::Tensor actionTensor =
+        torch::empty({ static_cast<long long>(actions.size()),
+                       ActionEncoder::ActionTensorSize },
+                     torch::kInt8);
+
+    for (size_t i = 0; i < actions.size(); ++i)
+    {
+        actionTensor[i] = ActionEncoder::ActionToTensor(game, actions[i]);
+    }
+
+    torch::Tensor result = m_module->forward(actionTensor, context);
+    size_t idx = result.argmax().item<size_t>();
+
+    m_action = actions[idx];
+    return TaskMeta(TaskMetaTrait(m_action.taskID));
+}
+
 TaskMeta TorchPolicy::RequireMulligan(Player& player)
 {
-    TaskMetaTrait trait(TaskID::MULLIGAN, TaskStatus::COMPLETE,
-                        player.GetID());
+    std::vector<Generic::ActionEncode> actions;
 
-    return TaskMeta(trait, SizedPtr<std::size_t>());
+    Hand& hand = player.GetHand();
+    for (size_t i = 0; i < hand.GetNumOfCards(); ++i)
+    {
+        actions.emplace_back(TaskID::MULLIGAN, hand.GetCard(i));
+    }
+
+    // TODO : Multiple Mulligan
+    return Inference(*player.GetGame(), actions);
 }
 
 TaskMeta TorchPolicy::RequirePlayCard(Player& player)
 {
-    (void)player;
-    return TaskMeta();
+    return ActionToTaskMeta();
 }
 
 TaskMeta TorchPolicy::RequireAttack(Player& player)
 {
-    (void)player;
-    return TaskMeta();
+    return ActionToTaskMeta();
 }
-
-void TorchPolicy::NotifyOverDraw(const TaskMeta& meta)
-{
-    (void)meta;
-}
-}  // namespace HSppTorch
+}  // namespace RosettaTorch
