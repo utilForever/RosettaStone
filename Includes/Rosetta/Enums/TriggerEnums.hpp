1008c15663925b781b3eb4d71aa624467e2c4609
Chris Ohk <utilForever@gmail.com>
2019-04-12 13:24:51 +0900

    feat(card-impl): Create TriggerType enum
    - An enumerator for identifying trigger type

---
+++ Includes/Rosetta/Enums/TriggerEnums.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef ROSETTASTONE_TRIGGER_ENUMS_HPP
+#define ROSETTASTONE_TRIGGER_ENUMS_HPP
+
+namespace RosettaStone
+{
+//! \brief An enumerator for identifying trigger type.
+enum class TriggerType
+{
+    NONE,        //!< The effect has nothing.
+    TURN_START,  //!< The effect will be triggered at the start of turn.
+};
+}  // namespace RosettaStone
+
+#endif  // ROSETTASTONE_TRIGGER_ENUMS_HPP
