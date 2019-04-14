b29a133dd4ed7150be178a0ea080b855b30a9ee9
kozistr <kozistr@gmail.com>
2019-04-14 15:52:31 +0900

    feat(Game2Vec): Add feature of number of cards

--- Extensions/RosettaTorch/Sources/RosettaTorch/GameToVec.cpp
+++ Extensions/RosettaTorch/Sources/RosettaTorch/GameToVec.cpp
@@ -10,12 +10,12 @@ namespace RosettaTorch
 {
 torch::Tensor GameToVec::GenerateTensor(const Game& game)
 {
-    // vector shape : [1 + 1 + n * 7 + 1 + n * 7 + n * 10 = 3 + n * 24]
-    // # 1      : number of opponent player's cards on hand (sparse)
-    // # 1      : number of opponent player's left cards at deck (sparse)
+    // vector shape : [1 + 1 + 1 + n * 7 + n * 7 + n * 10 = 3 + n * 24]
+    // # 1      : number of opponent player's cards on hand.
+    // # 1      : number of opponent player's left cards at deck.
+    // # 1      : number of current player's left cards at deck.
     // # n * 7  : opponent player's representation vectors of card at the deck. 
     //            each card has a dimensionality of n.
-    // # 1      : number of current player's left cards at deck (sparse)
     // # n * 7  : current player's representation vectors of card at the deck. 
     //            each card has a dimensionality of n.
     // # n * 10 : current player's representation vectors of card in the hand.
@@ -24,15 +24,27 @@ torch::Tensor GameToVec::GenerateTensor(const Game& game)
     // each card is represented as a vector which has n dimensionality.
     // it includes, cost, attack, health, ability.
     // vector shape : [1 + 1 + 1 + m = n]
-    // # 1 : cost (sparse)
-    // # 1 : attack (sparse)
-    // # 1 : health (sparse)
+    // # 1 : cost
+    // # 1 : attack
+    // # 1 : health
     // # m : ability
 
     // each card's abilities are represented as a vector which has m dimensionality.
     // it includes, ...
     // vector shape : [m]
-    torch::Tensor tensor = torch::empty(GameVectorSize, torch::kInt8);
+    torch::Tensor tensor = torch::empty(GameVectorSize, torch::kFloat32);
+
+	Player& curPlayer = game.GetCurrentPlayer();
+    Player& oppPlayer = game.GetOpponentPlayer();
+
+	// Write number of opponent player's cards, normalized, on hand.
+	tensor[0] = static_cast<float>(oppPlayer.GetHand().GetNumOfCards()) / HAND_SIZE;
+
+	// Write number of opp player's cards normalized.
+    tensor[1] = static_cast<float>(oppPlayer.GetDeck().GetNumOfCards()) / MAX_DECK_SIZE;
+
+	// Write number of current player's cards normalized.
+    tensor[2] = static_cast<float>(curPlayer.GetDeck().GetNumOfCards()) / MAX_DECK_SIZE;
 
     return tensor;
 }
