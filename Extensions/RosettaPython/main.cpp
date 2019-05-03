6ce467c40a8d36566d82e7e6e78bf9091413f195
Chris Ohk <utilForever@gmail.com>
2019-05-04 02:02:41 +0900

    feat(python-api): Create main() function for RosettaPython

---
+++ Extensions/RosettaPython/main.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Python/Cards/Card.hpp>
+
+#include <pybind11/pybind11.h>
+
+PYBIND11_MODULE(RosettaPython, m)
+{
+    m.doc() = R"pbdoc(Hearthstone simulator with some reinforcement learning)pbdoc";
+
+    // Cards
+    AddCard(m);
+}
