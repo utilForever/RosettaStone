95cdcad672c433886b6acd98332212f51a547435
Chris Ohk <utilForever@gmail.com>
2019-05-05 23:28:35 +0900

    feat(python-api): Create AddCardEnums() function

--- Extensions/RosettaPython/Sources/Python/Enums/CardEnums.cpp
+++ Extensions/RosettaPython/Sources/Python/Enums/CardEnums.cpp
@@ -13,9 +13,51 @@ using namespace RosettaStone;
 
 void AddCardEnums(pybind11::module& m)
 {
+    pybind11::enum_<CardClass>(m, "CardClass")
+#define X(a) .value(#a, CardClass::a)
+#include "Rosetta/Enums/CardClass.def"
+#undef X
+        ;
+
+    pybind11::enum_<CardSet>(m, "CardSet")
+#define X(a) .value(#a, CardSet::a)
+#include "Rosetta/Enums/CardSet.def"
+#undef X
+        ;
+
+    pybind11::enum_<CardType>(m, "CardType")
+#define X(a) .value(#a, CardType::a)
+#include "Rosetta/Enums/CardType.def"
+#undef X
+        ;
+
+    pybind11::enum_<Faction>(m, "Faction")
+#define X(a) .value(#a, Faction::a)
+#include "Rosetta/Enums/Faction.def"
+#undef X
+        ;
+
     pybind11::enum_<GameTag>(m, "GameTag")
 #define X(a) .value(#a, GameTag::a)
 #include "Rosetta/Enums/GameTag.def"
+#undef X
+        ;
+
+    pybind11::enum_<PlayReq>(m, "PlayReq")
+#define X(a) .value(#a, PlayReq::a)
+#include "Rosetta/Enums/PlayReq.def"
+#undef X
+        ;
+
+    pybind11::enum_<Race>(m, "Race")
+#define X(a) .value(#a, Race::a)
+#include "Rosetta/Enums/Race.def"
+#undef X
+        ;
+
+    pybind11::enum_<Rarity>(m, "Rarity")
+#define X(a) .value(#a, Rarity::a)
+#include "Rosetta/Enums/Rarity.def"
 #undef X
         ;
 }
