662d6941583f1cfcb10f9d2c72452835351e9fd1
Chris Ohk <utilForever@gmail.com>
2019-05-07 19:35:46 +0900

    test: Add unit test for checking game over

--- Tests/UnitTests/Games/GameTests.cpp
+++ Tests/UnitTests/Games/GameTests.cpp
@@ -7,11 +7,14 @@
 #include <Utils/TestUtils.hpp>
 #include "gtest/gtest.h"
 
+#include <Rosetta/Actions/Draw.hpp>
+#include <Rosetta/Cards/Cards.hpp>
 #include <Rosetta/Games/Game.hpp>
 #include <Rosetta/Games/GameConfig.hpp>
 #include <Rosetta/Games/GameManager.hpp>
 #include <Rosetta/Policies/BasicPolicy.hpp>
-#include <Rosetta/Tasks/PlayerTasks/ChooseTask.hpp>
+#include <Rosetta/Tasks/PlayerTasks/AttackTask.hpp>
+#include <Rosetta/Tasks/PlayerTasks/PlayCardTask.hpp>
 
 using namespace RosettaStone;
 using namespace PlayerTasks;
@@ -46,4 +49,36 @@ TEST(Game, Mulligan)
 
     game.nextStep = Step::BEGIN_MULLIGAN;
     GameManager::ProcessNextStep(game, game.nextStep);
+}
+
+TEST(Game, GameOver)
+{
+    GameConfig config;
+    config.player1Class = CardClass::WARRIOR;
+    config.player2Class = CardClass::ROGUE;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.StartGame();
+    game.ProcessUntil(Step::MAIN_START);
+
+    Player& curPlayer = game.GetCurrentPlayer();
+    Player& opPlayer = game.GetOpponentPlayer();
+    curPlayer.SetTotalMana(10);
+    curPlayer.SetUsedMana(0);
+    opPlayer.SetTotalMana(10);
+    opPlayer.SetUsedMana(0);
+    opPlayer.GetHero()->SetDamage(29);
+
+    const auto card1 = Generic::DrawCard(
+        curPlayer, Cards::GetInstance().FindCardByName("Wolfrider"));
+
+    Task::Run(curPlayer, PlayCardTask::Minion(curPlayer, card1));
+    game.Process(AttackTask(card1, opPlayer.GetHero()));
+
+    EXPECT_EQ(game.state, State::COMPLETE);
+    EXPECT_EQ(curPlayer.playState, PlayState::WON);
+    EXPECT_EQ(opPlayer.playState, PlayState::LOST);
 }
