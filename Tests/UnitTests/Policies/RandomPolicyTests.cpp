b7c48b200b87824656ab3056036f926c067a5357
Chris Ohk <utilForever@gmail.com>
2019-05-09 02:38:52 +0900

    test: Add test code for checking auto run

--- Tests/UnitTests/Policies/RandomPolicyTests.cpp
+++ Tests/UnitTests/Policies/RandomPolicyTests.cpp
@@ -9,17 +9,54 @@
 #include <Rosetta/Cards/Cards.hpp>
 #include <Rosetta/Games/Game.hpp>
 #include <Rosetta/Games/GameConfig.hpp>
-#include <Rosetta/Games/GameManager.hpp>
 #include <Rosetta/Policies/RandomPolicy.hpp>
 
 using namespace RosettaStone;
 
-TEST(Policies, RandomPolicy)
+TEST(RandomPolicy, AutoRun)
 {
     GameConfig config;
     config.player1Class = CardClass::ROGUE;
     config.player2Class = CardClass::PALADIN;
     config.startPlayer = PlayerType::RANDOM;
+    config.doShuffle = false;
+    config.doFillDecks = false;
+    config.skipMulligan = true;
+    config.autoRun = true;
+
+    std::array<std::string, START_DECK_SIZE> deck = {
+        "CS2_106", "CS2_105", "CS1_112", "CS1_112",  // 1
+        "CS1_113", "CS1_113", "CS1_130", "CS1_130",  // 2
+        "CS2_007", "CS2_007", "CS2_022", "CS2_022",  // 3
+        "CS2_023", "CS2_023", "CS2_024", "CS2_024",  // 4
+        "CS2_025", "CS2_025", "CS2_026", "CS2_026",  // 5
+        "CS2_027", "CS2_027", "CS2_029", "CS2_029",  // 6
+        "CS2_032", "CS2_032", "CS2_033", "CS2_033",  // 7
+        "CS2_037", "CS2_037"
+    };
+
+    for (size_t i = 0; i < START_DECK_SIZE; ++i)
+    {
+        config.player1Deck[i] = Cards::GetInstance().FindCardByID(deck[i]);
+        config.player2Deck[i] = Cards::GetInstance().FindCardByID(deck[i]);
+    }
+
+    Game game(config);
+
+    RandomPolicy policy;
+    game.GetPlayer1().policy = &policy;
+    game.GetPlayer2().policy = &policy;
+
+    game.StartGame();
+}
+
+TEST(RandomPolicy, Mulligan)
+{
+    GameConfig config;
+    config.player1Class = CardClass::ROGUE;
+    config.player2Class = CardClass::PALADIN;
+    config.startPlayer = PlayerType::RANDOM;
+    config.doShuffle = false;
     config.doFillDecks = false;
     config.skipMulligan = false;
     config.autoRun = false;
@@ -48,7 +85,5 @@ TEST(Policies, RandomPolicy)
     game.GetPlayer2().policy = &policy;
 
     game.StartGame();
-
-    game.nextStep = Step::BEGIN_MULLIGAN;
-    GameManager::ProcessNextStep(game, game.nextStep);
+    game.ProcessUntil(Step::BEGIN_MULLIGAN);
 }
