419a67b9ec764b025ec6bab8ae34ad69cf207905
Chris Ohk <utilForever@gmail.com>
2019-05-20 23:37:38 +0900

    refactor: Implement Zone and related classes (variables/methods)
    - Zone: constructor, operator[], MoveTo(), GetCount(), IsEmpty(), IsFull()
    - UnlimitedZone: Add(), Remove(), MoveTo(), GetCount(), IsFull()
    - LimitedZone: constructor, Add(), Remove(), MoveTo(), GetCount(), IsFull(), GetAll()
    - PositioningZone: constructor, Add(), Remove(), Reposition()

--- Includes/Rosetta/Zones/Zone.hpp
+++ Includes/Rosetta/Zones/Zone.hpp
@@ -6,7 +6,11 @@
 #ifndef ROSETTASTONE_ZONE_HPP
 #define ROSETTASTONE_ZONE_HPP
 
+#include <Rosetta/Enchants/Aura.hpp>
 #include <Rosetta/Zones/IZone.hpp>
+
+#include <algorithm>
+#include <stdexcept>
 
 namespace RosettaStone
 {
@@ -19,6 +23,22 @@ template <typename T>
 class Zone : public IZone
 {
  public:
+    //! Default constructor.
+    Zone() = default;
+
+    //! Constructor with given \p size.
+    //! \param size The size of container that stores entities.
+    Zone(int size) : m_entities(size)
+    {
+        // Do nothing
+    }
+
+    //! Operator overloading for operator[].
+    T* operator[](int zonePos)
+    {
+        return m_entities[zonePos];
+    }
+
     //! Adds the specified entity into this zone, at the given position.
     //! \param entity The entity.
     //! \param zonePos The zone position.
@@ -29,29 +49,79 @@ class Zone : public IZone
     //! \return The entity.
     Entity& Remove(Entity& entity) override = 0;
 
-protected:
-    std::vector<T> m_entities;
-};
+    //! Moves the specified entity to a new position.
+    //! \param entity The entity.
+    //! \param zonePos The zone position.
+    virtual void MoveTo(T& entity, int zonePos = -1) = 0;
+
+    //! Returns the number of entities in this zone.
+    //! \return The number of entities in this zone.
+    virtual int GetCount() const = 0;
+
+    //! Returns a value indicating whether this zone is empty.
+    //! \return true if this zone is empty, false otherwise.
+    bool IsEmpty() const
+    {
+        return GetCount() == 0;
+    }
+
+    //! Returns a value indicating whether this zone is full.
+    //! \return true if this zone is full, false otherwise.
+    virtual bool IsFull() const = 0;
+
+ protected:
+    std::vector<T*> m_entities;
+    Player* m_owner = nullptr;
+};  // namespace RosettaStone
 
 //!
 //! \brief UnlimitedZone class.
 //!
 //! This class is base implementation of GraveyardZone and SetasideZone.
 //!
-class UnlimitedZone : public Zone<Entity*>
+class UnlimitedZone : public Zone<Entity>
 {
  public:
     void Add(Entity& entity, int zonePos = -1) override
     {
-        (void)entity;
-        (void)zonePos;
+        if (entity.owner != m_owner)
+        {
+            throw std::logic_error(
+                "Can't add an opponent's entity to own zones");
+        }
 
-        // Do nothing
+        MoveTo(entity, zonePos);
     }
 
     Entity& Remove(Entity& entity) override
     {
+        if (entity.zone == nullptr || entity.zone->GetType() != m_type)
+        {
+            throw std::logic_error("Couldn't remove entity from zone.");
+        }
+
+        m_entities.erase(
+            std::remove(m_entities.begin(), m_entities.end(), &entity),
+            m_entities.end());
+
         return entity;
+    }
+
+    void MoveTo(Entity& entity, int) override
+    {
+        m_entities.emplace_back(&entity);
+        entity.zone = this;
+        entity.SetZoneType(m_type);
+    }
+
+    int GetCount() const override
+    {
+        return m_entities.size();
+    }
+
+    bool IsFull() const override
+    {
+        return false;
     }
 };
 
@@ -63,18 +133,127 @@ class UnlimitedZone : public Zone<Entity*>
 template <typename T>
 class LimitedZone : public Zone<T>
 {
+ public:
+    LimitedZone(int size) : Zone(size), m_maxSize(size)
+    {
+        // Do nothing
+    }
+
     void Add(Entity& entity, int zonePos = -1) override
     {
-        (void)entity;
-        (void)zonePos;
+        if (zonePos > m_count)
+        {
+            throw std::logic_error("Zone position isn't in a valid range.");
+        }
 
-        // Do nothing
+        if (entity.owner != Zone<T>::m_owner)
+        {
+            throw std::logic_error(
+                "Can't add an opponent's entity to own zones");
+        }
+
+        MoveTo((T&)(entity), zonePos < 0 ? m_count : zonePos);
     }
 
     Entity& Remove(Entity& entity) override
     {
+        if (entity.zone == nullptr || entity.zone->GetType() != Zone<T>::m_type)
+        {
+            throw std::logic_error("Couldn't remove entity from zone.");
+        }
+
+        int pos;
+        for (pos = m_count - 1; pos >= 0; --pos)
+        {
+            if (&entity == dynamic_cast<Entity*>(Zone<T>::m_entities[pos]))
+            {
+                break;
+            }
+        }
+
+        --m_count;
+
+        if (pos < m_count)
+        {
+            std::copy(Zone<T>::m_entities.begin() + pos + 1,
+                      Zone<T>::m_entities.end(),
+                      Zone<T>::m_entities.begin() + pos);
+        }
+
+        Zone<T>::m_entities[m_count] = nullptr;
+        entity.zone = nullptr;
+
+        if (entity.activatedTrigger != nullptr)
+        {
+            entity.activatedTrigger->Remove();
+        }
+
         return entity;
     }
+
+    void MoveTo(T& entity, int zonePos = -1) override
+    {
+        if (IsFull())
+        {
+            return;
+        }
+
+        if (zonePos < 0)
+        {
+            zonePos = m_count;
+        }
+
+        if (zonePos == m_count)
+        {
+            Zone<T>::m_entities[zonePos] = &entity;
+        }
+        else
+        {
+            for (int i = m_count - 1; i >= zonePos; --i)
+            {
+                Zone<T>::m_entities[i + 1] = Zone<T>::m_entities[i];
+            }
+
+            Zone<T>::m_entities[zonePos] = &entity;
+        }
+
+        ++m_count;
+
+        dynamic_cast<Entity&>(entity).zone = this;
+        dynamic_cast<Entity&>(entity).SetZoneType(Zone<T>::m_type);
+    }
+
+    int GetCount() const override
+    {
+        return m_count;
+    }
+
+    bool IsFull() const override
+    {
+        return m_count == m_maxSize;
+    }
+
+    virtual std::vector<T*> GetAll()
+    {
+        std::vector<T*> result;
+        result.reserve(m_count);
+
+        for (int i = 0; i < m_count; ++i)
+        {
+            if (Zone<T>::m_entities[i] == nullptr)
+            {
+                continue;
+            }
+
+            result.emplace_back(Zone<T>::m_entities[i]);
+        }
+
+        return result;
+    }
+
+ protected:
+    int m_count = 0;
+    int m_maxSize = 0;
 };
 
 //!
@@ -86,19 +265,84 @@ class LimitedZone : public Zone<T>
 template <typename T>
 class PositioningZone : public LimitedZone<T>
 {
+ public:
+    PositioningZone(int size) : LimitedZone<T>(size)
+    {
+        // Do nothing
+    }
+
     void Add(Entity& entity, int zonePos = -1) override
     {
-        (void)entity;
-        (void)zonePos;
+        LimitedZone<T>::Add(entity, zonePos);
 
-        // Do nothing
+        Reposition(zonePos);
     }
 
     Entity& Remove(Entity& entity) override
     {
+        if (entity.zone == nullptr ||
+            entity.zone->GetType() != LimitedZone<T>::m_type)
+        {
+            throw std::logic_error("Couldn't remove entity from zone.");
+        }
+
+        int pos;
+        for (pos = LimitedZone<T>::m_count - 1; pos >= 0; --pos)
+        {
+            if (&entity == dynamic_cast<Entity*>(Zone<T>::m_entities[pos]))
+            {
+                break;
+            }
+        }
+
+        --LimitedZone<T>::m_count;
+
+        if (pos < LimitedZone<T>::m_count)
+        {
+            std::copy(LimitedZone<T>::m_entities.begin() + pos + 1,
+                      LimitedZone<T>::m_entities.end(),
+                      LimitedZone<T>::m_entities.begin() + pos);
+        }
+
+        Reposition(pos);
+
+        Zone<T>::m_entities[LimitedZone<T>::m_count] = nullptr;
+        entity.zone = nullptr;
+
+        if (entity.activatedTrigger != nullptr)
+        {
+            entity.activatedTrigger->Remove();
+        }
+
+        for (std::size_t i = 0; i < auras.size(); ++i)
+        {
+            auras[i]->RemoveEntity(entity);
+        }
+
         return entity;
+    }
+
+    std::vector<Aura*> auras;
+
+ private:
+    void Reposition(int zonePos = 0)
+    {
+        if (zonePos < 0)
+        {
+            zonePos = LimitedZone<T>::m_count - 1;
+        }
+
+        for (int i = LimitedZone<T>::m_count - 1; i >= zonePos; --i)
+        {
+            dynamic_cast<Entity*>(LimitedZone<T>::m_entities[i])->zonePos = i;
+        }
+
+        for (std::size_t i = 0; i < auras.size(); ++i)
+        {
+            auras[i]->SetToBeUpdated(true);
+        }
     }
 };
 }  // namespace RosettaStone
 
-#endif  // ROSETTASTONE_IZONE_HPP
+#endif  // ROSETTASTONE_ZONE_HPP
