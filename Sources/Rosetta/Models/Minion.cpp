d4cf1200d260b82301143ad8e0490e86bc20a696
Chris Ohk <utilForever@gmail.com>
2019-06-01 16:13:45 +0900

    feat(card-impl): Add Silence() method
    - Disables all special effects on this minion

--- Sources/Rosetta/Models/Minion.cpp
+++ Sources/Rosetta/Models/Minion.cpp
@@ -4,7 +4,9 @@
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
+#include <Rosetta/Commons/Utils.hpp>
 #include <Rosetta/Games/Game.hpp>
+#include <Rosetta/Models/Enchantment.hpp>
 #include <Rosetta/Models/Minion.hpp>
 #include <Rosetta/Models/Player.hpp>
 
@@ -26,6 +28,76 @@ int Minion::GetLastBoardPos() const
 void Minion::SetLastBoardPos(int value)
 {
     SetGameTag(GameTag::TAG_LAST_KNOWN_COST_IN_HAND, value);
+}
+
+void Minion::Silence()
+    {
+    SetGameTag(GameTag::TAUNT, 0);
+    SetGameTag(GameTag::FROZEN, 0);
+    SetGameTag(GameTag::ENRAGED, 0);
+    SetGameTag(GameTag::CHARGE, 0);
+    SetGameTag(GameTag::WINDFURY, 0);
+    SetGameTag(GameTag::DIVINE_SHIELD, 0);
+    SetGameTag(GameTag::STEALTH, 0);
+    SetGameTag(GameTag::DEATHRATTLE, 0);
+    SetGameTag(GameTag::BATTLECRY, 0);
+    SetGameTag(GameTag::INSPIRE, 0);
+    SetGameTag(GameTag::LIFESTEAL, 0);
+    SetGameTag(GameTag::CANT_BE_TARGETED_BY_HERO_POWERS, 0);
+    SetGameTag(GameTag::CANT_BE_TARGETED_BY_SPELLS, 0);
+    SetGameTag(GameTag::IMMUNE, 0);
+
+    const int spellPower = GetGameTag(GameTag::SPELLPOWER);
+    if (spellPower > 0)
+    {
+        owner->currentSpellPower -= spellPower;
+        SetGameTag(GameTag::SPELLPOWER, 0);
+    }
+
+    if (onGoingEffect != nullptr)
+    {
+        onGoingEffect->Remove();
+    }
+
+    EraseIf(owner->GetGame()->oneTurnEffects,
+            [this](std::pair<Entity*, Effect*> effect) {
+                return effect.first->id == id;
+            });
+
+    if (activatedTrigger != nullptr)
+    {
+        activatedTrigger->Remove();
+    }
+
+    if (!appliedEnchantments.empty())
+    {
+        const auto size = static_cast<int>(appliedEnchantments.size());
+        for (int i = size - 1; i >= 0; --i)
+        {
+            if (appliedEnchantments[i]->card.power.GetAura() != nullptr)
+            {
+                appliedEnchantments[i]->Remove();
+            }
+        }
+    }
+
+    SetGameTag(GameTag::ATK, card.gameTags[GameTag::ATK]);
+    if (GetHealth() > card.gameTags[GameTag::HEALTH])
+    {
+        SetHealth(card.gameTags[GameTag::HEALTH]);
+    }
+    else
+    {
+        const int cardBaseHealth = card.gameTags[GameTag::HEALTH];
+        const int delta = GetGameTag(GameTag::HEALTH) - cardBaseHealth;
+        if (delta > 0)
+        {
+            SetDamage(GetDamage() - delta);
+        }
+        SetGameTag(GameTag::HEALTH, card.gameTags[GameTag::HEALTH]);
+    }
+
+    SetGameTag(GameTag::SILENCED, 1);
 }
 
 void Minion::Reset()
