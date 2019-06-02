82bbce9556329b30c06a2d9d1768af664445a798
Chris Ohk <utilForever@gmail.com>
2019-06-02 12:25:14 +0900

    feat(card-impl): Create Triggers class
    - This class includes utility methods for trigger

--- Includes/Rosetta/RosettaStone.hpp
+++ Includes/Rosetta/RosettaStone.hpp
@@ -36,6 +36,7 @@
 #include <Rosetta/Enchants/Power.hpp>
 #include <Rosetta/Enchants/Powers.hpp>
 #include <Rosetta/Enchants/Trigger.hpp>
+#include <Rosetta/Enchants/Triggers.hpp>
 #include <Rosetta/Enums/CardEnums.hpp>
 #include <Rosetta/Enums/GameEnums.hpp>
 #include <Rosetta/Enums/TaskEnums.hpp>
