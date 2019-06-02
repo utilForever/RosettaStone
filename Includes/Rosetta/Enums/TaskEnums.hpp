7ff21feb20110a2a57cf5005e015f4206ea97ab3
Chris Ohk <utilForever@gmail.com>
2019-06-02 16:20:38 +0900

    feat(card-impl): Create DamageNumberTask class
    - This class represents the task for taking damage with stacked number

--- Includes/Rosetta/Enums/TaskEnums.hpp
+++ Includes/Rosetta/Enums/TaskEnums.hpp
@@ -64,6 +64,7 @@ enum class TaskID
     MOVE_TO_GRAVEYARD,
     ADD_AURA_EFFECT,
     SILENCE,
+    DAMAGE_NUMBER,
 
     NUM_TASK_ID
 };
---
+++ Sources/Rosetta/Tasks/SimpleTasks/DamageNumberTask.cpp
@@ -0,0 +1,39 @@
+// This code is based on Sabberstone project.
+// Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+// RosettaStone is hearthstone simulator using C++ with reinforcement learning.
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+#include <Rosetta/Actions/Generic.hpp>
+#include <Rosetta/Games/Game.hpp>
+#include <Rosetta/Tasks/SimpleTasks/DamageNumberTask.hpp>
+#include <Rosetta/Tasks/SimpleTasks/IncludeTask.hpp>
+
+namespace RosettaStone::SimpleTasks
+{
+DamageNumberTask::DamageNumberTask(EntityType entityType, bool isSpellDamage)
+    : ITask(entityType), m_isSpellDamage(isSpellDamage)
+{
+    // Do nothing
+}
+
+TaskID DamageNumberTask::GetTaskID() const
+{
+    return TaskID::DAMAGE_NUMBER;
+}
+
+TaskStatus DamageNumberTask::Impl(Player& player)
+{
+    const int damage = m_source->owner->GetGame()->taskStack.num;
+    auto entities =
+        IncludeTask::GetEntities(m_entityType, player, m_source, m_target);
+
+    for (auto& entity : entities)
+    {
+        const auto character = dynamic_cast<Character*>(entity);
+        Generic::TakeDamageToCharacter(m_source, character, damage,
+                                       m_isSpellDamage);
+    }
+
+    return TaskStatus::COMPLETE;
+}
+}  // namespace RosettaStone::SimpleTasks
