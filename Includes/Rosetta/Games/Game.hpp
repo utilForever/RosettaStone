f9c44ab0ac3e49326654a9a66476a8968a172951
Chris Ohk <utilForever@gmail.com>
2019-06-03 11:21:25 +0900

    feat(card-impl): Add variable 'triggers'

--- Includes/Rosetta/Games/Game.hpp
+++ Includes/Rosetta/Games/Game.hpp
@@ -162,6 +162,7 @@ class Game
     TriggerManager triggerManager;
 
     std::vector<Aura*> auras;
+    std::vector<Trigger*> triggers;
     std::vector<std::pair<Entity*, Effect*>> oneTurnEffects;
     std::vector<Minion*> summonedMinions;
     std::map<std::size_t, Minion*> deadMinions;
