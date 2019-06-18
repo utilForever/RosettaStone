d47dc44c5a972f3df5afd97310a711e44c3ce3b0
Chris Ohk <utilForever@gmail.com>
2019-06-19 02:06:01 +0900

    feat(MCTS): Create namespace 'Policy' and 'Configs'
    - Add constant 'CREDIT_GRANULARITY'

---
+++ Extensions/RosettaTorch/Includes/MCTS/Configs.hpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+// It is based on peter1591's hearthstone-ai repository.
+// References: https://github.com/peter1591/hearthstone-ai
+
+#ifndef ROSETTASTONE_TORCH_MCTS_CONFIGS_HPP
+#define ROSETTASTONE_TORCH_MCTS_CONFIGS_HPP
+
+namespace RosettaTorch::MCTS
+{
+namespace Policy
+{
+}
+
+namespace Configs
+{
+    constexpr int CREDIT_GRANULARITY = 100;
+}
+}  // namespace RosettaTorch::MCTS
+
+#endif  // ROSETTASTONE_TORCH_MCTS_CONFIGS_HPP
