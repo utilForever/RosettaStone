bd5c649a3dfdb3715724444120ac23d38fdca996
Chris Ohk <utilForever@gmail.com>
2019-06-25 02:55:18 +0900

    [ci skip] [WIP] feat(MCTS): Create ActionChoice class

---
+++ Includes/Rosetta/Actions/ActionChoices.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+// It is based on peter1591's hearthstone-ai repository.
+// References: https://github.com/peter1591/hearthstone-ai
+
+#ifndef ROSETTASTONE_ACTION_CHOICES_HPP
+#define ROSETTASTONE_ACTION_CHOICES_HPP
+
+namespace RosettaStone
+{
+class ActionChoices
+{
+};
+}  // namespace RosettaStoneqkr
+
+#endif  // ROSETTASTONE_ACTION_CHOICES_HPP
