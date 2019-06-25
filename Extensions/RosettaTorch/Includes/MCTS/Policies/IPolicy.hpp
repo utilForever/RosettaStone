73ac7037842eda846bfac22a2526729401b12db6
Chris Ohk <utilForever@gmail.com>
2019-06-26 02:27:23 +0900

    [ci skip] feat(MCTS): Create IPolicy class

---
+++ Extensions/RosettaTorch/Includes/MCTS/Policies/IPolicy.hpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+// It is based on peter1591's hearthstone-ai repository.
+// References: https://github.com/peter1591/hearthstone-ai
+
+#ifndef ROSETTASTONE_TORCH_MCTS_IPOLICY_HPP
+#define ROSETTASTONE_TORCH_MCTS_IPOLICY_HPP
+
+#include <Rosetta/Actions/ActionType.hpp>
+
+#include <vector>
+
+namespace RosettaTorch::MCTS
+{
+//!
+//! \brief IPolicy class.
+//!
+class IPolicy
+{
+ public:
+    virtual ~IPolicy() = default;
+
+    virtual int SelectChoice(RosettaStone::ActionType actionType,
+                             std::vector<int> choices) = 0;
+};
+}  // namespace RosettaTorch::MCTS
+
+#endif  // ROSETTASTONE_TORCH_MCTS_IPOLICY_HPP
