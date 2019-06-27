acab4b597ca2b80e4d8a81e46e359797e03136a0
Chris Ohk <utilForever@gmail.com>
2019-06-27 21:47:32 +0900

    [ci skip] [WIP] feat(MCTS): Create RandomPlayoutsPolicy class

---
+++ Extensions/RosettaTorch/Includes/MCTS/Policies/RandomPlayoutsPolicy.hpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+// It is based on peter1591's hearthstone-ai repository.
+// References: https://github.com/peter1591/hearthstone-ai
+
+#ifndef ROSETTASTONE_TORCH_MCTS_RANDOM_PLAYOUTS_POLICY_HPP
+#define ROSETTASTONE_TORCH_MCTS_RANDOM_PLAYOUTS_POLICY_HPP
+
+#include <MCTS/Policies/ISimulationPolicy.hpp>
+
+namespace RosettaTorch::MCTS
+{
+class RandomPlayoutsPolicy : public ISimulationPolicy
+{
+ public:
+    bool IsEnableCutoff() override
+    {
+        return true;
+    }
+
+    bool GetCutoffResult(const RosettaStone::Game& game,
+                         StateValue& stateValue) override
+    {
+        return true;
+    }
+};
+}  // namespace RosettaTorch::MCTS
+
+#endif  // ROSETTASTONE_TORCH_MCTS_RANDOM_PLAYOUTS_POLICY_HPP
