b0a9069bb035271f0550050480780546fc7b0aa7
Chris Ohk <utilForever@gmail.com>
2019-07-08 21:15:50 +0900

    feat(MCTS): Add 'ConsistencyCheckAddons' class

--- Extensions/RosettaTorch/Includes/MCTS/Selection/TreeNodeAddon.hpp
+++ Extensions/RosettaTorch/Includes/MCTS/Selection/TreeNodeAddon.hpp
@@ -34,6 +34,7 @@ struct LeadingNodesItem
         {
             return false;
         }
+
         return true;
     }
 
@@ -67,31 +68,162 @@ struct hash<RosettaTorch::MCTS::LeadingNodesItem>
 namespace RosettaTorch::MCTS
 {
 //!
+//! \brief ConsistencyCheckAddons class.
+//!
+class ConsistencyCheckAddons
+{
+ public:
+    bool SetAndCheck(ActionType actionType, ActionChoices const& choices)
+    {
+        std::lock_guard<SpinLock> lock(m_mutex);
+
+        assert(actionType != ActionType::INVALID);
+
+        return CheckActionTypeAndChoices(actionType, choices);
+    }
+
+    bool SetAndCheckBoard(ReducedBoardView const& newView)
+    {
+        std::lock_guard<SpinLock> lock(m_mutex);
+        return LockedSetAndCheckBoard(newView);
+    }
+
+    bool CheckBoard(ReducedBoardView const& newView) const
+    {
+        std::lock_guard<SpinLock> lock(m_mutex);
+
+        if (!m_boardView)
+        {
+            return true;
+        }
+
+        return *m_boardView == newView;
+    }
+
+    bool CheckActionType(ActionType actionType) const
+    {
+        std::lock_guard<SpinLock> lock(m_mutex);
+        return LockedCheckActionType(actionType);
+    }
+
+    auto GetActionType() const
+    {
+        std::lock_guard<SpinLock> lock(m_mutex);
+        return m_actionType;
+    }
+
+    auto GetBoard() const
+    {
+        std::lock_guard<SpinLock> lock(m_mutex);
+        return m_boardView.get();
+    }
+
+ private:
+    bool LockedSetAndCheckBoard(ReducedBoardView const& newView)
+    {
+        if (!m_boardView)
+        {
+            m_boardView.reset(new ReducedBoardView(newView));
+            return true;
+        }
+
+        return *m_boardView == newView;
+    }
+
+    bool LockedCheckActionType(ActionType action_type) const
+    {
+        if (m_actionType == ActionType::INVALID)
+            return true;
+        return m_actionType == action_type;
+    }
+
+    bool CheckActionTypeAndChoices(ActionType actionType,
+                                   ActionChoices const& choices)
+    {
+        if (m_actionType == ActionType::INVALID)
+        {
+            m_actionType = actionType;
+            m_actionChoices = choices;
+
+            return true;
+        }
+
+        if (m_actionType != actionType)
+        {
+            return false;
+        }
+
+        if (m_actionChoices.GetIndex() != choices.GetIndex())
+        {
+            return false;
+        }
+
+        if (!m_actionChoices.Compare(choices, [](auto&& lhs, auto&& rhs) {
+                using Type1 = std::decay_t<decltype(lhs)>;
+                using Type2 = std::decay_t<decltype(rhs)>;
+
+                if (!std::is_same_v<Type1, Type2>)
+                {
+                    return false;
+                }
+
+                if (std::is_same_v<Type1, ActionChoices::ChooseFromCardIDs>)
+                {
+                    return true;
+                }
+
+                if (std::is_same_v<Type1,
+                                   ActionChoices::ChooseFromZeroToExclusiveMax>)
+                {
+                    return lhs.Size() == rhs.Size();
+                }
+
+                if (std::is_same_v<Type1, ActionChoices::InvalidChoice>)
+                {
+                    return false;
+                }
+
+                return false;
+            }))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    mutable SpinLock m_mutex{};
+    std::unique_ptr<ReducedBoardView> m_boardView;
+    ActionType m_actionType{};
+    ActionChoices m_actionChoices;
+};
+
+//!
 //! \brief LeadingNodes class.
 //!
 class LeadingNodes
 {
  public:
-    void AddLeadingNodes(TreeNode* node, EdgeAddon* edge_addon)
+    void AddLeadingNodes(TreeNode* node, EdgeAddon* edgeAddon)
     {
-        std::lock_guard<RosettaStone::SharedSpinLock> lock(m_mutex);
+        std::lock_guard<SharedSpinLock> lock(m_mutex);
         assert(node);
 
         for (const auto& item : m_items)
         {
-            if (item.node == node && item.edgeAddon == edge_addon)
+            if (item.node == node && item.edgeAddon == edgeAddon)
             {
                 return;
             }
         }
 
-        m_items.push_back(LeadingNodesItem{ node, edge_addon });
+        m_items.push_back(LeadingNodesItem{ node, edgeAddon });
     }
 
     template <class Functor>
     void ForEachLeadingNode(Functor&& op)
     {
-        std::shared_lock<RosettaStone::SharedSpinLock> lock(m_mutex);
+        std::shared_lock<SharedSpinLock> lock(m_mutex);
         for (const auto& item : m_items)
         {
             if (!op(item.node, item.edgeAddon))
@@ -102,7 +234,7 @@ class LeadingNodes
     }
 
  private:
-    mutable RosettaStone::SharedSpinLock m_mutex;
+    mutable SharedSpinLock m_mutex;
     std::vector<LeadingNodesItem> m_items;
 };
 
@@ -116,6 +248,7 @@ struct TreeNodeAddon
         // Do nothing
     };
 
+    ConsistencyCheckAddons consistencyChecker;
     BoardNodeMap boardNodeMap;
     std::conditional_t<RECORD_LEADING_NODES, LeadingNodes, Dummy> leadingNodes;
 };
