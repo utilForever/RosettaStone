2345eaa4d418b7269b9e8ed054f76d9351b62c41
Chris Ohk <utilForever@gmail.com>
2019-07-17 23:04:11 +0900

    fix: Add missing code to remove entity from previous zone

--- Sources/Rosetta/Tasks/SimpleTasks/MoveToGraveyardTask.cpp
+++ Sources/Rosetta/Tasks/SimpleTasks/MoveToGraveyardTask.cpp
@@ -31,6 +31,7 @@ TaskStatus MoveToGraveyardTask::Impl(Player& player)
 
     for (auto& entity : entities)
     {
+        entity->zone->Remove(*entity);
         entity->owner->GetGraveyardZone().Add(*entity);
     }
 
