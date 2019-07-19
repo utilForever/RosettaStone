45cfcaba6c50409dc2df2f32dcadb28616b562e3
Chris Ohk <utilForever@gmail.com>
2019-07-20 00:54:25 +0900

    fix: Rework CopyTask logic
    - Add member variable: m_addToStack
    - Change member variable name: m_isOpposite -> m_toOpposite
    - Change the order of arguments in constructor

--- Sources/Rosetta/Tasks/SimpleTasks/CopyTask.cpp
+++ Sources/Rosetta/Tasks/SimpleTasks/CopyTask.cpp
@@ -3,20 +3,22 @@
 // RosettaStone is hearthstone simulator using C++ with reinforcement learning.
 // Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
 
+#include <Rosetta/Actions/Copy.hpp>
 #include <Rosetta/Actions/Generic.hpp>
 #include <Rosetta/Cards/Cards.hpp>
 #include <Rosetta/Games/Game.hpp>
+#include <Rosetta/Models/Enchantment.hpp>
 #include <Rosetta/Tasks/SimpleTasks/CopyTask.hpp>
-#include <Rosetta/Tasks/SimpleTasks/IncludeTask.hpp>
-#include <iostream>
+
 namespace RosettaStone::SimpleTasks
 {
-CopyTask::CopyTask(EntityType entityType, int amount, bool isOpposite,
-                   ZoneType zoneType)
+CopyTask::CopyTask(EntityType entityType, ZoneType zoneType, int amount,
+                   bool addToStack, bool toOpposite)
     : ITask(entityType),
+      m_zoneType(zoneType),
       m_amount(amount),
-      m_isOpposite(isOpposite),
-      m_zoneType(zoneType)
+      m_addToStack(addToStack),
+      m_toOpposite(toOpposite)
 {
     // Do nothing
 }
@@ -28,41 +30,102 @@ TaskID CopyTask::GetTaskID() const
 
 TaskStatus CopyTask::Impl(Player& player)
 {
+    Player& owner = (m_toOpposite) ? *player.opponent : player;
+    IZone* targetZone = Generic::GetZone(owner, m_zoneType);
+
+    if (targetZone == nullptr || targetZone->IsFull())
+    {
+        return TaskStatus::STOP;
+    }
+
     std::vector<Entity*> result;
 
-    Player* owner = (m_isOpposite) ? player.opponent : &player;
-    IZone* zone = Generic::GetZone(*owner, m_zoneType);
-
-    auto entities = IncludeTask::GetEntities(m_entityType, *owner, m_source, m_target);
-
-    if (entities.empty())
+    if (m_entityType == EntityType::STACK)
     {
-        return TaskStatus::STOP;
-    }
-
-    if (zone != nullptr && zone->IsFull())
-    {
-        return TaskStatus::STOP;
-    }
-
-    if (m_amount < 1 || (m_amount != 1 && entities.size() != 1))
-    {
-        return TaskStatus::STOP;
-    }
-
-    for (int i = 0; i < m_amount; ++i)
-    {
-        for (auto& entity : entities)
+        if (player.GetGame()->taskStack.entities.empty())
         {
-            Card card = 
-                Cards::GetInstance().FindCardByID(entity->card.id);
-            result.emplace_back(Entity::GetFromCard(
-                player, std::move(card), std::nullopt, zone
-            ));
+            return TaskStatus::STOP;
+        }
+
+        for (auto& entity : player.GetGame()->taskStack.entities)
+        {
+            Entity* copied = Generic::Copy(owner, entity, m_zoneType);
+
+            if (m_addToStack)
+            {
+                result.emplace_back(copied);
+            }
+
+            if (targetZone->IsFull())
+            {
+                if (m_addToStack)
+                {
+                    player.GetGame()->taskStack.entities = result;
+                }
+
+                return TaskStatus::COMPLETE;
+            }
         }
     }
-    
-    owner->GetGame()->taskStack.entities = result;
+    else
+    {
+        Entity* toBeCopied;
+        bool deathrattle = false;
+
+        switch (m_entityType)
+        {
+            case EntityType::SOURCE:
+            {
+                toBeCopied = m_source;
+
+                auto enchantment = dynamic_cast<Enchantment*>(m_target);
+                deathrattle =
+                    (m_zoneType == ZoneType::PLAY) &&
+                    (enchantment != nullptr) &&
+                    (!enchantment->card.power.GetDeathrattleTask().empty());
+                break;
+            }
+            case EntityType::TARGET:
+            {
+                toBeCopied = m_target;
+                break;
+            }
+            default:
+                throw std::invalid_argument(
+                    "CopyTask::Impl() - Invalid entity type");                
+        }
+
+        if (toBeCopied == nullptr)
+        {
+            return TaskStatus::STOP;
+        }
+
+        for (int i = 0; i < m_amount; ++i)
+        {
+            Entity* copied =
+                Generic::Copy(owner, toBeCopied, m_zoneType, deathrattle);
+
+            if (m_addToStack)
+            {
+                result.emplace_back(copied);
+            }
+
+            if (targetZone->IsFull())
+            {
+                if (m_addToStack)
+                {
+                    player.GetGame()->taskStack.entities = result;
+                }
+
+                return TaskStatus::COMPLETE;
+            }
+        }
+    }
+
+    if (m_addToStack)
+    {
+        player.GetGame()->taskStack.entities = result;
+    }
 
     return TaskStatus::COMPLETE;
 }
