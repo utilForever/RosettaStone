282a199f3ec769a6d640913193d8ff02eb7dee0f
Chris Ohk <utilForever@gmail.com>
2019-08-01 01:39:14 +0900

    feat(refactor-mcts): Create IAgent class

--- Extensions/RosettaTorch/Includes/Agents/IAgent.hpp
+++ Extensions/RosettaTorch/Includes/Agents/IAgent.hpp
@@ -10,10 +10,32 @@
 #ifndef ROSETTASTONE_TORCH_AGENTS_IAGENT_HPP
 #define ROSETTASTONE_TORCH_AGENTS_IAGENT_HPP
 
+#include <Rosetta/Views/BoardRefView.hpp>
+
 namespace RosettaTorch::Agents
 {
+//!
+//! \brief IAgent class.
+//!
+//! This class is interface of various agent classes.
+//! All classes that inherit from it must implement Think() related methods.
+//!
 class IAgent
 {
+ public:
+    //! Default virtual destructor.
+    virtual ~IAgent() = default;
+
+    //! Processes something before calling Think() method.
+    virtual void BeforeThink() = 0;
+
+    //! Processes something related to agent.
+    //! \param iteration The number of iteration.
+    virtual void Think(uint64_t iteration) = 0;
+
+    //! Processes something after calling Think() method.
+    //! \param iteration The number of iteration.
+    virtual void AfterThink(uint64_t iteration) = 0;
 };
 }  // namespace RosettaTorch::Agents
 
