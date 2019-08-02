fd8b1287a4f2601b52cd6c1b573229934e2c84df
JYPark09 <jyp10987@gmail.com>
2019-08-03 02:48:24 +0900

    test: fix deck code codecov issue

--- Tests/UnitTests/Commons/DeckStringTests.cpp
+++ Tests/UnitTests/Commons/DeckStringTests.cpp
@@ -12,6 +12,13 @@ using namespace RosettaStone;
 
 TEST(DeckString, ParseFromString)
 {
+    EXPECT_ANY_THROW(DeckString::ParseFromString("AAECAR8I"));
+    EXPECT_ANY_THROW(DeckString::ParseFromString("AQECAR8IxwOHBMkErgaggAOnggObhQPWmQMLngGoArUDxQj+DJjwAu/xAvWJA+aWA/mWA76YAwA="));
+    EXPECT_ANY_THROW(DeckString::ParseFromString("AAICAR8IxwOHBMkErgaggAOnggObhQPWmQMLngGoArUDxQj+DJjwAu/xAvWJA+aWA/mWA76YAwA="));
+    EXPECT_ANY_THROW(DeckString::ParseFromString("AAESAR8IxwOHBMkErgaggAOnggObhQPWmQMLngGoArUDxQj+DJjwAu/xAvWJA+aWA/mWA76YAwA="));
+    EXPECT_ANY_THROW(DeckString::ParseFromString("AAECAh8FCMcDhwTJBK4GoIADp4IDm4UD1pkDC54BqAK1A8UI/gyY8ALv8QL1iQPmlgP5lgO+mAMA"));
+    EXPECT_ANY_THROW(DeckString::ParseFromString("AAECAQAIxwOHBMkErgaggAOnggObhQPWmQMLngGoArUDxQj+DJjwAu/xAvWJA+aWA/mWA76YAwA="));
+
     DeckInfo info = DeckString::ParseFromString("AAECAR8IxwOHBMkErgaggAOnggObhQPWmQMLngGoArUDxQj+DJjwAu/xAvWJA+aWA/mWA76YAwA=");
 
     EXPECT_EQ(info.GetClass(), CardClass::HUNTER);
@@ -35,4 +42,28 @@ TEST(DeckString, ParseFromString)
     EXPECT_EQ(info.GetNumCardInDeck("BOT_548"), 1);  // Zilliax
     EXPECT_EQ(info.GetNumCardInDeck("DAL_378"), 2);  // Unleash the Beast
     EXPECT_EQ(info.GetNumCardInDeck("TRL_065"), 1);  // Zul'jin
+
+	DeckInfo info2 = DeckString::ParseFromString("AAECAR8IxwOHBMkErgaggAOnggObhQPWmQMAC54BAqgCArUDAsUIAv4MApjwAgLv8QIC9YkDAuaWAwL5lgMCvpgDAg==");
+
+    EXPECT_EQ(info.GetClass(), CardClass::HUNTER);
+    EXPECT_EQ(info.GetNumOfCards(), 30u);
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_080"), 2);   // Secretkeeper
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_610"), 1);   // Explosive Trap
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_611"), 1);   // Freezing Trap
+    EXPECT_EQ(info.GetNumCardInDeck("GIL_577"), 2);   // Rat Trap
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_554"), 1);   // Snake Trap
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_609"), 1);   // Snipe
+    EXPECT_EQ(info.GetNumCardInDeck("DAL_086"), 2);   // Sunreaver Spy
+    EXPECT_EQ(info.GetNumCardInDeck("NEW1_031"), 2);  // Animal Companion
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_617"), 2);   // Deadly Shot
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_536"), 2);   // Eaglehorn Bow
+    EXPECT_EQ(info.GetNumCardInDeck("EX1_539"), 2);   // Kill Command
+    EXPECT_EQ(info.GetNumCardInDeck("TRL_530"), 2);   // Masked Contender
+    EXPECT_EQ(info.GetNumCardInDeck("DAL_558"), 1);   // Archmage Vargoth
+    EXPECT_EQ(info.GetNumCardInDeck("GIL_622"), 2);   // Lifedrinker
+    EXPECT_EQ(info.GetNumCardInDeck("DAL_371"), 2);   // Marked Shot
+    EXPECT_EQ(info.GetNumCardInDeck("BOT_573"), 1);   // Subject 9
+    EXPECT_EQ(info.GetNumCardInDeck("BOT_548"), 1);   // Zilliax
+    EXPECT_EQ(info.GetNumCardInDeck("DAL_378"), 2);   // Unleash the Beast
+    EXPECT_EQ(info.GetNumCardInDeck("TRL_065"), 1);   // Zul'jin
 }
