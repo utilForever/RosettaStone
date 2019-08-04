32926a94b1aa875dd1dc18d576d21ecfd7f4b84f
Chris Ohk <utilForever@gmail.com>
2019-08-04 15:00:45 +0900

    feat(refactor-mcts): Separate header file and add comments for doxygen

--- Extensions/RosettaTorch/Includes/MCTS/Selection/BoardNodeMap.hpp
+++ Extensions/RosettaTorch/Includes/MCTS/Selection/BoardNodeMap.hpp
@@ -24,47 +24,32 @@ namespace RosettaTorch::MCTS
 {
 struct TreeNode;
 
+using MapType = std::unordered_map<ReducedBoardView, std::unique_ptr<TreeNode>>;
+
 //!
 //! \brief BoardNodeMap class.
 //!
+//! This class stores several boards that are reduced by hash function.
+//!
 class BoardNodeMap
 {
-    using MapType =
-        std::unordered_map<ReducedBoardView, std::unique_ptr<TreeNode>>;
-
  public:
+    //! Creates an new node or returns an node if the board already exists.
+    //! \param board The game board.
+    //! \param newNodeCreated The flag indicates whether to create new node.
+    //! \return An node that is newly created or is already existed.
     TreeNode* GetOrCreateNode(const Board& board,
                               bool* newNodeCreated = nullptr);
 
+    //! Runs \p functor on each element of the map.
+    //! \param functor A function to run for each element.
     template <typename Functor>
-    void ForEach(Functor&& functor) const
-    {
-        std::shared_lock<SharedSpinLock> lock(m_mutex);
-
-        if (!m_map)
-        {
-            return;
-        }
-
-        for (const auto& kv : *m_map)
-        {
-            if (!functor(kv.first, kv.second.get()))
-            {
-                return;
-            }
-        }
-    }
+    void ForEach(Functor&& functor) const;
 
  private:
-    MapType& GetLockedMap()
-    {
-        if (!m_map)
-        {
-            m_map.reset(new MapType());
-        }
-
-        return *m_map;
-    }
+    //! Returns the map that stores several boards.
+    //! \return The map that stores several boards.
+    MapType& GetLockedMap();
 
     mutable SharedSpinLock m_mutex;
     std::unique_ptr<MapType> m_map;
