dc07e2aaf3a7d20858bd044f870004c8716dbf70
Chris Ohk <utilForever@gmail.com>
2019-08-22 17:33:43 +0900

    feat(pytorch): Combine header and source file of MCTSAgent

--- Extensions/RosettaTorch/Includes/Agents/MCTSAgent.hpp
+++ Extensions/RosettaTorch/Includes/Agents/MCTSAgent.hpp
@@ -10,25 +10,66 @@
 #ifndef ROSETTASTONE_TORCH_AGENTS_MCTS_AGENT_HPP
 #define ROSETTASTONE_TORCH_AGENTS_MCTS_AGENT_HPP
 
-#include <Agents/IAgent.hpp>
 #include <Agents/MCTSConfig.hpp>
 #include <Agents/MCTSRunner.hpp>
 #include <MCTS/Selection/TreeNode.hpp>
 
+#include <effolkronium/random.hpp>
+
+using Random = effolkronium::random_static;
+
 namespace RosettaTorch::Agents
 {
+//!
+//! \brief DummyAgentCallback class.
+//!
+//! This class is dummy class for MCTS agent.
+//!
+class DummyAgentCallback
+{
+ public:
+    //! Default constructor.
+    DummyAgentCallback() = default;
+
+    //! Processes something before calling Think() method.
+    void BeforeThink()
+    {
+        // Do nothing
+    }
+
+    //! Processes something related to agent.
+    //! \param iteration The number of iteration.
+    void Think([[maybe_unused]] std::uint64_t iteration)
+    {
+        // Do nothing
+    }
+
+    //! Processes something after calling Think() method.
+    //! \param iteration The number of iteration.
+    void AfterThink([[maybe_unused]] std::uint64_t iteration)
+    {
+        // Do nothing
+    }
+};
+
 //!
 //! \brief MCTSAgent class.
 //!
 //! This class is simple agent to run MCTS runner.
 //!
+template <class AgentCallback = DummyAgentCallback>
 class MCTSAgent
 {
  public:
-    //! Constructs MCTS agent with given \p config and \p agent.
+    //! Constructs MCTS agent with given \p config and \p callback.
     //! \param config The MCTS config.
-    //! \param agent The agent to define Think() related methods.
-    MCTSAgent(const MCTSConfig& config, IAgent& agent);
+    //! \param callback The agent callback.
+    MCTSAgent(const MCTSConfig& config,
+              AgentCallback callback = AgentCallback())
+        : m_config(config), m_callback(callback)
+    {
+        // Do nothing
+    }
 
     //! Deleted copy constructor.
     MCTSAgent(const MCTSAgent&) = delete;
@@ -44,20 +85,132 @@ class MCTSAgent
 
     //! Process Think() related methods.
     //! \param gameConfig The game config.
-    void Think(const GameConfig& gameConfig);
+    void Think(const GameConfig& gameConfig)
+    {
+        m_callback.BeforeThink();
+
+        m_controller.reset(new MCTSRunner(m_config));
+        m_controller->Run(gameConfig);
+
+        while (true)
+        {
+            const uint64_t iterations =
+                m_controller->GetStatistics().GetSuccededIterates();
+            m_callback.Think(iterations);
+
+            if (iterations >=
+                static_cast<uint64_t>(m_config.iterationsPerAction))
+            {
+                break;
+            }
+
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(m_config.callbackInterval));
+        }
+
+        m_controller->WaitUntilStopped();
+
+        m_callback.AfterThink(
+            m_controller->GetStatistics().GetSuccededIterates());
+
+        m_node = m_controller->GetRootNode(gameConfig.startPlayer);
+        m_rootNode = m_node;
+    }
 
     //! Returns action according to \p actionType and \p choices.
     //! \param actionType The type of action.
     //! \param choices The choices of action.
     //! \return The index of chosen action.
-    int GetAction(ActionType actionType, ActionChoices choices);
+    int GetAction(ActionType actionType, ActionChoices choices)
+    {
+        if (actionType != ActionType::MAIN_ACTION)
+        {
+            if (choices.Size() == 1)
+            {
+                return 0;
+            }
+        }
+
+        if (!m_node->addon.consistencyChecker.LockAndCheckActionType(
+                actionType))
+        {
+            throw std::runtime_error("Action type not match");
+        }
+
+        auto canBeChosen = [&](int choice) {
+            for (choices.Begin(); !choices.IsEnd(); choices.StepNext())
+            {
+                if (static_cast<int>(choices.Get()) == choice)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        };
+
+        struct Item
+        {
+            double value;
+            int choice;
+            const MCTS::TreeNode* node;
+        };
+
+        std::vector<Item> items;
+        double totalValue = 0.0;
+
+        double temperature = m_config.actionFollowTemperature;
+        if (temperature < 0.1)
+        {
+            temperature = 0.1;
+        }
+
+        m_node->children.ForEach([&](int choice,
+                                     const MCTS::EdgeAddon* edgeAddon,
+                                     MCTS::TreeNode* child) {
+            if (!canBeChosen(choice))
+            {
+                return true;
+            }
+
+            auto choiceValue = static_cast<double>(edgeAddon->GetChosenTimes());
+            choiceValue = pow(choiceValue, 1.0 / temperature);
+            totalValue += choiceValue;
+
+            items.push_back({ choiceValue, choice, child });
+            return true;
+        });
+
+        // Normalize
+        double accumulated = 0.0;
+        for (auto& item : items)
+        {
+            const double normalized = item.value / totalValue;
+            accumulated += normalized;
+            item.value = accumulated;
+        }
+
+        const auto v = Random::get<double>(0.0, accumulated);
+        for (const auto& item : items)
+        {
+            if (v < item.value)
+            {
+                m_node = item.node;
+                return item.choice;
+            }
+        }
+
+        // If goes here, the only possible reason is we don't have any child
+        // nodes no any choice is evaluated. randomly choose one.
+        return Random::get<int>(0, choices.Size() - 1);
+    }
 
  private:
     MCTSConfig m_config;
     const MCTS::TreeNode* m_rootNode = nullptr;
     const MCTS::TreeNode* m_node = nullptr;
     std::unique_ptr<MCTSRunner> m_controller = nullptr;
-    IAgent& m_agent;
+    AgentCallback m_callback;
 };
 }  // namespace RosettaTorch::Agents
 
