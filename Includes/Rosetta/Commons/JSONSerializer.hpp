2eb8e944b7106497b405e554d5f85e138ec2ce42
Chris Ohk <utilForever@gmail.com>
2019-08-25 10:09:06 +0900

    [ci skip] [WIP] feat(pytorch): Create class 'JSONSerializer'

--- Includes/Rosetta/Commons/JSONSerializer.hpp
+++ Includes/Rosetta/Commons/JSONSerializer.hpp
@@ -54,32 +54,36 @@ class JSONSerializer
 
         obj["current_player"] = GetPlayerString(game.GetCurrentPlayer());
         obj["turn"] = game.GetTurn();
-        obj["player1"] = SerializePlayer(game, game.GetPlayer1());
-        obj["player2"] = SerializePlayer(game, game.GetPlayer2());
+        obj["player1"] = SerializePlayer(game.GetPlayer1());
+        obj["player2"] = SerializePlayer(game.GetPlayer2());
 
         return obj;
     }
 
     //! Serializes the player data.
-    //! \param game The game context.
     //! \param player The player context.
     //! \return The serialized player data.
-    static nlohmann::json SerializePlayer(const Game& game,
-                                          const Player& player)
+    static nlohmann::json SerializePlayer(const Player& player)
     {
         nlohmann::json obj;
 
-        obj["hero"] = SerializeHero(game, *player.GetHero());
-        // obj["hero_power"] = SerializeHeroPower(game, player.GetHeroPower());
+        obj["hero"] = SerializeHero(*player.GetHero());
+        obj["hero_power"] = SerializeHeroPower(player.GetHeroPower());
+        obj["weapon"] = SerializeWeapon(player.GetWeapon());
+        obj["mana_crystal"] = SerializeManaCrystal(player);
+        obj["fatigue"] = player.GetHero()->fatigue;
+
+        obj["deck"] = SerializeDeck(player.GetDeckZone());
+        obj["hand"] = SerializeHand(player.GetHandZone());
+        obj["minions"] = SerializeField(player.GetFieldZone());
 
         return obj;
     }
 
     //! Serializes the hero data.
-    //! \param game The game context.
     //! \param hero The hero context.
     //! \return The serialized hero data.
-    static nlohmann::json SerializeHero(const Game& game, const Hero& hero)
+    static nlohmann::json SerializeHero(const Hero& hero)
     {
         nlohmann::json obj;
 
@@ -88,6 +92,113 @@ class JSONSerializer
         obj["max_health"] = hero.GetMaxHealth();
         obj["attack"] = hero.GetAttack();
         obj["armor"] = hero.GetArmor();
+
+        return obj;
+    }
+
+    //! Serializes the hero power data.
+    //! \param heroPower The hero power context.
+    //! \return The serialized hero power data.
+    static nlohmann::json SerializeHeroPower(const HeroPower& heroPower)
+    {
+        nlohmann::json obj;
+
+        obj["card_id"] = heroPower.card->id;
+        obj["usable"] = !heroPower.IsExhausted();
+
+        return obj;
+    }
+
+    //! Serializes the weapon data.
+    //! \param weapon The weapon context.
+    //! \return The serialized weapon data.
+    static nlohmann::json SerializeWeapon(const Weapon& weapon)
+    {
+        nlohmann::json obj;
+
+        obj["card_id"] = weapon.card->id;
+        obj["attack"] = weapon.GetAttack();
+        obj["durability"] = weapon.GetDurability();
+
+        return obj;
+    }
+
+    //! Serializes the mana crystal data.
+    //! \param player The player context.
+    //! \return The serialized mana crystal data.
+    static nlohmann::json SerializeManaCrystal(const Player& player)
+    {
+        nlohmann::json obj;
+
+        obj["remaining"] = player.GetRemainingMana();
+        obj["total"] = player.GetTotalMana();
+        obj["overload_owed"] = player.GetOverloadOwed();
+        obj["overload_locked"] = player.GetOverloadLocked();
+
+        return obj;
+    }
+
+    //! Serializes the deck data.
+    //! \param deck The deck context.
+    //! \return The serialized deck data.
+    static nlohmann::json SerializeDeck(const DeckZone& deck)
+    {
+        nlohmann::json obj;
+
+        obj["count"] = deck.GetCount();
+
+        return obj;
+    }
+
+    //! Serializes the hand data.
+    //! \param hand The hand context.
+    //! \return The serialized hand data.
+    static nlohmann::json SerializeHand(const HandZone& hand)
+    {
+        nlohmann::json obj;
+
+        for (const auto& card : hand.GetAll())
+        {
+            obj.emplace_back(SerializeHandCard(*card));
+        }
+
+        return obj;
+    }
+
+    //! Serializes the field data.
+    //! \param field The field context.
+    //! \return The serialized field data.
+    static nlohmann::json SerializeField(const FieldZone& field)
+    {
+        nlohmann::json obj;
+
+        for (const auto& card : field.GetAll())
+        {
+            obj.emplace_back(SerializeMinion(*card));
+        }
+
+        return obj;
+    }
+
+    //! Serializes the hand card data.
+    //! \param handCard The hand card context.
+    //! \return The serialized hand card data.
+    static nlohmann::json SerializeHandCard(const Entity& handCard)
+    {
+        nlohmann::json obj;
+
+        obj["card_id"] = handCard.card->id;
+        obj["cost"] = handCard.GetCost();
+
+        return obj;
+    }
+
+    //! Serializes the minion data.
+    //! \param minion The minion context.
+    //! \return The serialized minion data.
+    static nlohmann::json SerializeMinion(const Minion& minion)
+    {
+        nlohmann::json obj;
 
         return obj;
     }
