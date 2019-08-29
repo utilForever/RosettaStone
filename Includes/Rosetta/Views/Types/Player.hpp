d8989547618c3264519bf93bcbb3ebd5781ea0b9
Chris Ohk <utilForever@gmail.com>
2019-08-29 20:15:03 +0900

    [ci skip] [WIP] feat(pytorch): Create class 'Player'

---
+++ Includes/Rosetta/Views/Types/Player.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+// It is based on peter1591's hearthstone-ai repository.
+// References: https://github.com/peter1591/hearthstone-ai
+
+#ifndef ROSETTASTONE_VIEW_TYPES_PLAYER_HPP
+#define ROSETTASTONE_VIEW_TYPES_PLAYER_HPP
+
+#include <Rosetta/Models/Player.hpp>
+
+namespace RosettaStone::ViewTypes
+{
+//!
+//! \brief Player class.
+//!
+//!
+class Player
+{
+};
+}  // namespace RosettaStone::ViewTypes
+
+#endif  // ROSETTASTONE_VIEW_TYPES_PLAYER_HPP
