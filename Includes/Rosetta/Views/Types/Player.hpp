8f1a69a129785cf24277372304212648d202c5fb
Chris Ohk <utilForever@gmail.com>
2019-09-02 02:32:03 +0900

    [ci skip] [WIP] feat(pytorch): Add method 'Parse()' for several structs
    - Hero/HeroPower/ManaCrystal/Minions

--- Includes/Rosetta/Views/Types/Player.hpp
+++ Includes/Rosetta/Views/Types/Player.hpp
@@ -7,20 +7,64 @@
 // It is based on peter1591's hearthstone-ai repository.
 // References: https://github.com/peter1591/hearthstone-ai
 
-#ifndef ROSETTASTONE_VIEW_TYPES_PLAYER_HPP
-#define ROSETTASTONE_VIEW_TYPES_PLAYER_HPP
+#ifndef ROSETTASTONE_VIEWS_TYPES_PLAYER_HPP
+#define ROSETTASTONE_VIEWS_TYPES_PLAYER_HPP
 
 #include <Rosetta/Models/Player.hpp>
+#include <Rosetta/Views/BoardRefView.hpp>
+#include <Rosetta/Views/Types/CardInfo.hpp>
 
-namespace RosettaStone::ViewTypes
+namespace RosettaStone::Views::Types
 {
 //!
-//! \brief Player class.
+//! \brief Hero struct.
 //!
-//!
-class Player
+struct Hero
 {
+    void Parse(BoardRefView gameState, PlayerType side);
 };
-}  // namespace RosettaStone::ViewTypes
 
-#endif  // ROSETTASTONE_VIEW_TYPES_PLAYER_HPP
+//!
+//! \brief HeroPower struct.
+//!
+struct HeroPower
+{
+    void Parse(BoardRefView gameState, PlayerType side);
+};
+
+//!
+//! \brief ManaCrystal struct.
+//!
+struct ManaCrystal
+{
+    void Parse(int remainingMana, int totalMana, int overloadOwedMana,
+               int overloadLockedMana);
+};
+
+//!
+//! \brief Minions struct.
+//!
+struct Minions
+{
+    void Parse(BoardRefView gameState, PlayerType side);
+};
+
+//!
+//! \brief Player struct.
+//!
+struct Player
+{
+    void Parse(BoardRefView gameState, PlayerType side);
+
+    Hero hero;
+    HeroPower heroPower;
+    ManaCrystal manaCrystal;
+    Minions minions;
+    int fatigue;
+
+    std::vector<CardInfo> hand;
+    std::vector<CardInfo> deck;
+};
+}  // namespace RosettaStone::Views::Types
+
+#endif  // ROSETTASTONE_VIEWS_TYPES_PLAYER_HPP
