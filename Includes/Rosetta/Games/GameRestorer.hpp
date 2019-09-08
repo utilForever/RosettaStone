a3099b841f6afd1743af67f678211eb2e9d8e028
Chris Ohk <utilForever@gmail.com>
2019-09-08 10:27:10 +0900

    feat(pytorch): Implement methods of class 'GameRestorer'
    - Prepare(), RestoreGame(), MakePlayer(), MakeDec(), MakeHand(), MakeMinions(), MakeManaCrystal(), AddMinion()

--- Includes/Rosetta/Games/GameRestorer.hpp
+++ Includes/Rosetta/Games/GameRestorer.hpp
@@ -20,12 +20,50 @@ namespace RosettaStone
 class GameRestorer
 {
  public:
-    GameRestorer(const BoardView& view) : m_view(view)
-    {
-    }
+    GameRestorer(const BoardView& view);
+
+    static GameRestorer Prepare(BoardView& view,
+                                Views::Types::UnknownCardsInfo& p1Unknown,
+                                Views::Types::UnknownCardsInfo& p2Unknown);
+
+    Game RestoreGame();
 
  private:
+    void MakePlayer(
+        PlayerType playerType, Game& game,
+        const Views::Types::Player& viewPlayer,
+        const Views::Types::UnknownCardsSetsManager& unknownCardsSetsManager);
+
+    void MakeHeroAndHeroPower(PlayerType playerType, Game& game,
+                              const Views::Types::Hero& hero,
+                              const Views::Types::HeroPower& heroPower);
+
+    void MakeDeck(
+        PlayerType playerType, Game& game,
+        std::vector<Views::Types::CardInfo> cards,
+        const Views::Types::UnknownCardsSetsManager& unknownCardsSetsManager);
+
+    void MakeHand(
+        PlayerType playerType, Game& game,
+        std::vector<Views::Types::CardInfo> cards,
+        const Views::Types::UnknownCardsSetsManager& unknownCardsSetsManager);
+
+    void MakeMinions(PlayerType playerType, Game& game,
+                     const Views::Types::Minions& minions);
+
+    void MakeManaCrystal(Player& player,
+                         const Views::Types::ManaCrystal& manaCrystal);
+
+    void AddMinion(PlayerType playerType, Game& game,
+                   const Views::Types::Minion& minion, int pos);
+
     const BoardView& m_view;
+
+    Views::Types::UnknownCardsInfo p1UnknownCardsInfo;
+    Views::Types::UnknownCardsInfo p2UnknownCardsInfo;
+
+    Views::Types::UnknownCardsSetsManager p1UnknownCardsManager;
+    Views::Types::UnknownCardsSetsManager p2UnknownCardsManager;
 };
 }  // namespace RosettaStone
 
