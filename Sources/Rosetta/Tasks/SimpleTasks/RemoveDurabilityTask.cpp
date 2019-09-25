ec173b371b857a1dc715a2e8fc3b5231cecf21be
HyeonSu Kim <rla92233@naver.com>
2019-09-25 11:06:11 +0900

    impl: Implement RemoveDurabilityTask

---
+++ Sources/Rosetta/Tasks/SimpleTasks/RemoveDurabilityTask.cpp
@@ -0,0 +1,33 @@
+#include <Rosetta/Models/Weapon.hpp>
+#include <Rosetta/Tasks/SimpleTasks/RemoveDurabilityTask.hpp>
+
+namespace RosettaStone::SimpleTasks
+{
+RemoveDurabilityTask::RemoveDurabilityTask(int amount, bool isOpponent)
+    : m_amount(amount), m_isOpponent(isOpponent)
+{
+    // Do nothing
+}
+
+TaskID RemoveDurabilityTask::GetTaskID() const
+{
+    return TaskID::REMOVE_DURABILITY;
+}
+
+TaskStatus RemoveDurabilityTask::Impl(Player& player)
+{
+    if (Weapon* weapon = m_isOpponent ? player.opponent->GetHero()->weapon
+                                      : player.GetHero()->weapon;
+        weapon != nullptr)
+    {
+        weapon->RemoveDurability(m_amount);
+    }
+
+    return TaskStatus::COMPLETE;
+}
+
+ITask* RemoveDurabilityTask::CloneImpl()
+{
+    return new RemoveDurabilityTask(m_amount, m_isOpponent);
+}
+}  // namespace RosettaStone::SimpleTasks
