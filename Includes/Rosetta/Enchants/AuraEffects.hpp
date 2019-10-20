64ded34812af29cf8815a71bdc2479eb5fc93f80
Chris Ohk <utilForever@gmail.com>
2019-10-20 23:21:42 +0900

    feat(impl-card): Add getters/setters instead of using 'm_gameTag'

--- Includes/Rosetta/Enchants/AuraEffects.hpp
+++ Includes/Rosetta/Enchants/AuraEffects.hpp
@@ -8,17 +8,15 @@
 
 #include <Rosetta/Enums/CardEnums.hpp>
 
-#include <map>
-
 namespace RosettaStone
 {
 class Entity;
 
-constexpr int AURA_EFFECT_CARD_SIZE = 2;
+constexpr int AURA_EFFECT_CARD_SIZE = 0;
 constexpr int AURA_EFFECT_WEAPON_SIZE = AURA_EFFECT_CARD_SIZE + 1;
 constexpr int AURA_EFFECT_CHARACTER_SIZE = AURA_EFFECT_CARD_SIZE + 2;
 constexpr int AURA_EFFECT_HERO_SIZE = AURA_EFFECT_CHARACTER_SIZE + 3;
-constexpr int AURA_EFFECT_MINION_SIZE = AURA_EFFECT_CHARACTER_SIZE + 7;
+constexpr int AURA_EFFECT_MINION_SIZE = AURA_EFFECT_CHARACTER_SIZE + 5;
 
 //!
 //! \brief AuraEffects class.
@@ -43,11 +41,112 @@ class AuraEffects
     //! \param value The value to set for game tag.
     void SetGameTag(GameTag tag, int value);
 
+    //! Returns the value of GameTag::IMMUNE.
+    //! \return The value of GameTag::IMMUNE.
+    int GetImmune() const;
+
+    //! Sets the value of GameTag::IMMUNE.
+    //! \param value The value of GameTag::IMMUNE to set.
+    void SetImmune(int value);
+
+    //! Returns the value of GameTag::CANT_BE_TARGETED_BY_SPELLS and
+    //! GameTag::CANT_BE_TARGETED_BY_HERO_POWERS.
+    //! \return The value of GameTag::CANT_BE_TARGETED_BY_SPELLS and
+    //! GameTag::CANT_BE_TARGETED_BY_HERO_POWERS.
+    int GetCantBeTargetedBySpells() const;
+
+    //! Sets the value of GameTag::CANT_BE_TARGETED_BY_SPELLS and
+    //! GameTag::CANT_BE_TARGETED_BY_HERO_POWERS.
+    //! \param value The value of GameTag::CANT_BE_TARGETED_BY_SPELLS and
+    //! GameTag::CANT_BE_TARGETED_BY_HERO_POWERS to set.
+    void SetCantBeTargetedBySpells(int value);
+
+    //! Returns the value of GameTag::ATK.
+    //! \return The value of GameTag::ATK.
+    int GetAttack() const;
+
+    //! Sets the value of GameTag::ATK.
+    //! \param value The value of GameTag::ATK to set.
+    void SetAttack(int value);
+
+    //! Returns the value of GameTag::CANNOT_ATTACK_HEROES.
+    //! \return The value of GameTag::CANNOT_ATTACK_HEROES.
+    int GetCannotAttackHeroes() const;
+
+    //! Sets the value of GameTag::CANNOT_ATTACK_HEROES.
+    //! \param value The value of GameTag::CANNOT_ATTACK_HEROES to set.
+    void SetCannotAttackHeroes(int value);
+
+    //! Returns the value of GameTag::HEROPOWER_DAMAGE.
+    //! \return The value of GameTag::HEROPOWER_DAMAGE.
+    int GetHeroPowerDamage() const;
+
+    //! Sets the value of GameTag::HEROPOWER_DAMAGE.
+    //! \param value The value of GameTag::HEROPOWER_DAMAGE to set.
+    void SetHeroPowerDamage(int value);
+
+    //! Returns the value of GameTag::HEALTH.
+    //! \return The value of GameTag::HEALTH.
+    int GetHealth() const;
+
+    //! Sets the value of GameTag::HEALTH.
+    //! \param value The value of GameTag::HEALTH to set.
+    void SetHealth(int value);
+
+    //! Returns the value of GameTag::CHARGE.
+    //! \return The value of GameTag::CHARGE.
+    int GetCharge() const;
+
+    //! Sets the value of GameTag::CHARGE.
+    //! \param value The value of GameTag::CHARGE to set.
+    void SetCharge(int value);
+
+    //! Returns the value of GameTag::TAUNT.
+    //! \return The value of GameTag::TAUNT.
+    int GetTaunt() const;
+
+    //! Sets the value of GameTag::TAUNT.
+    //! \param value The value of GameTag::TAUNT to set.
+    void SetTaunt(int value);
+
+    //! Returns the value of GameTag::LIFESTEAL.
+    //! \return The value of GameTag::LIFESTEAL.
+    int GetLifesteal() const;
+
+    //! Sets the value of GameTag::LIFESTEAL.
+    //! \param value The value of GameTag::LIFESTEAL to set.
+    void SetLifesteal(int value);
+
+    //! Returns the value of GameTag::CANT_ATTACK.
+    //! \return The value of GameTag::CANT_ATTACK.
+    int GetCantAttack() const;
+
+    //! Sets the value of GameTag::CANT_ATTACK.
+    //! \param value The value of GameTag::CANT_ATTACK to set.
+    void SetCantAttack(int value);
+
  private:
     CardType m_type = CardType::INVALID;
-    int* m_data = nullptr;
 
-    std::map<GameTag, int> m_gameTags;
+    // Indices:
+    // Cards
+    // ...
+    // Weapon
+    // 0 : IMMUNE
+    // Character
+    // 1 : CANT_BE_TARGETED_BY_SPELLS / CANT_BE_TARGETED_BY_HERO_POWERS
+    // 2 : ATK
+    // Hero
+    // 3 : CANNOT_ATTACK_HEROES
+    // 4 : IMMUNE
+    // 5 : HEROPOWER_DAMAGE
+    // Minion
+    // 3 : HEALTH
+    // 4 : CHARGE
+    // 5 : TAUNT
+    // 6 : LIFESTEAL
+    // 7 : CANT_ATTACK
+    int* m_data = nullptr;
 };
 }  // namespace RosettaStone
 
