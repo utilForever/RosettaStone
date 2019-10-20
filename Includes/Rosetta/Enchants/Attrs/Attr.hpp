8f417a70e237bb507749d6a238108fc27e8890f0
Chris Ohk <utilForever@gmail.com>
2019-10-21 02:18:07 +0900

    feat(impl-card): Create class 'Attr'

---
+++ Includes/Rosetta/Enchants/Attrs/Attr.hpp
@@ -0,0 +1,28 @@
+// This code is based on Sabberstone project.
+// Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+// RosettaStone is hearthstone simulator using C++ with reinforcement learning.
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+#ifndef ROSETTASTONE_ATTR_HPP
+#define ROSETTASTONE_ATTR_HPP
+
+#include <Rosetta/Models/Entity.hpp>
+
+namespace RosettaStone
+{
+template <typename T = Entity>
+class Attr<T>
+{
+    virtual ~Attr() = default;
+
+    virtual void Apply(T* entity, EffectOperator effectOp, int value) = 0;
+
+    virtual void ApplyAura(T* entity, EffectOperator effectOp, int value) = 0;
+
+    virtual void Remove(T* entity, EffectOperator effectOp, int value) = 0;
+
+    virtual void RemoveAura(T* entity, EffectOperator effectOp, int value) = 0;
+};
+}  // namespace RosettaStone
+
+#endif  // ROSETTASTONE_ATTR_HPP
