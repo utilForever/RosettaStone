7b8c79c22dd4808ea0bfbe47d5fd5cc32e4af072
Chris Ohk <utilForever@gmail.com>
2019-11-01 21:05:39 +0900

    feat(impl-card): Create class 'SummonOpTask'
    - This class represents the task for summoning minion at opponent's battlefield

--- Includes/Rosetta/RosettaStone.hpp
+++ Includes/Rosetta/RosettaStone.hpp
@@ -143,6 +143,7 @@
 #include <Rosetta/Tasks/SimpleTasks/ReturnHandTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/SetGameTagTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/SilenceTask.hpp>
+#include <Rosetta/Tasks/SimpleTasks/SummonOpTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/SummonTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/SwapAttackHealthTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/TempManaTask.hpp>
