4b2984783efa801881ddce8ff6bf063cc253465b
Chris Ohk <utilForever@gmail.com>
2019-11-04 20:14:32 +0900

    feat(impl-card): Add method 'ReqTargetWithRace()' and related methods
    - ReqTargetWithRace(): Predicate wrapper for checking the target requires that it has race
    - Related methods: ReqMurlocTarget(), ReqDemonTarget(), ReqMechanicalTarget(), ReqElementalTarget(), ReqBeastTarget(), ReqTotemTarget(), ReqPirateTarget(), ReqDragonTarget()

--- Includes/Rosetta/Loaders/TargetingPredicates.hpp
+++ Includes/Rosetta/Loaders/TargetingPredicates.hpp
@@ -7,6 +7,8 @@
 #ifndef ROSETTASTONE_TARGETING_PREDICATES_HPP
 #define ROSETTASTONE_TARGETING_PREDICATES_HPP
 
+#include <Rosetta/Enums/CardEnums.hpp>
+
 #include <functional>
 
 namespace RosettaStone
@@ -26,6 +28,51 @@ using AvailabilityPredicate = std::function<bool(Player*, Card*)>;
 class TargetingPredicates
 {
  public:
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::MURLOC.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqMurlocTarget();
+
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::DEMON.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqDemonTarget();
+
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::MECHANICAL.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqMechanicalTarget();
+
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::ELEMENTAL.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqElementalTarget();
+
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::BEAST.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqBeastTarget();
+
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::TOTEM.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqTotemTarget();
+
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::PIRATE.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqPirateTarget();
+
+    //! Predicate wrapper for checking the target requires that
+    //! it has Race::DRAGON.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqDragonTarget();
+
+    //! Predicate wrapper for checking the target requires that it has \p race.
+    //! \param race The value of race.
+    //! \return Generated TargetingPredicate for intended purpose.
+    static TargetingPredicate ReqTargetWithRace(Race race);
+
     //! Predicate wrapper for checking the target requires combo active.
     //! \return Generated AvailabilityPredicate for intended purpose.
     static AvailabilityPredicate ReqTargetForCombo();
--- Sources/Rosetta/Loaders/TargetingPredicates.cpp
+++ Sources/Rosetta/Loaders/TargetingPredicates.cpp
@@ -9,6 +9,79 @@
 
 namespace RosettaStone
 {
+TargetingPredicate TargetingPredicates::ReqMurlocTarget()
+{
+    return [](Character* character) { return character->IsRace(Race::MURLOC); };
+}
+
+TargetingPredicate TargetingPredicates::ReqDemonTarget()
+{
+    return [](Character* character) { return character->IsRace(Race::DEMON); };
+}
+
+TargetingPredicate TargetingPredicates::ReqMechanicalTarget()
+{
+    return [](Character* character) {
+        return character->IsRace(Race::MECHANICAL);
+    };
+}
+
+TargetingPredicate TargetingPredicates::ReqElementalTarget()
+{
+    return
+        [](Character* character) { return character->IsRace(Race::ELEMENTAL); };
+}
+
+TargetingPredicate TargetingPredicates::ReqBeastTarget()
+{
+    return [](Character* character) { return character->IsRace(Race::BEAST); };
+}
+
+TargetingPredicate TargetingPredicates::ReqTotemTarget()
+{
+    return [](Character* character) { return character->IsRace(Race::TOTEM); };
+}
+
+TargetingPredicate TargetingPredicates::ReqPirateTarget()
+{
+    return [](Character* character) { return character->IsRace(Race::PIRATE); };
+}
+
+TargetingPredicate TargetingPredicates::ReqDragonTarget()
+{
+    return [](Character* character) { return character->IsRace(Race::DRAGON); };
+}
+
+TargetingPredicate TargetingPredicates::ReqTargetWithRace(Race race)
+{
+    switch (race)
+    {
+        case Race::MURLOC:
+            return ReqMurlocTarget();
+        case Race::DEMON:
+            return ReqDemonTarget();
+        case Race::MECHANICAL:
+            return ReqMechanicalTarget();
+        case Race::ELEMENTAL:
+            return ReqElementalTarget();
+        case Race::BEAST:
+            return ReqBeastTarget();
+        case Race::TOTEM:
+            return ReqTotemTarget();
+        case Race::PIRATE:
+            return ReqPirateTarget();
+        case Race::DRAGON:
+            return ReqDragonTarget();
+        case Race::UNDEAD:
+        case Race::EGG:
+            return []([[maybe_unused]] Character* character) { return true; };
+        default:
+            throw std::invalid_argument(
+                "TargetingPredicates::ReqTargetWithRace() - "
+                "Race is not implemented!");
+    }
+}
+
 AvailabilityPredicate TargetingPredicates::ReqTargetForCombo()
 {
     return [](Player* player, [[maybe_unused]] Card* card) {
