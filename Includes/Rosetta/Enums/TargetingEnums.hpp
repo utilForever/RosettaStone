326451d23fe6f1bf9c2d184a014bbc85f82e0af4
Chris Ohk <utilForever@gmail.com>
2019-11-04 19:53:22 +0900

    feat(impl-card): Create enum 'CharacterType' and 'FriendlyType'
    - CharacterType: An enumerator for identifying character type
    - FriendlyType: An enumerator for identifying friendly type

--- Includes/Rosetta/Enums/TargetingEnums.hpp
+++ Includes/Rosetta/Enums/TargetingEnums.hpp
@@ -21,6 +21,22 @@ enum class TargetingType
     ENEMY_MINIONS,
     HEROES
 };
+
+//! \brief An enumerator for identifying character type.
+enum class CharacterType
+{
+    CHARACTERS,
+    HEROES,
+    MINIONS
+};
+
+//! \brief An enumerator for identifying friendly type.
+enum class FriendlyType
+{
+    ALL,
+    FRIENDLY,
+    ENEMY
+};
 }  // namespace RosettaStone
 
 #endif  // ROSETTASTONE_TARGETING_ENUMS_HPP
