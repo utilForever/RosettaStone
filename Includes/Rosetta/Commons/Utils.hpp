e001029c100c3a057f04944251ee9fc29374412c
Chris Ohk <utilForever@gmail.com>
2019-11-08 12:37:04 +0900

    refactor: Pass parameter 'list' as const reference for performance

--- Includes/Rosetta/Commons/Utils.hpp
+++ Includes/Rosetta/Commons/Utils.hpp
@@ -72,7 +72,7 @@ void EraseIf(ContainerT& items, const PredicateT& predicate)
 //! \param amount The number of elements to choose.
 //! \return A list of N distinct elements.
 template <typename T>
-std::vector<T*> ChooseNElements(std::vector<T*> list, int amount)
+std::vector<T*> ChooseNElements(const std::vector<T*>& list, int amount)
 {
     const int size = static_cast<int>(list.size());
     if (amount > size)
