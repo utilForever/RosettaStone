e637ccd9fdde54c51c4285839dd9f559d8de416a
Chris Ohk <utilForever@gmail.com>
2019-11-16 16:16:42 +0900

    feat(alphazero): Create struct 'RunOptions'
    - This struct holds the options of the optimizer

---
+++ Extensions/RosettaTorch/Includes/AlphaZero/Optimizer/RunOptions.hpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+// It is based on peter1591's hearthstone-ai repository.
+// References: https://github.com/peter1591/hearthstone-ai
+
+#ifndef ROSETTASTONE_TORCH_ALPHA_ZERO_OPTIMIZER_RUN_OPTIONS_HPP
+#define ROSETTASTONE_TORCH_ALPHA_ZERO_OPTIMIZER_RUN_OPTIONS_HPP
+
+namespace RosettaTorch::AlphaZero::Optimizer
+{
+//!
+//! \brief RunOptions struct.
+//!
+//! This struct holds the options of the optimizer.
+//!
+struct RunOptions
+{
+    int batchSize = 32;
+    int batches = 100;
+    int epoches = 10000;
+    int epochesForRun = 100;
+    double maximumFetchFailureRate = 0.1;
+};
+}  // namespace RosettaTorch::AlphaZero::Optimizer
+
+#endif  // ROSETTASTONE_TORCH_ALPHA_ZERO_OPTIMIZER_RUN_OPTIONS_HPP
