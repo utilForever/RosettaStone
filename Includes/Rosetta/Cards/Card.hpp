5b04de274c4bb490fabc469c0ae33312edee3524
Chris Ohk <utilForever@gmail.com>
2019-11-24 15:10:14 +0900

    feat(impl-card): Add method 'IsStandardSet()'
    - Finds out if this card is in STANDARD set

--- Includes/Rosetta/Cards/Card.hpp
+++ Includes/Rosetta/Cards/Card.hpp
@@ -87,6 +87,10 @@ class Card
     //! \return The flag that indicates whether it is collectible.
     bool IsCollectible() const;
 
+    //! Finds out if this card is in STANDARD set.
+    //! \return true if this card is in STANDARD set, and false otherwise.
+    bool IsStandardSet() const;
+
     //! Returns the number of cards that can be inserted into the deck.
     //! \return The number of cards that can be inserted into the deck.
     std::size_t GetMaxAllowedInDeck() const;
--- Sources/Rosetta/Cards/Card.cpp
+++ Sources/Rosetta/Cards/Card.cpp
@@ -5,6 +5,7 @@
 // property of any third parties.
 
 #include <Rosetta/Cards/Card.hpp>
+#include <Rosetta/Commons/Constants.hpp>
 #include <Rosetta/Models/Player.hpp>
 #include <Rosetta/Zones/FieldZone.hpp>
 
@@ -176,6 +177,19 @@ bool Card::IsUntouchable() const
 bool Card::IsCollectible() const
 {
     return static_cast<bool>(gameTags.at(GameTag::COLLECTIBLE));
+}
+
+bool Card::IsStandardSet() const
+{
+    for (auto& cardSet : STANDARD_CARD_SETS)
+    {
+        if (GetCardSet() == cardSet)
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 std::size_t Card::GetMaxAllowedInDeck() const
