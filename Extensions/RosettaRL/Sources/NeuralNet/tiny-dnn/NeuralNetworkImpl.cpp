a22071c741c57e65fc3a0c52dc1177690561e0a6
Chris Ohk <utilForever@gmail.com>
2020-01-05 02:10:16 +0900

    feat(rework-rl): Implement method 'CreateWithRandomWeights()'

--- Extensions/RosettaRL/Sources/NeuralNet/tiny-dnn/NeuralNetworkImpl.cpp
+++ Extensions/RosettaRL/Sources/NeuralNet/tiny-dnn/NeuralNetworkImpl.cpp
@@ -26,6 +26,71 @@ namespace RosettaTorch::NeuralNet
 {
 void NeuralNetworkImpl::CreateWithRandomWeights(const std::string& fileName)
 {
+    constexpr static int HERO_IN_DIM = 1;
+    constexpr static int HERO_OUT_DIM = 1;
+
+    constexpr static int MINION_IN_DIM = 7;
+    constexpr static int MINION_OUT_DIM = 3;
+
+    constexpr static int MINION_COUNT = 7;
+
+    constexpr static int STANDALONE_IN_DIM = 17;
+
+    // 2 = my hero, opponent hero
+    tiny_dnn::layers::input heroesIn(tiny_dnn::shape3d(HERO_IN_DIM, 2, 1));
+
+    tiny_dnn::layers::conv heroConv1(HERO_IN_DIM, 2, HERO_IN_DIM, 1, 1,
+                                     HERO_OUT_DIM);
+    // out: 1 * 2 * HERO_OUT_DIM
+    heroesIn << heroConv1;
+
+    auto heroConv1a = tiny_dnn::activation::leaky_relu();
+    // out: 1 * 2 * HERO_OUT_DIM
+    heroConv1 << heroConv1a;
+
+    // MINION_COUNT * 2 = my minions, opponent minions
+    tiny_dnn::layers::input minionsIn(
+        tiny_dnn::shape3d(MINION_IN_DIM, MINION_COUNT * 2, 1));
+
+    tiny_dnn::layers::conv minionConv1(MINION_IN_DIM, MINION_COUNT * 2,
+                                       MINION_IN_DIM, 1, 1, 1 * MINION_OUT_DIM);
+    // out: 1 * (MINION_COUNT * 2) * MINION_OUT_DIM
+    minionsIn << minionConv1;
+
+    auto minionConv1a = tiny_dnn::activation::leaky_relu();
+    // out: 1 * (MINION_COUNT * 2) * MINION_OUT_DIM
+    minionConv1 << minionConv1a;
+
+    tiny_dnn::layers::input standaloneIn(
+        tiny_dnn::shape3d(1, 1, STANDALONE_IN_DIM));
+
+    // Reshape to 1x1
+    auto concat = tiny_dnn::layers::concat(
+        { tiny_dnn::shape3d(1, 1, 2 * HERO_OUT_DIM),
+          tiny_dnn::shape3d(1, 1, MINION_COUNT * 2 * MINION_OUT_DIM),
+          standaloneIn.out_shape()[0] });
+    (heroConv1a, minionConv1a, standaloneIn) << concat;
+
+    auto fc1 = tiny_dnn::fully_connected_layer(
+        2 * HERO_OUT_DIM + 2 * MINION_COUNT * MINION_OUT_DIM +
+            STANDALONE_IN_DIM,
+        10);
+    concat << fc1;
+
+    auto fc1a = tiny_dnn::activation::leaky_relu();
+    auto fc2 = tiny_dnn::fully_connected_layer(10, 1);
+    fc1 << fc1a << fc2;
+
+    auto output = tiny_dnn::activation::tanh(1);
+    fc2 << output;
+
+    tiny_dnn::network<tiny_dnn::graph> net;
+    tiny_dnn::construct_graph(net, { &heroesIn, &minionsIn, &standaloneIn },
+                              { &output });
+
+    net.init_weight();
+
+    net.save(fileName);
 }
 
 void NeuralNetworkImpl::Save(const std::string& fileName) const
