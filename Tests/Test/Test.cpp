ef4420c8d20de9de363fba6f5cd7ec8894d005e4
Chris Ohk <utilForever@gmail.com>
2020-01-11 17:06:33 +0900

    feat(fix-leak): Change test code to analyze memory leak

--- Tests/Test/Test.cpp
+++ Tests/Test/Test.cpp
@@ -6,6 +6,8 @@
 
 #include <Rosetta/Games/Game.hpp>
 
+#include <memory>
+
 #ifdef _DEBUG
 #define new new (_CLIENT_BLOCK, __FILE__, __LINE__)
 #endif
@@ -14,8 +16,9 @@ int _tmain(int argc, _TCHAR* argv[])
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-    RosettaStone::Game game1, game2;
-    game1.RefCopyFrom(game2);
+    std::unique_ptr<RosettaStone::Game> game1, game2;
+
+    game1 = std::move(game2);
 
     return 0;
 }
