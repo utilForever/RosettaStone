bf2d78ba873ff80888975e76317968106cf7e9a3
Doun <jusmint3@gmail.com>
2020-01-21 14:45:59 +0900

    feat(card-impl): implement card 'Lifeweaver' (DAL_355)

--- Tests/UnitTests/CardSets/DalaranCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/DalaranCardsGenTests.cpp
@@ -6,7 +6,54 @@
 
 #include <Utils/CardSetUtils.hpp>
 
-TEST(DalaranCardsGen, Temp)
+#include <Rosetta/Actions/Draw.hpp>
+#include <Rosetta/Cards/Cards.hpp>
+#include <Rosetta/Zones/DeckZone.hpp>
+#include <Rosetta/Zones/FieldZone.hpp>
+#include <Rosetta/Zones/HandZone.hpp>
+
+using namespace RosettaStone;
+using namespace PlayerTasks;
+using namespace SimpleTasks;
+
+// ----------------------------------------- MINION - DRUID
+// [DAL_355] Lifeweaver - COST:3 [ATK:2/HP:5]
+// - Set: Dalaran, Rarity: Rare
+// --------------------------------------------------------
+// Text: Whenever you restore Health,
+//       add a random Druid spell to your hand.
+// --------------------------------------------------------
+TEST(DruidDalaranTest, DAL_355_Lifeweaver)
 {
-    EXPECT_TRUE(true);
+    GameConfig config;
+    config.player1Class = CardClass::DRUID;
+    config.player2Class = CardClass::PRIEST;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_START);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+    curPlayer->GetHero()->SetDamage(10);
+
+	auto& curField = *(curPlayer->GetFieldZone());
+
+    const auto card1 = Generic::DrawCard(
+        curPlayer, Cards::FindCardByName("Lifeweaver"));
+    const auto card2 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Healing Touch"));
+
+	game.Process(curPlayer, PlayCardTask::Minion(card1));
+    game.Process(curPlayer,
+                 PlayCardTask::SpellTarget(card2, curPlayer->GetHero()));
+    EXPECT_EQ(curPlayer->GetHero()->GetHealth(), 28);
+    EXPECT_EQ(curPlayer->GetHandZone()->GetCount(), 4);
 }
