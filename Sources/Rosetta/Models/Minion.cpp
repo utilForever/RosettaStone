639ae468a7318055668c68a65d765bedae1a56e7
Chris Ohk <utilForever@gmail.com>
2020-01-29 22:31:05 +0900

    feat(impl-card): Add code to silence keyword 'Rush' and 'Reborn'

--- Sources/Rosetta/Models/Minion.cpp
+++ Sources/Rosetta/Models/Minion.cpp
@@ -77,6 +77,8 @@ void Minion::Silence()
     SetGameTag(GameTag::CANT_BE_TARGETED_BY_SPELLS, 0);
     SetGameTag(GameTag::IMMUNE, 0);
     SetGameTag(GameTag::CANT_ATTACK, 0);
+    SetGameTag(GameTag::RUSH, 0);
+    SetGameTag(GameTag::REBORN, 0);
 
     const int spellPower = GetGameTag(GameTag::SPELLPOWER);
     if (spellPower > 0)
