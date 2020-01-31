ac20ef5c3d7d92a9562f67c19abe12c2d808261e
Doun <jusmint3@gmail.com>
2020-01-31 14:31:14 +0900

    fix: Correct unit test code of 'Lifeweaver'(DAL_355)

--- Tests/UnitTests/CardSets/DalaranCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/DalaranCardsGenTests.cpp
@@ -43,17 +43,76 @@ TEST(DruidDalaranTest, DAL_355_Lifeweaver)
     opPlayer->SetTotalMana(10);
     opPlayer->SetUsedMana(0);
     curPlayer->GetHero()->SetDamage(10);
+    opPlayer->GetHero()->SetDamage(10);
 
-	auto& curField = *(curPlayer->GetFieldZone());
+    auto& curHand = *(curPlayer->GetHandZone());
 
     const auto card1 = Generic::DrawCard(
         curPlayer, Cards::FindCardByName("Lifeweaver"));
     const auto card2 =
         Generic::DrawCard(curPlayer, Cards::FindCardByName("Healing Touch"));
+    const auto card3 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Voodoo Doctor"));
 
 	game.Process(curPlayer, PlayCardTask::Minion(card1));
     game.Process(curPlayer,
                  PlayCardTask::SpellTarget(card2, curPlayer->GetHero()));
+    game.Process(curPlayer,
+                 PlayCardTask::MinionTarget(card3, opPlayer->GetHero()));
+
+    EXPECT_EQ(curPlayer->GetHero()->GetHealth(), 28);
+    EXPECT_EQ(opPlayer->GetHero()->GetHealth(), 22);
+    EXPECT_EQ(curPlayer->GetHandZone()->GetCount(), 6);
+    EXPECT_EQ(curHand[4]->card->GetCardClass(), CardClass::DRUID);
+    EXPECT_EQ(curHand[5]->card->GetCardClass(), CardClass::DRUID);
+
+	game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_START);
+
+	game.Process(opPlayer, HeroPowerTask(curPlayer->GetHero()));
+
+    EXPECT_EQ(curPlayer->GetHero()->GetHealth(), 30);
+    EXPECT_EQ(curPlayer->GetHandZone()->GetCount(), 6);
+}
+// ------------------------------------------ SPELL - DRUID
+// [DAL_733] Dreamway Guardians - COST:2
+// - Faction: Neutral, Set: Dalaran, Rarity: Rare
+// --------------------------------------------------------
+// Text: Summon two 1/2 Dryads with <b>Lifesteal</b>.
+// --------------------------------------------------------
+// PlayReq:
+// - REQ_MINION_TARGET = 0
+// --------------------------------------------------------
+// RefTag:
+// - LIFESTEAL = 1
+// --------------------------------------------------------
+/*
+TEST(DruidDalaranTest, DAL_733_Dreamway Guardians)
+{
+    GameConfig config;
+    config.player1Class = CardClass::DRUID;
+    config.player2Class = CardClass::PRIEST;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_START);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+    curPlayer->GetHero()->SetDamage(10);
+
+    auto& curField = *(curPlayer->GetFieldZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Dreamway Guardians"));
+
     EXPECT_EQ(curPlayer->GetHero()->GetHealth(), 28);
     EXPECT_EQ(curPlayer->GetHandZone()->GetCount(), 4);
-}
+}*/
