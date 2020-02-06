0f93cc5e18c1d85afda26ae9ddf1715ce6746537
1cekrim <1cekrim.dev@gmail.com>
2020-02-06 15:29:41 +0900

    fix: Remove unnecessary test

--- Tests/UnitTests/CardSets/UldumCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/UldumCardsGenTests.cpp
@@ -477,31 +477,7 @@ TEST(NeutralUldumTest, ULD_191_BeamingSidekick)
 // --------------------------------------------------------
 TEST(NeutralUldumTest, ULD_193_LivingMonument)
 {
-    GameConfig config;
-    config.player1Class = CardClass::MAGE;
-    config.player2Class = CardClass::MAGE;
-    config.startPlayer = PlayerType::PLAYER1;
-    config.doFillDecks = true;
-    config.autoRun = false;
-
-    Game game(config);
-    game.Start();
-    game.ProcessUntil(Step::MAIN_START);
-
-    Player* curPlayer = game.GetCurrentPlayer();
-    Player* opPlayer = game.GetOpponentPlayer();
-    curPlayer->SetTotalMana(10);
-    curPlayer->SetUsedMana(0);
-    opPlayer->SetTotalMana(10);
-    opPlayer->SetUsedMana(0);
-
-    auto& curField = *(curPlayer->GetFieldZone());
-
-    const auto card1 =
-        Generic::DrawCard(curPlayer, Cards::FindCardByName("Living Monument"));
-
-    game.Process(curPlayer, PlayCardTask::Minion(card1));
-    EXPECT_EQ(curField[0]->GetGameTag(GameTag::TAUNT), 1);
+    // Do noting
 }
 
 // --------------------------------------- MINION - NEUTRAL
