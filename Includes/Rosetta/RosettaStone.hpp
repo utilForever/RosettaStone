ec48617ced307d826c2cf14a7dcc5611916f97bd
Chris Ohk <utilForever@gmail.com>
2020-03-03 18:21:55 +0900

    feat(impl-card): Create class 'ActivateCapturedDeathrattleTask'
    - This class represents the task for activating captured card's deathrattle

--- Includes/Rosetta/RosettaStone.hpp
+++ Includes/Rosetta/RosettaStone.hpp
@@ -114,6 +114,7 @@
 #include <Rosetta/Tasks/PlayerTasks/EndTurnTask.hpp>
 #include <Rosetta/Tasks/PlayerTasks/HeroPowerTask.hpp>
 #include <Rosetta/Tasks/PlayerTasks/PlayCardTask.hpp>
+#include <Rosetta/Tasks/SimpleTasks/ActivateCapturedDeathrattleTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/AddAuraEffectTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/AddCardTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/AddEnchantmentTask.hpp>
