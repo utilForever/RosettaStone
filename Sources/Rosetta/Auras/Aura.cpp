78856d9d43efbf3fafd6e86690f71466fa26d34e
Chris Ohk <utilForever@gmail.com>
2020-03-24 23:59:34 +0900

    feat(impl-card): Add code to process 'GameTag::CANT_BE_FROZEN'
    - When you summon this minion, it also unfreezes all friendly minions that were frozen previously

--- Sources/Rosetta/Auras/Aura.cpp
+++ Sources/Rosetta/Auras/Aura.cpp
@@ -443,6 +443,27 @@ void Aura::UpdateInternal()
             {
                 const auto effectPtr = dynamic_cast<Effect*>(effect.get());
                 effectPtr->ApplyTo(m_owner->player->playerAuraEffects);
+
+                // NOTE: Living Dragonbreath (DRG_068)
+                // When you summon this minion, it also unfreezes all friendly
+                // minions that were frozen previously.
+                if (effectPtr->GetGameTag() == GameTag::CANT_BE_FROZEN)
+                {
+                    if (auto hero = m_owner->player->GetHero();
+                        hero->IsFrozen())
+                    {
+                        hero->SetGameTag(GameTag::FROZEN, 0);
+                    }
+
+                    for (auto& minion :
+                         m_owner->player->GetFieldZone()->GetAll())
+                    {
+                        if (minion->IsFrozen())
+                        {
+                            minion->SetGameTag(GameTag::FROZEN, 0);
+                        }
+                    }
+                }
             }
             break;
         }
