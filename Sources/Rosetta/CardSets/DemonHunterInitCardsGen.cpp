0bc433a520c75a5d917b508ff2fe1ee26f5e2435
Chris Ohk <utilForever@gmail.com>
2020-04-01 11:49:45 +0900

    feat(demon): Implement card 'Command the Illidari' (BT_173)

--- Sources/Rosetta/CardSets/DemonHunterInitCardsGen.cpp
+++ Sources/Rosetta/CardSets/DemonHunterInitCardsGen.cpp
@@ -1,15 +1,27 @@
-// This code is based on Sabberstone project.
+﻿// This code is based on Sabberstone project.
 // Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
 // Hearthstone++ is hearthstone simulator using C++ with reinforcement learning.
 // Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
 
 #include <Rosetta/CardSets/DemonHunterInitCardsGen.hpp>
+#include <Rosetta/Tasks/SimpleTasks/SummonTask.hpp>
 
 namespace RosettaStone
 {
 void DemonHunterInitCardsGen::AddDemonHunter(
     std::map<std::string, CardDef>& cards)
 {
+    Power power;
+
+    // ------------------------------------ SPELL - DEMONHUNTER
+    // [BT_173] Command the Illidari - COST:5
+    // - Set: Demon Hunter Initiate, Rarity: Common
+    // --------------------------------------------------------
+    // Text: Summon six 1/1 Illidari with <b>Rush</b>.
+    // --------------------------------------------------------
+    power.ClearData();
+    power.AddPowerTask(std::make_shared<SimpleTasks::SummonTask>("BT_036t", 6));
+    cards.emplace("BT_173", CardDef(power));
 }
 
 void DemonHunterInitCardsGen::AddDemonHunterNonCollect(
--- Tests/UnitTests/CardSets/DemonHunterInitCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/DemonHunterInitCardsGenTests.cpp
@@ -1,12 +1,92 @@
-// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+﻿// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
 
 // We are making my contributions/submissions to this project solely in our
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
 #include <Utils/CardSetUtils.hpp>
+#include <Utils/TestUtils.hpp>
 
-TEST_CASE("[DemonHunterInitCardsGen] - Temp")
+#include <Rosetta/Actions/Draw.hpp>
+#include <Rosetta/Cards/Cards.hpp>
+#include <Rosetta/Zones/FieldZone.hpp>
+
+using namespace RosettaStone;
+using namespace PlayerTasks;
+using namespace SimpleTasks;
+
+// ------------------------------------ SPELL - DEMONHUNTER
+// [BT_173] Command the Illidari - COST:5
+// - Set: Demon Hunter Initiate, Rarity: Common
+// --------------------------------------------------------
+// Text: Summon six 1/1 Illidari with <b>Rush</b>.
+// --------------------------------------------------------
+TEST_CASE("[Demon Hunter : Spell] - BT_173 : Command the Illidari")
 {
-    CHECK(true);
+    GameConfig config;
+    config.player1Class = CardClass::WARLOCK;
+    config.player2Class = CardClass::DEMONHUNTER;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curField = *(curPlayer->GetFieldZone());
+    auto& opField = *(opPlayer->GetFieldZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Malygos"));
+    const auto card2 = Generic::DrawCard(
+        opPlayer, Cards::FindCardByName("Command the Illidari"));
+
+    game.Process(curPlayer, PlayCardTask::Minion(card1));
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(opPlayer, PlayCardTask::Spell(card2));
+    CHECK_EQ(opField.GetCount(), 6);
+    CHECK_EQ(opField[0]->card->name, "Illidari Initiate");
+    CHECK_EQ(opField[0]->GetAttack(), 1);
+    CHECK_EQ(opField[0]->GetHealth(), 1);
+    CHECK_EQ(opField[0]->IsRush(), true);
+    CHECK_EQ(opField[1]->card->name, "Illidari Initiate");
+    CHECK_EQ(opField[1]->GetAttack(), 1);
+    CHECK_EQ(opField[1]->GetHealth(), 1);
+    CHECK_EQ(opField[1]->IsRush(), true);
+    CHECK_EQ(opField[2]->card->name, "Illidari Initiate");
+    CHECK_EQ(opField[2]->GetAttack(), 1);
+    CHECK_EQ(opField[2]->GetHealth(), 1);
+    CHECK_EQ(opField[2]->IsRush(), true);
+    CHECK_EQ(opField[3]->card->name, "Illidari Initiate");
+    CHECK_EQ(opField[3]->GetAttack(), 1);
+    CHECK_EQ(opField[3]->GetHealth(), 1);
+    CHECK_EQ(opField[3]->IsRush(), true);
+    CHECK_EQ(opField[4]->card->name, "Illidari Initiate");
+    CHECK_EQ(opField[4]->GetAttack(), 1);
+    CHECK_EQ(opField[4]->GetHealth(), 1);
+    CHECK_EQ(opField[4]->IsRush(), true);
+    CHECK_EQ(opField[5]->card->name, "Illidari Initiate");
+    CHECK_EQ(opField[5]->GetAttack(), 1);
+    CHECK_EQ(opField[5]->GetHealth(), 1);
+    CHECK_EQ(opField[5]->IsRush(), true);
+
+    game.Process(opPlayer, AttackTask(opField[0], card1));
+    game.Process(opPlayer, AttackTask(opField[0], card1));
+    game.Process(opPlayer, AttackTask(opField[0], card1));
+    game.Process(opPlayer, AttackTask(opField[0], card1));
+    game.Process(opPlayer, AttackTask(opField[0], card1));
+    game.Process(opPlayer, AttackTask(opField[0], card1));
+    CHECK_EQ(curField[0]->GetHealth(), 6);
+    CHECK_EQ(opField.GetCount(), 0);
 }
