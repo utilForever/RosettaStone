75b8386c4dd850dd63d728ed1ba5f65b228dcf94
Chris Ohk <utilForever@gmail.com>
2020-04-01 12:54:52 +0900

    feat(demon): Implement card 'Twin Slice' (BT_175)

--- Sources/Rosetta/CardSets/DemonHunterInitCardsGen.cpp
+++ Sources/Rosetta/CardSets/DemonHunterInitCardsGen.cpp
@@ -4,7 +4,13 @@
 // Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
 
 #include <Rosetta/CardSets/DemonHunterInitCardsGen.hpp>
+#include <Rosetta/Enchants/Enchants.hpp>
+#include <Rosetta/Tasks/SimpleTasks/AddCardTask.hpp>
+#include <Rosetta/Tasks/SimpleTasks/AddEnchantmentTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/SummonTask.hpp>
+
+using namespace RosettaStone;
+using namespace SimpleTasks;
 
 namespace RosettaStone
 {
@@ -20,13 +26,52 @@ void DemonHunterInitCardsGen::AddDemonHunter(
     // Text: Summon six 1/1 Illidari with <b>Rush</b>.
     // --------------------------------------------------------
     power.ClearData();
-    power.AddPowerTask(std::make_shared<SimpleTasks::SummonTask>("BT_036t", 6));
+    power.AddPowerTask(std::make_shared<SummonTask>("BT_036t", 6));
     cards.emplace("BT_173", CardDef(power));
+
+    // ------------------------------------ SPELL - DEMONHUNTER
+    // [BT_175] Twin Slice - COST:0
+    // - Set: Demon Hunter Initiate, Rarity: Common
+    // --------------------------------------------------------
+    // Text: Give your hero +1 Attack this turn.
+    //       Add 'Second Slice' to your hand.
+    // --------------------------------------------------------
+    power.ClearData();
+    power.AddPowerTask(
+        std::make_shared<AddEnchantmentTask>("BT_175e", EntityType::HERO));
+    power.AddPowerTask(
+        std::make_shared<AddCardTask>(EntityType::HAND, "BT_175t", 1));
+    cards.emplace("BT_175", CardDef(power));
 }
 
 void DemonHunterInitCardsGen::AddDemonHunterNonCollect(
     std::map<std::string, CardDef>& cards)
 {
+    Power power;
+
+    // ------------------------------ ENCHANTMENT - DEMONHUNTER
+    // [BT_175e] Twin Slice (*) - COST:0
+    // - Set: Demon Hunter Initiate
+    // --------------------------------------------------------
+    // Text: Your hero has +1 Attack this turn.
+    // --------------------------------------------------------
+    // GameTag:
+    // - TAG_ONE_TURN_EFFECT = 1
+    // --------------------------------------------------------
+    power.ClearData();
+    power.AddEnchant(Enchants::GetEnchantFromText("BT_175e"));
+    cards.emplace("BT_175e", CardDef(power));
+
+    // ------------------------------------ SPELL - DEMONHUNTER
+    // [BT_175t] Second Slice - COST:0
+    // - Set: Demon Hunter Initiate, Rarity: Common
+    // --------------------------------------------------------
+    // Text: Give your hero +1 Attack this turn.
+    // --------------------------------------------------------
+    power.ClearData();
+    power.AddPowerTask(
+        std::make_shared<AddEnchantmentTask>("BT_175e", EntityType::HERO));
+    cards.emplace("BT_175t", CardDef(power));
 }
 
 void DemonHunterInitCardsGen::AddAll(std::map<std::string, CardDef>& cards)
--- Tests/UnitTests/CardSets/DemonHunterInitCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/DemonHunterInitCardsGenTests.cpp
@@ -10,6 +10,7 @@
 #include <Rosetta/Actions/Draw.hpp>
 #include <Rosetta/Cards/Cards.hpp>
 #include <Rosetta/Zones/FieldZone.hpp>
+#include <Rosetta/Zones/HandZone.hpp>
 
 using namespace RosettaStone;
 using namespace PlayerTasks;
@@ -89,4 +90,53 @@ TEST_CASE("[Demon Hunter : Spell] - BT_173 : Command the Illidari")
     game.Process(opPlayer, AttackTask(opField[0], card1));
     CHECK_EQ(curField[0]->GetHealth(), 6);
     CHECK_EQ(opField.GetCount(), 0);
+}
+
+// ------------------------------------ SPELL - DEMONHUNTER
+// [BT_175] Twin Slice - COST:0
+// - Set: Demon Hunter Initiate, Rarity: Common
+// --------------------------------------------------------
+// Text: Give your hero +1 Attack this turn.
+//       Add 'Second Slice' to your hand.
+// --------------------------------------------------------
+TEST_CASE("[Demon Hunter : Spell] - BT_175 : Twin Slice")
+{
+    GameConfig config;
+    config.player1Class = CardClass::DEMONHUNTER;
+    config.player2Class = CardClass::WARLOCK;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curHand = *(curPlayer->GetHandZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Twin Slice"));
+
+    game.Process(curPlayer, HeroPowerTask());
+    CHECK_EQ(curPlayer->GetHero()->GetAttack(), 1);
+
+    game.Process(curPlayer, PlayCardTask::Spell(card1));
+    CHECK_EQ(curPlayer->GetHero()->GetAttack(), 2);
+    CHECK_EQ(curHand.GetCount(), 5);
+    CHECK_EQ(curHand[4]->card->name, "Second Slice");
+
+    game.Process(curPlayer, PlayCardTask::Spell(curHand[4]));
+    CHECK_EQ(curPlayer->GetHero()->GetAttack(), 3);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    CHECK_EQ(curPlayer->GetHero()->GetAttack(), 0);
 }
