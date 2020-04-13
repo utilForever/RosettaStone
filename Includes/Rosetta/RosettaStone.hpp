fc65440d2ee48ba1a8b91370e9f0175c00731854
Chris Ohk <utilForever@gmail.com>
2020-04-13 16:28:02 +0900

    feat(impl-card): Create class 'GetPlayerManaTask'
    - This class represents the task for getting player's remaining mana

--- Includes/Rosetta/RosettaStone.hpp
+++ Includes/Rosetta/RosettaStone.hpp
@@ -154,6 +154,7 @@
 #include <Rosetta/Tasks/SimpleTasks/GetEventNumberTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/GetGameTagTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/GetPlayerGameTagTask.hpp>
+#include <Rosetta/Tasks/SimpleTasks/GetPlayerManaTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/HealFullTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/HealTask.hpp>
 #include <Rosetta/Tasks/SimpleTasks/IncludeAdjacentTask.hpp>
