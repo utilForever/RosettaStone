a5614406013fffea382ba7403efb5e78a605df42
Chris Ohk <utilForever@gmail.com>
2020-05-04 18:18:30 +0900

    feat(impl-card): Implement card 'Untapped Potential' (ULD_131)

--- Tests/UnitTests/CardSets/UldumCardsGenTests.cpp
+++ Tests/UnitTests/CardSets/UldumCardsGenTests.cpp
@@ -32,100 +32,103 @@ using namespace SimpleTasks;
 // - 839 = 1
 // - QUEST_REWARD_DATABASE_ID = 53499
 // --------------------------------------------------------
-//TEST_CASE("[Druid : Spell] - ULD_131 : Untapped Potential")
-//{
-//    GameConfig config;
-//    config.player1Class = CardClass::DRUID;
-//    config.player2Class = CardClass::WARRIOR;
-//    config.startPlayer = PlayerType::PLAYER1;
-//    config.doFillDecks = true;
-//    config.autoRun = false;
-//
-//    Game game(config);
-//    game.Start();
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    Player* curPlayer = game.GetCurrentPlayer();
-//    Player* opPlayer = game.GetOpponentPlayer();
-//    curPlayer->SetTotalMana(1);
-//    curPlayer->SetUsedMana(0);
-//    opPlayer->SetTotalMana(10);
-//    opPlayer->SetUsedMana(0);
-//
-//    auto& curField = *(curPlayer->GetFieldZone());
-//    auto& opField = *(opPlayer->GetFieldZone());
-//    const auto curSecret = curPlayer->GetSecretZone();
-//
-//    const auto card1 = Generic::DrawCard(
-//        curPlayer, Cards::FindCardByName("Untapped Potential"));
-//    const auto card2 = Generic::DrawCard(
-//        curPlayer, Cards::FindCardByName("Druid of the Claw"));
-//    const auto card3 =
-//        Generic::DrawCard(curPlayer, Cards::FindCardByName("Starfall"));
-//    const auto card4 =
-//        Generic::DrawCard(opPlayer, Cards::FindCardByName("Malygos"));
-//
-//    auto quest = dynamic_cast<Spell*>(card1);
-//
-//    game.Process(curPlayer, PlayCardTask::Spell(card1));
-//    CHECK(curSecret->quest != nullptr);
-//    CHECK_EQ(quest->GetQuestProgress(), 0);
-//    CHECK_EQ(quest->GetQuestProgressTotal(), 4);
-//
-//    game.Process(curPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    CHECK_EQ(quest->GetQuestProgress(), 0);
-//
-//    game.Process(opPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    game.Process(curPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    CHECK_EQ(quest->GetQuestProgress(), 1);
-//
-//    game.Process(opPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    game.Process(curPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    CHECK_EQ(quest->GetQuestProgress(), 2);
-//
-//    game.Process(opPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    game.Process(curPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    CHECK_EQ(quest->GetQuestProgress(), 3);
-//
-//    game.Process(opPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    game.Process(curPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    CHECK(curSecret->quest == nullptr);
-//    CHECK_EQ(quest->GetQuestProgress(), 4);
-//    CHECK_EQ(curPlayer->GetHero()->heroPower->card->id, "ULD_131p");
-//
-//    game.Process(opPlayer, PlayCardTask::Minion(card4));
-//
-//    game.Process(opPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    game.Process(curPlayer, PlayCardTask::Minion(card2));
-//    CHECK_EQ(curField.GetCount(), 1);
-//    CHECK_EQ(curField[0]->GetAttack(), 4);
-//    CHECK_EQ(curField[0]->GetHealth(), 6);
-//    CHECK_EQ(curField[0]->HasCharge(), true);
-//    CHECK_EQ(curField[0]->HasTaunt(), true);
-//
-//    game.Process(curPlayer, PlayCardTask::SpellTarget(card3, card4));
-//    CHECK_EQ(opField[0]->GetHealth(), 5);
-//}
+TEST_CASE("[Druid : Spell] - ULD_131 : Untapped Potential")
+{
+    GameConfig config;
+    config.player1Class = CardClass::DRUID;
+    config.player2Class = CardClass::WARRIOR;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(1);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curField = *(curPlayer->GetFieldZone());
+    auto& opField = *(opPlayer->GetFieldZone());
+    const auto curSecret = curPlayer->GetSecretZone();
+
+    const auto card1 = Generic::DrawCard(
+        curPlayer, Cards::FindCardByName("Untapped Potential"));
+    const auto card2 = Generic::DrawCard(
+        curPlayer, Cards::FindCardByName("Druid of the Claw"));
+    const auto card3 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Starfall"));
+    const auto card4 =
+        Generic::DrawCard(opPlayer, Cards::FindCardByName("Malygos"));
+
+    auto quest = dynamic_cast<Spell*>(card1);
+
+    game.Process(curPlayer, PlayCardTask::Spell(card1));
+    CHECK(curSecret->quest != nullptr);
+    CHECK_EQ(quest->GetQuestProgress(), 0);
+    CHECK_EQ(quest->GetQuestProgressTotal(), 4);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    CHECK_EQ(quest->GetQuestProgress(), 0);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    CHECK_EQ(quest->GetQuestProgress(), 1);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    CHECK_EQ(quest->GetQuestProgress(), 2);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    CHECK_EQ(quest->GetQuestProgress(), 3);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    CHECK(curSecret->quest == nullptr);
+    CHECK_EQ(quest->GetQuestProgress(), 4);
+    CHECK_EQ(curPlayer->GetHero()->heroPower->card->id, "ULD_131p");
+
+    game.Process(opPlayer, PlayCardTask::Minion(card4));
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, PlayCardTask::Minion(card2));
+    CHECK_EQ(curField.GetCount(), 1);
+    CHECK_EQ(curField[0]->GetAttack(), 4);
+    CHECK_EQ(curField[0]->GetHealth(), 6);
+    CHECK_EQ(curField[0]->HasCharge(), true);
+    CHECK_EQ(curField[0]->HasTaunt(), true);
+
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+
+    game.Process(curPlayer, PlayCardTask::SpellTarget(card3, card4));
+    CHECK_EQ(opField[0]->GetHealth(), 5);
+}
 
 // ----------------------------------------- MINION - DRUID
 // [ULD_133] Crystal Merchant - COST:2 [ATK:1/HP:4]
