2379d0200701467a1bec9fb1532dddcf7cd82121
Chris Ohk <utilforever@gmail.com>
2020-05-22 23:56:02 +0900

    feat(battlegrounds): Add constants for the number of tier minions
    - NUM_TIER1_MINIONS_IN_BATTLEGROUNDS
    - NUM_TIER2_MINIONS_IN_BATTLEGROUNDS
    - NUM_TIER3_MINIONS_IN_BATTLEGROUNDS
    - NUM_TIER4_MINIONS_IN_BATTLEGROUNDS
    - NUM_TIER5_MINIONS_IN_BATTLEGROUNDS
    - NUM_TIER6_MINIONS_IN_BATTLEGROUNDS

--- Includes/Rosetta/Common/Constants.hpp
+++ Includes/Rosetta/Common/Constants.hpp
@@ -77,6 +77,24 @@ constexpr int NUM_PLAYER_CLASS = 10;
 //! The number of heroes in Battlegrounds.
 constexpr int NUM_HEROES_IN_BATTLEGROUNDS = 34;
 
+//! The number of tier 1 minions in Battlegrounds.
+constexpr int NUM_TIER1_MINIONS_IN_BATTLEGROUNDS = 14;
+
+//! The number of tier 2 minions in Battlegrounds.
+constexpr int NUM_TIER2_MINIONS_IN_BATTLEGROUNDS = 17;
+
+//! The number of tier 3 minions in Battlegrounds.
+constexpr int NUM_TIER3_MINIONS_IN_BATTLEGROUNDS = 19;
+
+//! The number of tier 4 minions in Battlegrounds.
+constexpr int NUM_TIER4_MINIONS_IN_BATTLEGROUNDS = 16;
+
+//! The number of tier 5 minions in Battlegrounds.
+constexpr int NUM_TIER5_MINIONS_IN_BATTLEGROUNDS = 15;
+
+//! The number of tier 6 minions in Battlegrounds.
+constexpr int NUM_TIER6_MINIONS_IN_BATTLEGROUNDS = 11;
+
 //! The number of cards that can be stored in the deck at game start.
 constexpr int START_DECK_SIZE = 30;
 
