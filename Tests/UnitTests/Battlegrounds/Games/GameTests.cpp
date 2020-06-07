e5d7baeb660a47866f24675971c56e0481aff60d
Chris Ohk <utilforever@gmail.com>
2020-06-07 22:15:53 +0900

    test: Add unit test code to check method 'RearrangeMinion()'

--- Tests/UnitTests/Battlegrounds/Games/GameTests.cpp
+++ Tests/UnitTests/Battlegrounds/Games/GameTests.cpp
@@ -84,4 +84,26 @@ TEST_CASE("[Game] - Basic")
     minions = game.GetGameState().minionPool.GetMinions(1, 6, true);
     CHECK_EQ(static_cast<int>(minions.size()),
              NUM_TOTAL_TAVERN_MINIONS - 3 * 8);
+
+    player.remainCoin = 10;
+
+    player.PurchaseMinion(0);
+    CHECK_EQ(player.handZone.GetCount(), 1);
+    CHECK_EQ(player.tavernMinions.GetCount(), 2);
+    CHECK_EQ(player.remainCoin, 7);
+
+    player.PurchaseMinion(0);
+    CHECK_EQ(player.handZone.GetCount(), 2);
+    CHECK_EQ(player.tavernMinions.GetCount(), 1);
+    CHECK_EQ(player.remainCoin, 4);
+
+    player.PlayCard(0, 0);
+    player.PlayCard(0, 1);
+
+    const int poolIdx1 = player.minions[0].GetPoolIndex();
+    const int poolIdx2 = player.minions[1].GetPoolIndex();
+
+    player.RearrangeMinion(0, 1);
+    CHECK_EQ(poolIdx1, player.minions[1].GetPoolIndex());
+    CHECK_EQ(poolIdx2, player.minions[0].GetPoolIndex());
 }
