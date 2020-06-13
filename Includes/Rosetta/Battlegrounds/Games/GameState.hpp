7ba6483d1c30bed60489eddeaf08bd57ad67feea
Chris Ohk <utilForever@gmail.com>
2020-06-13 22:00:16 +0900

    feat(battlegrounds): Add variable 'ghostPlayerIdx'

--- Includes/Rosetta/Battlegrounds/Games/GameState.hpp
+++ Includes/Rosetta/Battlegrounds/Games/GameState.hpp
@@ -12,6 +12,7 @@
 #include <Rosetta/Common/Enums/GameEnums.hpp>
 
 #include <array>
+#include <limits>
 
 namespace RosettaStone::Battlegrounds
 {
@@ -28,6 +29,7 @@ struct GameState
     MinionPool minionPool;
 
     std::array<Player, NUM_BATTLEGROUNDS_PLAYERS> players;
+    std::size_t ghostPlayerIdx = std::numeric_limits<std::size_t>::max();
 };
 }  // namespace RosettaStone::Battlegrounds
 
