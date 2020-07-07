65303852ae060e11af7e206b68fb70b6d416bc0a
Chris Ohk <utilforever@gmail.com>
2020-07-07 20:55:10 +0900

    feat(battlegrounds): Create class 'Enchant'
    - This class stores an enchantment, also known as a buff or debuff. It is a special effect gained by a minion, or in rarer occasions by a weapon

---
+++ Includes/Rosetta/Battlegrounds/Enchants/Enchant.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef ROSETTASTONE_BATTLEGROUNDS_ENCHANT_HPP
+#define ROSETTASTONE_BATTLEGROUNDS_ENCHANT_HPP
+
+namespace RosettaStone::Battlegrounds
+{
+//!
+//! \brief Enchant class.
+//!
+//! This class stores an enchantment, also known as a buff or debuff. It is a
+//! special effect gained by a minion, or in rarer occasions by a weapon.
+//!
+class Enchant
+{
+};
+}  // namespace RosettaStone::Battlegrounds
+
+#endif  // ROSETTASTONE_BATTLEGROUNDS_ENCHANT_HPP
