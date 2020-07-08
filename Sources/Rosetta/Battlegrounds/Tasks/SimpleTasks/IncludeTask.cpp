9c96442f6dd7fb150779b926d16ac0d0ec3502cf
Chris Ohk <utilforever@gmail.com>
2020-07-08 22:10:55 +0900

    feat(battlegrounds): Create class 'IncludeTask'
    - This class represents the task for including minions to be applied by another task

---
+++ Sources/Rosetta/Battlegrounds/Tasks/SimpleTasks/IncludeTask.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Rosetta/Battlegrounds/Models/Player.hpp>
+#include <Rosetta/Battlegrounds/Tasks/SimpleTasks/IncludeTask.hpp>
+
+#include <stdexcept>
+
+namespace RosettaStone::Battlegrounds::SimpleTasks
+{
+std::vector<std::reference_wrapper<Minion>> IncludeTask::GetMinions(
+    EntityType entityType, [[maybe_unused]] Player& player, Minion& source)
+{
+    std::vector<std::reference_wrapper<Minion>> minions;
+
+    switch (entityType)
+    {
+        case EntityType::SOURCE:
+            minions.emplace_back(source);
+            break;
+        default:
+            throw std::invalid_argument(
+                "IncludeTask::GetEntities() - Invalid entity type");
+    }
+
+    return minions;
+}
+}  // namespace RosettaStone::Battlegrounds::SimpleTasks
