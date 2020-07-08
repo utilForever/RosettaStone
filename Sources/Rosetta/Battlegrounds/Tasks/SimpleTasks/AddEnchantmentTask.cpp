93c592fdb586b27d0fd586a7b1b7dfccc7fb929b
Chris Ohk <utilforever@gmail.com>
2020-07-08 23:10:23 +0900

    feat(battlegrounds): Implement code of class 'AddEnhantmentTask'

--- Sources/Rosetta/Battlegrounds/Tasks/SimpleTasks/AddEnchantmentTask.cpp
+++ Sources/Rosetta/Battlegrounds/Tasks/SimpleTasks/AddEnchantmentTask.cpp
@@ -4,8 +4,11 @@
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
+#include <Rosetta/Battlegrounds/Actions/Generic.hpp>
+#include <Rosetta/Battlegrounds/Cards/Cards.hpp>
 #include <Rosetta/Battlegrounds/Models/Player.hpp>
 #include <Rosetta/Battlegrounds/Tasks/SimpleTasks/AddEnchantmentTask.hpp>
+#include <Rosetta/Battlegrounds/Tasks/SimpleTasks/IncludeTask.hpp>
 
 namespace RosettaStone::Battlegrounds::SimpleTasks
 {
@@ -18,12 +21,28 @@ AddEnchantmentTask::AddEnchantmentTask(const std::string_view& cardID,
 
 TaskStatus AddEnchantmentTask::Run(Player& player, Minion& source)
 {
+    auto minions = IncludeTask::GetMinions(m_entityType, player, source);
+    Card enchantmentCard = Cards::FindCardByID(m_cardID);
+
+    for (auto& minion : minions)
+    {
+        Generic::AddEnchantment(enchantmentCard, minion);
+    }
+
     return TaskStatus::COMPLETE;
 }
 
 TaskStatus AddEnchantmentTask::Run(Player& player, Minion& source,
                                    [[maybe_unused]] Minion& target)
 {
+    auto minions = IncludeTask::GetMinions(m_entityType, player, source);
+    Card enchantmentCard = Cards::FindCardByID(m_cardID);
+
+    for (auto& minion : minions)
+    {
+        Generic::AddEnchantment(enchantmentCard, minion);
+    }
+
     return TaskStatus::COMPLETE;
 }
 }  // namespace RosettaStone::Battlegrounds::SimpleTasks
