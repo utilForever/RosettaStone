3db836a96058c6dfd7d6e2dd0d6f549251fed6ea
Chris Ohk <utilForever@gmail.com>
2020-07-17 16:03:46 +0900

    feat(battlegrounds): Create class 'DamageHeroTask'
    - This class represents the task for taking damage to the hero

--- Includes/Rosetta/Battlegrounds/Tasks/TaskType.hpp
+++ Includes/Rosetta/Battlegrounds/Tasks/TaskType.hpp
@@ -8,6 +8,7 @@
 #define ROSETTASTONE_BATTLEGROUNDS_TASK_TYPE_HPP
 
 #include <Rosetta/Battlegrounds/Tasks/SimpleTasks/AddEnchantmentTask.hpp>
+#include <Rosetta/Battlegrounds/Tasks/SimpleTasks/DamageHeroTask.hpp>
 #include <Rosetta/Battlegrounds/Tasks/SimpleTasks/GetGameTagTask.hpp>
 #include <Rosetta/Battlegrounds/Tasks/SimpleTasks/RandomTask.hpp>
 #include <Rosetta/Battlegrounds/Tasks/SimpleTasks/SummonTask.hpp>
@@ -17,8 +18,8 @@
 namespace RosettaStone::Battlegrounds
 {
 using namespace SimpleTasks;
-using TaskType =
-    std::variant<AddEnchantmentTask, GetGameTagTask, RandomTask, SummonTask>;
+using TaskType = std::variant<AddEnchantmentTask, DamageHeroTask,
+                              GetGameTagTask, RandomTask, SummonTask>;
 }  // namespace RosettaStone::Battlegrounds
 
 #endif  // ROSETTASTONE_BATTLEGROUNDS_TASK_TYPE_HPP
