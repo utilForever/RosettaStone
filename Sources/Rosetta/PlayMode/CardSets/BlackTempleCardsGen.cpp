24b1397d5ae75c700c8f9f5890e19656a17e2d07
Chris Ohk <utilforever@gmail.com>
2020-08-10 01:03:18 +0900

    feat(impl-card): Implement card 'Imprisoned Homunculus' (BT_258)

--- Sources/Rosetta/PlayMode/CardSets/BlackTempleCardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/BlackTempleCardsGen.cpp
@@ -4,6 +4,10 @@
 // Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
 
 #include <Rosetta/PlayMode/CardSets/BlackTempleCardsGen.hpp>
+#include <Rosetta/PlayMode/Tasks/SimpleTasks/CustomTask.hpp>
+#include <Rosetta/PlayMode/Tasks/SimpleTasks/SetGameTagTask.hpp>
+
+using namespace RosettaStone::PlayMode::SimpleTasks;
 
 namespace RosettaStone::PlayMode
 {
@@ -53,6 +57,41 @@ void BlackTempleCardsGen::AddPaladinNonCollect(
 
 void BlackTempleCardsGen::AddPriest(std::map<std::string, CardDef>& cards)
 {
+    Power power;
+
+    // ---------------------------------------- MINION - PRIEST
+    // [BT_258] Imprisoned Homunculus - COST:1 [ATK:2/HP:5]
+    // - Race: Demon, Set: Black Temple, Rarity: Common
+    // --------------------------------------------------------
+    // Text: <b>Dormant</b> for 2 turns.
+    //       <b>Taunt</b>
+    // --------------------------------------------------------
+    // GameTag:
+    // - TAUNT = 1
+    // --------------------------------------------------------
+    power.ClearData();
+    power.AddPowerTask(std::make_shared<SetGameTagTask>(
+        EntityType::SOURCE, GameTag::UNTOUCHABLE, 1));
+    power.AddPowerTask(std::make_shared<SetGameTagTask>(
+        EntityType::SOURCE, GameTag::TAG_SCRIPT_DATA_NUM_1, 2));
+    power.AddTrigger(std::make_shared<Trigger>(TriggerType::TURN_START));
+    power.GetTrigger()->tasks = { std::make_shared<CustomTask>(
+        []([[maybe_unused]] Player* player, Entity* source,
+           [[maybe_unused]] Playable* target) {
+            const int value =
+                source->GetGameTag(GameTag::TAG_SCRIPT_DATA_NUM_2);
+            if (value <= 2)
+            {
+                source->SetGameTag(GameTag::TAG_SCRIPT_DATA_NUM_2, value + 1);
+            }
+
+            if (value + 1 == source->GetGameTag(GameTag::TAG_SCRIPT_DATA_NUM_1))
+            {
+                source->SetGameTag(GameTag::UNTOUCHABLE, 0);
+                source->SetGameTag(GameTag::EXHAUSTED, 1);
+            }
+        }) };
+    cards.emplace("BT_258", CardDef(power));
 }
 
 void BlackTempleCardsGen::AddPriestNonCollect(
