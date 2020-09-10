f172609c4fc411ee69513481e87b07f8e79c0529
Chris Ohk <utilforever@gmail.com>
2020-09-10 12:12:37 +0900

    refactor: Replace the type 'int' with 'std::size_t'

--- Sources/Rosetta/PlayMode/CardSets/CoreCardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/CoreCardsGen.cpp
@@ -225,7 +225,7 @@ void CoreCardsGen::AddHeroPowers(std::map<std::string, CardDef>& cards)
             return 0;
         }
 
-        const auto idx = Random::get<int>(0, totemCards.size() - 1);
+        const auto idx = Random::get<std::size_t>(0, totemCards.size() - 1);
         Playable* totem =
             Entity::GetFromCard(playable->player, totemCards[idx]);
         playable->player->GetFieldZone()->Add(dynamic_cast<Minion*>(totem));
