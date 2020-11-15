4fe773cb033a0d6cbb79718c5cdc7fd71511c1ef
Chris Ohk <utilforever@gmail.com>
2020-11-15 23:47:07 +0900

    feat(darkmoon): Apply card update for 'Madness at the Darkmoon Faire'
    - Add new expansion to card set
    - NUM_ALL_CARDS: 10090 -> 10528

--- Includes/Rosetta/Common/Constants.hpp
+++ Includes/Rosetta/Common/Constants.hpp
@@ -24,7 +24,7 @@ constexpr int DECK_CODE_VERSION = 1;
 const std::string INVALID_CARD_ID = "INVALID";
 
 //! Specifies which card sets combine into the STANDARD set.
-constexpr std::array<CardSet, 9> STANDARD_CARD_SETS = {
+constexpr std::array<CardSet, 10> STANDARD_CARD_SETS = {
     CardSet::CORE,                   // Basic, 2014
     CardSet::EXPERT1,                // Classic, 2014
     CardSet::DEMON_HUNTER_INITIATE,  // Ashes of Outland, 2020
@@ -33,11 +33,12 @@ constexpr std::array<CardSet, 9> STANDARD_CARD_SETS = {
     CardSet::DRAGONS,                // Descent of Dragons, 2019
     CardSet::YEAR_OF_THE_DRAGON,     // Galakrond's Awakening, 2020
     CardSet::BLACK_TEMPLE,           // Ashes of Outland, 2020
-    CardSet::SCHOLOMANCE             // Scholomance Academy, 2020
+    CardSet::SCHOLOMANCE,            // Scholomance Academy, 2020
+    CardSet::DARKMOON_FAIRE          // Madness at the Darkmoon Faire, 2020
 };
 
 //! Specifies which card sets combine into the WILD set.
-constexpr std::array<CardSet, 24> WILD_CARD_SETS = {
+constexpr std::array<CardSet, 25> WILD_CARD_SETS = {
     // Standard
     CardSet::CORE,                   // Basic, 2014
     CardSet::EXPERT1,                // Classic, 2014
@@ -48,6 +49,7 @@ constexpr std::array<CardSet, 24> WILD_CARD_SETS = {
     CardSet::YEAR_OF_THE_DRAGON,     // Galakrond's Awakening, 2020
     CardSet::BLACK_TEMPLE,           // Ashes of Outland, 2020
     CardSet::SCHOLOMANCE,            // Scholomance Academy, 2020
+    CardSet::DARKMOON_FAIRE,         // Madness at the Darkmoon Faire, 2020
 
     // Wild
     CardSet::NAXX,          // Curse of Naxxramas, 2014
@@ -68,7 +70,7 @@ constexpr std::array<CardSet, 24> WILD_CARD_SETS = {
 };
 
 //! The number of all cards.
-constexpr int NUM_ALL_CARDS = 10090;
+constexpr int NUM_ALL_CARDS = 10528;
 
 //! The number of player class.
 //! \note Druid, Hunter, Mage, Paladin, Priest, Rogue, Shaman, Warlock, Warrior,
