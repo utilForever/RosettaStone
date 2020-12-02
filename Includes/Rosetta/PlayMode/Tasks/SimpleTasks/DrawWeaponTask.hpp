e77de6898be535d243fe110327d8f0978e327804
Chris Ohk <utilforever@gmail.com>
2020-12-03 00:59:27 +0900

    refactor: Create class 'DrawWeaponTask'
    - This class represents the task for drawing weapon card(s) from the deck

---
+++ Includes/Rosetta/PlayMode/Tasks/SimpleTasks/DrawWeaponTask.hpp
@@ -0,0 +1,41 @@
+// This code is based on Sabberstone project.
+// Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+// RosettaStone is hearthstone simulator using C++ with reinforcement learning.
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+#ifndef ROSETTASTONE_PLAYMODE_DRAW_WEAPON_TASK_HPP
+#define ROSETTASTONE_PLAYMODE_DRAW_WEAPON_TASK_HPP
+
+#include <Rosetta/PlayMode/Tasks/ITask.hpp>
+
+namespace RosettaStone::PlayMode::SimpleTasks
+{
+//!
+//! \brief DrawWeaponTask class.
+//!
+//! This class represents the task for drawing weapon card(s) from the deck.
+//!
+class DrawWeaponTask : public ITask
+{
+ public:
+    //! Constructs task with given \p amount and \p addToStack.
+    //! \param amount The amount to draw minion card(s).
+    //! \param addToStack A flag to store card to stack.
+    explicit DrawWeaponTask(int amount, bool addToStack = false);
+
+ private:
+    //! Processes task logic internally and returns meta data.
+    //! \param player The player to run task.
+    //! \return The result of task processing.
+    TaskStatus Impl(Player* player) override;
+
+    //! Internal method of Clone().
+    //! \return The cloned task.
+    std::unique_ptr<ITask> CloneImpl() override;
+
+    int m_amount = 0;
+    bool m_addToStack = false;
+};
+}  // namespace RosettaStone::PlayMode::SimpleTasks
+
+#endif  // ROSETTASTONE_PLAYMODE_DRAW_WEAPON_TASK_HPP
---
+++ Sources/Rosetta/PlayMode/Tasks/SimpleTasks/DrawWeaponTask.cpp
@@ -0,0 +1,87 @@
+// This code is based on Sabberstone project.
+// Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+// Hearthstone++ is hearthstone simulator using C++ with reinforcement learning.
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+#include <Rosetta/PlayMode/Actions/Draw.hpp>
+#include <Rosetta/PlayMode/Games/Game.hpp>
+#include <Rosetta/PlayMode/Tasks/SimpleTasks/DrawWeaponTask.hpp>
+#include <Rosetta/PlayMode/Zones/DeckZone.hpp>
+
+#include <effolkronium/random.hpp>
+
+using Random = effolkronium::random_static;
+
+namespace RosettaStone::PlayMode::SimpleTasks
+{
+DrawWeaponTask::DrawWeaponTask(int amount, bool addToStack)
+    : m_amount(amount), m_addToStack(addToStack)
+{
+    // Do nothing
+}
+
+TaskStatus DrawWeaponTask::Impl(Player* player)
+{
+    if (m_addToStack)
+    {
+        player->game->taskStack.playables.clear();
+    }
+
+    auto deck = player->GetDeckZone()->GetAll();
+    if (deck.empty())
+    {
+        return TaskStatus::STOP;
+    }
+
+    std::vector<Playable*> cards;
+    cards.reserve(m_amount);
+
+    for (auto& deckCard : deck)
+    {
+        if (deckCard->card->GetCardType() == CardType::WEAPON)
+        {
+            cards.emplace_back(deckCard);
+        }
+    }
+
+    if (cards.empty())
+    {
+        return TaskStatus::STOP;
+    }
+
+    if (static_cast<int>(cards.size()) <= m_amount)
+    {
+        for (int i = 0; i < m_amount; ++i)
+        {
+            if (m_addToStack)
+            {
+                player->game->taskStack.playables.emplace_back(cards[i]);
+            }
+
+            Generic::Draw(player, cards[i]);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < m_amount; ++i)
+        {
+            const auto pick = Random::get<std::size_t>(0, cards.size() - 1);
+
+            if (m_addToStack)
+            {
+                player->game->taskStack.playables.emplace_back(cards[pick]);
+            }
+
+            Generic::Draw(player, cards[pick]);
+            cards.erase(std::begin(cards) + pick);
+        }
+    }
+
+    return TaskStatus::COMPLETE;
+}
+
+std::unique_ptr<ITask> DrawWeaponTask::CloneImpl()
+{
+    return std::make_unique<DrawWeaponTask>(m_amount, m_addToStack);
+}
+}  // namespace RosettaStone::PlayMode::SimpleTasks
