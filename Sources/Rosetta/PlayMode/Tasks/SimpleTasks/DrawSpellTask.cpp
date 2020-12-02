ed0273fe145547733a383f00e53c24bb9c96b0bb
Chris Ohk <utilforever@gmail.com>
2020-12-02 23:50:21 +0900

    refactor: Create class 'DrawSpellTask'
    - This class represents the task for drawing spell card(s) from the deck

---
+++ Sources/Rosetta/PlayMode/Tasks/SimpleTasks/DrawSpellTask.cpp
@@ -0,0 +1,87 @@
+// This code is based on Sabberstone project.
+// Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+// Hearthstone++ is hearthstone simulator using C++ with reinforcement learning.
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+#include <Rosetta/PlayMode/Actions/Draw.hpp>
+#include <Rosetta/PlayMode/Games/Game.hpp>
+#include <Rosetta/PlayMode/Tasks/SimpleTasks/DrawSpellTask.hpp>
+#include <Rosetta/PlayMode/Zones/DeckZone.hpp>
+
+#include <effolkronium/random.hpp>
+
+using Random = effolkronium::random_static;
+
+namespace RosettaStone::PlayMode::SimpleTasks
+{
+DrawSpellTask::DrawSpellTask(int amount, bool addToStack)
+    : m_amount(amount), m_addToStack(addToStack)
+{
+    // Do nothing
+}
+
+TaskStatus DrawSpellTask::Impl(Player* player)
+{
+    if (m_addToStack)
+    {
+        player->game->taskStack.playables.clear();
+    }
+
+    auto deck = player->GetDeckZone()->GetAll();
+    if (deck.empty())
+    {
+        return TaskStatus::STOP;
+    }
+
+    std::vector<Playable*> cards;
+    cards.reserve(m_amount);
+
+    for (auto& deckCard : deck)
+    {
+        if (deckCard->card->GetCardType() == CardType::SPELL)
+        {
+            cards.emplace_back(deckCard);
+        }
+    }
+
+    if (cards.empty())
+    {
+        return TaskStatus::STOP;
+    }
+
+    if (static_cast<int>(cards.size()) <= m_amount)
+    {
+        for (int i = 0; i < m_amount; ++i)
+        {
+            if (m_addToStack)
+            {
+                player->game->taskStack.playables.emplace_back(cards[i]);
+            }
+
+            Generic::Draw(player, cards[i]);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < m_amount; ++i)
+        {
+            const auto pick = Random::get<std::size_t>(0, cards.size() - 1);
+
+            if (m_addToStack)
+            {
+                player->game->taskStack.playables.emplace_back(cards[pick]);
+            }
+
+            Generic::Draw(player, cards[pick]);
+            cards.erase(std::begin(cards) + pick);
+        }
+    }
+
+    return TaskStatus::COMPLETE;
+}
+
+std::unique_ptr<ITask> DrawSpellTask::CloneImpl()
+{
+    return std::make_unique<DrawSpellTask>(m_amount, m_addToStack);
+}
+}  // namespace RosettaStone::PlayMode::SimpleTasks
