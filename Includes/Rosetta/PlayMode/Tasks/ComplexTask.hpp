ee9dd1e845ffc1155fe945f5eb3b2bd3ff7bb166
Chris Ohk <utilforever@gmail.com>
2020-12-11 02:10:23 +0900

    refactor: Add method 'DestroyRandomEnemyMinion()'
    - Returns a list of task for destroying random enemy minion(s)

--- Includes/Rosetta/PlayMode/Tasks/ComplexTask.hpp
+++ Includes/Rosetta/PlayMode/Tasks/ComplexTask.hpp
@@ -6,6 +6,7 @@
 #ifndef ROSETTASTONE_PLAYMODE_COMPLEX_TASK_HPP
 #define ROSETTASTONE_PLAYMODE_COMPLEX_TASK_HPP
 
+#include <Rosetta/PlayMode/Tasks/SimpleTasks/DestroyTask.hpp>
 #include <Rosetta/PlayMode/Tasks/SimpleTasks/FilterStackTask.hpp>
 #include <Rosetta/PlayMode/Tasks/SimpleTasks/IncludeTask.hpp>
 #include <Rosetta/PlayMode/Tasks/SimpleTasks/RandomTask.hpp>
@@ -33,6 +34,20 @@ class ComplexTask
                 std::make_shared<SelfCondition>(SelfCondition::IsMinion()) }),
             std::make_shared<SimpleTasks::RandomTask>(EntityType::STACK, 1),
             std::make_shared<SimpleTasks::SummonStackTask>(true)
+        };
+    }
+
+    //! Returns a list of task for destroying random enemy minion(s).
+    //! \param num The number of minion(s) to destroy.
+    static TaskList DestroyRandomEnemyMinion(int num)
+    {
+        return TaskList{
+            std::make_shared<SimpleTasks::IncludeTask>(
+                EntityType::ENEMY_MINIONS),
+            std::make_shared<SimpleTasks::FilterStackTask>(SelfCondList{
+                std::make_shared<SelfCondition>(SelfCondition::IsNotDead()) }),
+            std::make_shared<SimpleTasks::RandomTask>(EntityType::STACK, num),
+            std::make_shared<SimpleTasks::DestroyTask>(EntityType::STACK)
         };
     }
 };
