b7748f55865fa8e781b7d7eaefca1e89f7fb7bbf
Chris Ohk <utilForever@gmail.com>
2020-12-16 13:28:26 +0900

    test: Add unit test code to check the number of secret

--- Tests/UnitTests/PlayMode/CardSets/ScholomanceCardsGenTests.cpp
+++ Tests/UnitTests/PlayMode/CardSets/ScholomanceCardsGenTests.cpp
@@ -14,6 +14,7 @@
 #include <Rosetta/PlayMode/Zones/DeckZone.hpp>
 #include <Rosetta/PlayMode/Zones/FieldZone.hpp>
 #include <Rosetta/PlayMode/Zones/HandZone.hpp>
+#include <Rosetta/PlayMode/Zones/SecretZone.hpp>
 
 using namespace RosettaStone;
 using namespace PlayMode;
@@ -1452,6 +1453,7 @@ TEST_CASE("[Rogue : Spell] - SCH_706 : Plagiarize")
     opPlayer->SetUsedMana(0);
 
     auto& curHand = *(curPlayer->GetHandZone());
+    auto& curSecret = *(curPlayer->GetSecretZone());
 
     const auto card1 =
         Generic::DrawCard(curPlayer, Cards::FindCardByName("Plagiarize"));
@@ -1461,6 +1463,7 @@ TEST_CASE("[Rogue : Spell] - SCH_706 : Plagiarize")
         Generic::DrawCard(opPlayer, Cards::FindCardByName("Blizzard"));
 
     game.Process(curPlayer, PlayCardTask::Spell(card1));
+    CHECK_EQ(curSecret.GetCount(), 1);
 
     game.Process(curPlayer, EndTurnTask());
     game.ProcessUntil(Step::MAIN_ACTION);
@@ -1479,6 +1482,7 @@ TEST_CASE("[Rogue : Spell] - SCH_706 : Plagiarize")
     game.Process(opPlayer, EndTurnTask());
     game.ProcessUntil(Step::MAIN_ACTION);
 
+    CHECK_EQ(curSecret.GetCount(), 0);
     CHECK_EQ(curHand.GetCount(), 2);
     CHECK_EQ(curHand[0]->card->name, "Arcane Missiles");
     CHECK_EQ(curHand[1]->card->name, "Blizzard");
