a59629e918c1fd0a475870a0dddd2cc629f9491f
Chris Ohk <utilforever@gmail.com>
2020-12-18 15:44:05 +0900

    feat(impl-card): Implement card 'Guardian Augmerchant' (BT_722)

--- Sources/Rosetta/PlayMode/CardSets/BlackTempleCardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/BlackTempleCardsGen.cpp
@@ -2471,9 +2471,21 @@ void BlackTempleCardsGen::AddNeutral(std::map<std::string, CardDef>& cards)
     // GameTag:
     // - BATTLECRY = 1
     // --------------------------------------------------------
+    // PlayReq:
+    // - REQ_TARGET_IF_AVAILABLE = 0
+    // - REQ_MINION_TARGET = 0
+    // --------------------------------------------------------
     // RefTag:
     // - DIVINE_SHIELD = 1
     // --------------------------------------------------------
+    power.ClearData();
+    power.AddPowerTask(std::make_shared<DamageTask>(EntityType::TARGET, 1));
+    power.AddPowerTask(std::make_shared<SetGameTagTask>(
+        EntityType::TARGET, GameTag::DIVINE_SHIELD, 1));
+    cards.emplace(
+        "BT_722",
+        CardDef(power, PlayReqs{ { PlayReq::REQ_TARGET_IF_AVAILABLE, 0 },
+                                 { PlayReq::REQ_MINION_TARGET, 0 } }));
 
     // --------------------------------------- MINION - NEUTRAL
     // [BT_723] Rocket Augmerchant - COST:1 [ATK:2/HP:1]
