a04ad9cdc295bae68cfd3fe9f119d9f120209d95
Chris Ohk <utilforever@gmail.com>
2021-01-30 11:40:33 +0900

    fix: Correct unit test code of card 'King Phaoris' (ULD_304)

--- Tests/UnitTests/PlayMode/CardSets/UldumCardsGenTests.cpp
+++ Tests/UnitTests/PlayMode/CardSets/UldumCardsGenTests.cpp
@@ -7307,8 +7307,16 @@ TEST_CASE("[Neutral : Minion] - ULD_304 : King Phaoris")
 
     game.Process(curPlayer, PlayCardTask::Minion(card2));
     CHECK_EQ(curField.GetCount(), 7);
-    CHECK_EQ(curField[5]->card->GetCost(), 3);
-    CHECK_EQ(curField[6]->card->GetCost(), 6);
+    if (curField[1]->card->name == "Kirin Tor Tricaster")
+    {
+        CHECK_EQ(curField[5]->card->GetCost(), 4);
+        CHECK_EQ(curField[6]->card->GetCost(), 7);
+    }
+    else
+    {
+        CHECK_EQ(curField[5]->card->GetCost(), 3);
+        CHECK_EQ(curField[6]->card->GetCost(), 6);
+    }
 }
 
 // --------------------------------------- MINION - NEUTRAL
