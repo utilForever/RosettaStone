b1e084a9a1774d3a4b9abc42cd3d08a5bb72a930
Chris Ohk <utilforever@gmail.com>
2021-02-20 20:29:43 +0900

    feat(update-data): Add code to load card data of 'THE_BARRENS'

--- Sources/Rosetta/PlayMode/Cards/CardDefs.cpp
+++ Sources/Rosetta/PlayMode/Cards/CardDefs.cpp
@@ -16,6 +16,7 @@
 #include <Rosetta/PlayMode/CardSets/LootapaloozaCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/ScholomanceCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/TgtCardsGen.hpp>
+#include <Rosetta/PlayMode/CardSets/TheBarrensCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/UldumCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/YoDCardsGen.hpp>
 #include <Rosetta/PlayMode/Cards/CardDefs.hpp>
@@ -41,6 +42,7 @@ CardDefs::CardDefs()
     BlackTempleCardsGen::AddAll(m_data);
     ScholomanceCardsGen::AddAll(m_data);
     DarkmoonFaireCardsGen::AddAll(m_data);
+    TheBarrensCardsGen::AddAll(m_data);
 }
 
 CardDefs::~CardDefs()
