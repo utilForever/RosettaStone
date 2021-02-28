51616dd963ee278c76defeb5a57b276d9d2e0bfa
Chris Ohk <utilforever@gmail.com>
2021-03-01 02:05:06 +0900

    feat(console): Create class 'Console'

---
+++ Extensions/RosettaConsole/Console.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "Console.hpp"
+
+namespace RosettaStone
+{
+Console::Console(Mode mode) : m_mode{ mode }
+{
+    // Do nothing
+}
+
+void Console::ProcessGame()
+{
+}
+}  // namespace RosettaStone
---
+++ Extensions/RosettaConsole/Console.hpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef ROSETTASTONE_CONSOLE_HPP
+#define ROSETTASTONE_CONSOLE_HPP
+
+namespace RosettaStone
+{
+class Console
+{
+ public:
+    enum class Mode
+    {
+        STANDARD,
+        WILD,
+        BATTLEGROUNDS,
+    };
+
+    //! Constructs a Console instance with \p mode.
+    //! \param mode The game mode such as standard, wild and battlegrounds.
+    explicit Console(Mode mode);
+
+    //! Process a Hearthstone game.
+    void ProcessGame();
+
+ private:
+    Mode m_mode;
+};
+}  // namespace RosettaStone
+
+#endif
