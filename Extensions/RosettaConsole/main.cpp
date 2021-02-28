55aeafcd29390037f5b3024762105c1c66bf2bfb
Chris Ohk <utilforever@gmail.com>
2021-03-01 02:05:24 +0900

    feat(console): Add code to process CLI and game

--- Extensions/RosettaConsole/main.cpp
+++ Extensions/RosettaConsole/main.cpp
@@ -4,7 +4,45 @@
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
-int main()
+#include "Console.hpp"
+
+#include <lyra/lyra.hpp>
+
+using namespace RosettaStone;
+
+int main(int argc, char* argv[])
 {
-    return 0;
+    int mode = 0;
+    bool showHelp = false;
+
+    // Process CLI
+    const auto cli =
+        lyra::cli() | lyra::help(showHelp) |
+        lyra::opt(mode, "mode")["m"]["--mode"](
+            "The game mode. (1 - Standard, 2 - Wild, 3 - Battlegrounds");
+    const auto result = cli.parse({ argc, argv });
+
+    if (!result)
+    {
+        std::cerr << "Error in command line: " << result.errorMessage() << '\n';
+        std::cerr << cli << '\n';
+        return EXIT_FAILURE;
+    }
+
+    if (showHelp)
+    {
+        std::cout << cli << '\n';
+        return EXIT_SUCCESS;
+    }
+
+    if (mode < 1 || mode > 3)
+    {
+        std::cerr << "Invalid game mode.\n";
+        return EXIT_FAILURE;
+    }
+
+    Console console{ static_cast<Console::Mode>(mode) };
+    console.ProcessGame();
+
+    return EXIT_SUCCESS;
 }
