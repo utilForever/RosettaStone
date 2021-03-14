f6582b63e2a43014f41e15e35b81ac4961f83cb2
Chris Ohk <utilforever@gmail.com>
2021-03-14 23:52:46 +0900

    feat(console): Implement method 'InputDeckCodes()'

--- Extensions/RosettaConsole/Console.cpp
+++ Extensions/RosettaConsole/Console.cpp
@@ -6,6 +6,11 @@
 
 #include "Console.hpp"
 
+#include <Rosetta/PlayMode/Decks/Deck.hpp>
+#include <Rosetta/PlayMode/Utils/DeckCode.hpp>
+
+#include <iostream>
+
 namespace RosettaStone
 {
 Console::Console(Mode mode) : m_mode{ mode }
@@ -29,6 +34,35 @@ void Console::ProcessGame()
 
 void Console::InputDeckCodes()
 {
+    std::string deckCode1, deckCode2;
+
+    std::cout << "Input first player's deck code: ";
+    std::cin >> deckCode1;
+
+    std::cout << "Input second player's deck code: ";
+    std::cin >> deckCode2;
+
+    PlayMode::Deck deck1 = PlayMode::DeckCode::Decode(deckCode1);
+    PlayMode::Deck deck2 = PlayMode::DeckCode::Decode(deckCode2);
+
+    if (m_mode == Mode::STANDARD)
+    {
+        if (deck1.GetFormatType() != FormatType::STANDARD ||
+            deck2.GetFormatType() != FormatType::STANDARD)
+        {
+            throw std::runtime_error(
+                "The game mode and the deck's format type aren't match.");
+        }
+    }
+    else
+    {
+        if (deck1.GetFormatType() != FormatType::WILD ||
+            deck2.GetFormatType() != FormatType::WILD)
+        {
+            throw std::runtime_error(
+                "The game mode and the deck's format type aren't match.");
+        }
+    }
 }
 
 void Console::PlayBattlegrounds()
