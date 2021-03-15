f9eac392b1c460b411b4206ed59a56c35a08d2d3
Chris Ohk <utilforever@gmail.com>
2021-03-15 20:40:20 +0900

    feat(console): Add code to remove unnecessary substrings

--- Sources/Rosetta/PlayMode/Loaders/CardLoader.cpp
+++ Sources/Rosetta/PlayMode/Loaders/CardLoader.cpp
@@ -4,6 +4,7 @@
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
+#include <Rosetta/Common/Utils.hpp>
 #include <Rosetta/PlayMode/Loaders/CardLoader.hpp>
 
 #include <fstream>
@@ -40,9 +41,9 @@ void CardLoader::Load(std::vector<Card*>& cards)
                                      : cardData["name"].get<std::string>();
         const int dbfID =
             cardData["dbfId"].is_null() ? 0 : cardData["dbfId"].get<int>();
-        const std::string text = cardData["text"].is_null()
-                                     ? ""
-                                     : cardData["text"].get<std::string>();
+        std::string text = cardData["text"].is_null()
+                               ? ""
+                               : cardData["text"].get<std::string>();
 
         const int attack =
             cardData["attack"].is_null() ? 0 : cardData["attack"].get<int>();
@@ -134,7 +135,8 @@ void CardLoader::Load(std::vector<Card*>& cards)
         {
             gameTags.emplace(GameTag::DIVINE_SHIELD, 1);
         }
-        else if (dbfID == 16221 || dbfID == 16222 || dbfID == 16223 || dbfID == 16225)
+        else if (dbfID == 16221 || dbfID == 16222 || dbfID == 16223 ||
+                 dbfID == 16225)
         {
             cardRace = static_cast<int>(Race::TOTEM);
         }
@@ -143,6 +145,10 @@ void CardLoader::Load(std::vector<Card*>& cards)
         card->id = id;
         card->dbfID = dbfID;
         card->name = name;
+
+        // Remove unnecessary substrings
+        RemoveSubstrs(text, std::string{ "\n" });
+        RemoveSubstrs(text, std::string{ "[x]" });
         card->text = text;
 
         card->gameTags = gameTags;
