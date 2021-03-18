d1c4cb8e2e40f69b2311533f4d8c6861c6a31432
Chris Ohk <utilforever@gmail.com>
2021-03-19 02:33:34 +0900

    feat(console): Implement method 'GetActionForMulligan()'

--- Sources/Rosetta/PlayMode/Agents/RandomAgent.cpp
+++ Sources/Rosetta/PlayMode/Agents/RandomAgent.cpp
@@ -5,6 +5,12 @@
 
 #include <Rosetta/PlayMode/Agents/RandomAgent.hpp>
 
+#include <effolkronium/random.hpp>
+
+#include <cassert>
+
+using Random = effolkronium::random_static;
+
 namespace RosettaStone::PlayMode
 {
 void RandomAgent::SetPlayerType(PlayerType playerType)
@@ -14,6 +20,22 @@ void RandomAgent::SetPlayerType(PlayerType playerType)
 
 std::vector<int> RandomAgent::GetActionForMulligan(const Game& state)
 {
-    return std::vector<int>{};
+    assert(m_playerType == PlayerType::PLAYER1 ||
+           m_playerType == PlayerType::PLAYER2);
+
+    const Player* player = m_playerType == PlayerType::PLAYER1
+                               ? state.GetPlayer1()
+                               : state.GetPlayer2();
+
+    std::vector<int> indices;
+    for (std::size_t i = 0; i < player->choice->choices.size(); ++i)
+    {
+        if (Random::get<std::size_t>(0, 1) == 1)
+        {
+            indices.emplace_back(i);
+        }
+    }
+
+    return indices;
 }
 }  // namespace RosettaStone::PlayMode
