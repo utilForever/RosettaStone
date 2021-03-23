921e468cf4cc433a92322a9633a874f42a0e22cd
Chris Ohk <utilforever@gmail.com>
2021-03-23 21:58:08 +0900

    test: Add unit test code for checking function 'ChooseNElements()'

---
+++ Tests/UnitTests/Common/UtilsTests.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include "doctest_proxy.hpp"
+
+#include <Rosetta/Common/Utils.hpp>
+
+#include <array>
+
+TEST_CASE("[Utils] - ChooseNElements (std::array)")
+{
+    std::array<int*, 5> intVec{};
+    intVec[0] = new int(10);
+    intVec[1] = new int(20);
+    intVec[2] = new int(30);
+    intVec[3] = new int(40);
+    intVec[4] = new int(50);
+
+    std::vector<int*> result = ChooseNElements(intVec, 2);
+    CHECK_EQ(result.size(), 2);
+    CHECK_NE(*result[0], *result[1]);
+
+    for (auto& elem : intVec)
+    {
+        delete elem;
+    }
+}
+
+TEST_CASE("[Utils] - ChooseNElements (std::vector)")
+{
+    std::vector<int*> intVec;
+    intVec.emplace_back(new int(10));
+    intVec.emplace_back(new int(20));
+    intVec.emplace_back(new int(30));
+    intVec.emplace_back(new int(40));
+    intVec.emplace_back(new int(50));
+
+    std::vector<int*> result = ChooseNElements(intVec, 2);
+    CHECK_EQ(result.size(), 2);
+    CHECK_NE(*result[0], *result[1]);
+
+    for (auto& elem : intVec)
+    {
+        delete elem;
+    }
+}
