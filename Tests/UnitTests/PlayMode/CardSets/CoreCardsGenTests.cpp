8339b024f21ecb6a34b1f41a7d53891c6559213b
Chris Ohk <utilforever@gmail.com>
2021-04-20 21:55:56 +0900

    feat(impl-card): Implement card 'Lock and Load' (CORE_AT_061)

--- Tests/UnitTests/PlayMode/CardSets/CoreCardsGenTests.cpp
+++ Tests/UnitTests/PlayMode/CardSets/CoreCardsGenTests.cpp
@@ -892,3 +892,64 @@ TEST_CASE("[Druid : Minion] - CS3_012 : Nordrassil Druid")
 
     CHECK_EQ(card2->GetCost(), 4);
 }
+
+// ----------------------------------------- SPELL - HUNTER
+// [CORE_AT_061] Lock and Load - COST:1
+// - Set: CORE, Rarity: Epic
+// --------------------------------------------------------
+// Text: Each time you cast a spell this turn,
+//       add a random Hunter card to your hand.
+// --------------------------------------------------------
+TEST_CASE("[Hunter : Spell] - CORE_AT_061 : Lock and Load")
+{
+    GameConfig config;
+    config.formatType = FormatType::STANDARD;
+    config.player1Class = CardClass::DRUID;
+    config.player2Class = CardClass::HUNTER;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = false;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curHand = *(curPlayer->GetHandZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Lock and Load"));
+    const auto card2 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Arcane Shot"));
+    const auto card3 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Arcane Shot"));
+    const auto card4 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Timber Wolf"));
+
+    game.Process(curPlayer, PlayCardTask::Spell(card1));
+    CHECK_EQ(curHand.GetCount(), 3);
+
+    game.Process(curPlayer, PlayCardTask::Minion(card4));
+    CHECK_EQ(curHand.GetCount(), 2);
+
+    game.Process(curPlayer,
+                 PlayCardTask::SpellTarget(card2, opPlayer->GetHero()));
+    CHECK_EQ(curHand.GetCount(), 2);
+    CHECK(curHand[1]->card->IsCardClass(CardClass::HUNTER));
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer,
+                 PlayCardTask::SpellTarget(card3, opPlayer->GetHero()));
+    CHECK_EQ(curHand.GetCount(), 1);
+}
