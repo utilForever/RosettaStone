be80a599cc6190de4ac0471b27b201e5ad6801d8
Chris Ohk <utilforever@gmail.com>
2021-05-07 13:29:10 +0900

    fix: Correct logic of card 'Avenge' (FP1_020)

--- Sources/Rosetta/PlayMode/CardSets/NaxxCardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/NaxxCardsGen.cpp
@@ -108,16 +108,12 @@ void NaxxCardsGen::AddPaladin(std::map<std::string, CardDef>& cards)
     power.ClearData();
     power.AddTrigger(std::make_shared<Trigger>(TriggerType::DEATH));
     power.GetTrigger()->triggerSource = TriggerSource::FRIENDLY;
-    power.GetTrigger()->tasks = {
-        std::make_shared<ConditionTask>(
-            EntityType::SOURCE, SelfCondList{ std::make_shared<SelfCondition>(
-                                    SelfCondition::IsFieldCount(0)) }),
-        std::make_shared<FlagTask>(
-            false, ComplexTask::ActivateSecret(TaskList{
-                       std::make_shared<RandomTask>(EntityType::MINIONS, 1),
-                       std::make_shared<AddEnchantmentTask>(
-                           "FP1_020e", EntityType::STACK) }))
-    };
+    power.GetTrigger()->condition = std::make_shared<SelfCondition>(
+        SelfCondition::IsFieldCount(2, RelaSign::GEQ));
+    power.GetTrigger()->tasks = { ComplexTask::ActivateSecret(
+        TaskList{ std::make_shared<RandomTask>(EntityType::MINIONS, 1),
+                  std::make_shared<AddEnchantmentTask>("FP1_020e",
+                                                       EntityType::STACK) }) };
     cards.emplace("FP1_020", CardDef(power));
 }
 
