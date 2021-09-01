dd4619eb64e43bd1d1ceaa6f48fcac754154daa4
Chris Ohk <utilforever@gmail.com>
2021-09-01 23:09:16 +0900

    feat(update-data): Apply Battlegrounds update by Hearthstone Patch 21.2
    - NEW HERO: Master Nguyen, Cariel Roame

--- Includes/Rosetta/Common/Constants.hpp
+++ Includes/Rosetta/Common/Constants.hpp
@@ -107,7 +107,7 @@ constexpr int MAX_SECERT_SIZE = 5;
 constexpr int NUM_BATTLEGROUNDS_PLAYERS = 8;
 
 //! The number of heroes in Battlegrounds.
-constexpr int NUM_BATTLEGROUNDS_HEROES = 65;
+constexpr int NUM_BATTLEGROUNDS_HEROES = 67;
 
 //! The number of heroes on the selection list in Battlegrounds.
 constexpr int NUM_HEROES_ON_SELECTION_LIST = 4;
