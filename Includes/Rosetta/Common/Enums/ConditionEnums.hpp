ff5e2e167a5479e700a055e2f865b65619a7f2b0
Chris Ohk <utilforever@gmail.com>
2021-09-25 10:31:51 +0900

    feat(impl-card): Add enum 'MultiCondLogic'
    - An enumerator for checking the logic of multiple conditions

---
+++ Includes/Rosetta/Common/Enums/ConditionEnums.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef ROSETTASTONE_CONDITION_ENUMS_HPP
+#define ROSETTASTONE_CONDITION_ENUMS_HPP
+
+namespace RosettaStone
+{
+//! \brief An enumerator for checking the logic of multiple conditions.
+enum class MultiCondLogic
+{
+    AND,
+    OR
+};
+}  // namespace RosettaStone
+
+#endif  // ROSETTASTONE_CONDITION_ENUMS_HPP
