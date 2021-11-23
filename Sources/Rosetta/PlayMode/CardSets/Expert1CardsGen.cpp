990af0df124a73bd1cf8f8d426d3cf8792fd0075
Chris Ohk <utilforever@gmail.com>
2021-11-23 23:28:37 +0900

    style: Separate text to improve readability

--- Sources/Rosetta/PlayMode/CardSets/Expert1CardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/Expert1CardsGen.cpp
@@ -162,7 +162,8 @@ void Expert1CardsGen::AddDruid(std::map<std::string, CardDef>& cards)
     // - Faction: Neutral, Set: Expert1, Rarity: Common
     // - Spell School: Nature
     // --------------------------------------------------------
-    // Text: Give your minions "<b>Deathrattle:</b> Summon a 2/2 Treant."
+    // Text: Give your minions
+    //       "<b>Deathrattle:</b> Summon a 2/2 Treant."
     // --------------------------------------------------------
     // RefTag:
     // - DEATHRATTLE = 1
