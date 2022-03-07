bf38cb8d0c7e1c7b4da373701aaa3592f7c42aaa
Chris Ohk <utilforever@gmail.com>
2022-03-07 19:24:27 +0900

    feat(vanilla): Implement card 'Elven Archer' (VAN_CS2_189)

--- Sources/Rosetta/PlayMode/CardSets/VanillaCardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/VanillaCardsGen.cpp
@@ -5692,6 +5692,16 @@ void VanillaCardsGen::AddNeutral(std::map<std::string, CardDef>& cards)
     // GameTag:
     // - BATTLECRY = 1
     // --------------------------------------------------------
+    // PlayReq:
+    // - REQ_TARGET_IF_AVAILABLE = 0
+    // - REQ_NONSELF_TARGET = 0
+    // --------------------------------------------------------
+    power.ClearData();
+    power.AddPowerTask(std::make_shared<DamageTask>(EntityType::TARGET, 1));
+    cards.emplace(
+        "VAN_CS2_189",
+        CardDef(power, PlayReqs{ { PlayReq::REQ_TARGET_IF_AVAILABLE, 0 },
+                                 { PlayReq::REQ_NONSELF_TARGET, 0 } }));
 
     // --------------------------------------- MINION - NEUTRAL
     // [VAN_CS2_196] Razorfen Hunter - COST:3 [ATK:2/HP:3]
