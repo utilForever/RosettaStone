5a0ebc72b6f707cd5ae72aed22d7a3abd492fbe9
Chris Ohk <utilforever@gmail.com>
2022-03-13 13:57:09 +0900

    feat(vanilla): Implement card 'Youthful Brewmaster' (VAN_EX1_049)

--- Sources/Rosetta/PlayMode/CardSets/VanillaCardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/VanillaCardsGen.cpp
@@ -6361,6 +6361,20 @@ void VanillaCardsGen::AddNeutral(std::map<std::string, CardDef>& cards)
     // GameTag:
     // - BATTLECRY = 1
     // --------------------------------------------------------
+    // PlayReq:
+    // - REQ_TARGET_IF_AVAILABLE = 0
+    // - REQ_NONSELF_TARGET = 0
+    // - REQ_MINION_TARGET = 0
+    // - REQ_FRIENDLY_TARGET = 0
+    // --------------------------------------------------------
+    power.ClearData();
+    power.AddPowerTask(std::make_shared<ReturnHandTask>(EntityType::TARGET));
+    cards.emplace(
+        "VAN_EX1_049",
+        CardDef(power, PlayReqs{ { PlayReq::REQ_TARGET_IF_AVAILABLE, 0 },
+                                 { PlayReq::REQ_NONSELF_TARGET, 0 },
+                                 { PlayReq::REQ_MINION_TARGET, 0 },
+                                 { PlayReq::REQ_FRIENDLY_TARGET, 0 } }));
 
     // --------------------------------------- MINION - NEUTRAL
     // [VAN_EX1_050] Coldlight Oracle - COST:3 [ATK:2/HP:2]
