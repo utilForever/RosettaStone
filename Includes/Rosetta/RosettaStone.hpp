13fd7c555cdf66d14a44233788acbb387a0d143b
Chris Ohk <utilforever@gmail.com>
2022-03-19 01:38:54 +0900

    feat(patch): Create card generation files for 'THE_SUNKEN_CITY'

--- Includes/Rosetta/RosettaStone.hpp
+++ Includes/Rosetta/RosettaStone.hpp
@@ -106,6 +106,7 @@
 #include <Rosetta/PlayMode/CardSets/StormwindCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/TgtCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/TheBarrensCardsGen.hpp>
+#include <Rosetta/PlayMode/CardSets/TheSunkenCityCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/TrollCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/UldumCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/UngoroCardsGen.hpp>
