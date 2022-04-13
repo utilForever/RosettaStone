7535faa99803021f964bc945255d46f8c495277a
Chris Ohk <utilforever@gmail.com>
2022-04-13 23:50:03 +0900

    feat(patch): Add code to ignore card 'Brann Bronzebeard' (CORE_LOE_077)

--- Sources/Rosetta/Battlegrounds/Loaders/CardLoader.cpp
+++ Sources/Rosetta/Battlegrounds/Loaders/CardLoader.cpp
@@ -31,7 +31,8 @@ void CardLoader::Load(std::array<Card, NUM_BATTLEGROUNDS_CARDS>& cards)
 
         if (cardData["set"].is_number())
         {
-            // NOTE: 1810 -> CardSet::PLACEHOLDER_202204
+            // NOTE: We don't know the meaning of the value "1810".
+            //       We replace it with "CardSet::PLACEHOLDER_202204".
             cardSet = static_cast<int>(CardSet::PLACEHOLDER_202204);
         }
         else
@@ -46,6 +47,14 @@ void CardLoader::Load(std::array<Card, NUM_BATTLEGROUNDS_CARDS>& cards)
         }
 
         const std::string id = cardData["id"].get<std::string>();
+        // NOTE: The value "isBattlegroundsPoolMinion" of
+        //       Brann Bronzebeard (CORE_LOE_077) is true, too.
+        //       It's incorrect. Therefore, we ignore it.
+        if (id == "CORE_LOE_077")
+        {
+            continue;
+        }
+
         const int dbfID =
             cardData["dbfId"].is_null() ? 0 : cardData["dbfId"].get<int>();
         const int normalDbfID =
