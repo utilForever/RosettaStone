aa77d7fd2f87f8630005abdbc8fafc58228e50ac
Chris Ohk <utilforever@gmail.com>
2022-04-17 14:04:47 +0900

    feat(core): Create card generation files for 'PLACEHOLDER_202204'

---
+++ Sources/Rosetta/PlayMode/CardSets/Placeholder202204CardsGen.cpp
@@ -0,0 +1,161 @@
+// This code is based on Sabberstone project.
+// Copyright (c) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+// Hearthstone++ is hearthstone simulator using C++ with reinforcement learning.
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+#include <Rosetta/PlayMode/CardSets/Placeholder202204CardsGen.hpp>
+
+namespace RosettaStone::PlayMode
+{
+void Placeholder202204CardsGen::AddHeroes(std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddHeroPowers(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddDruid(std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddDruidNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddHunter(std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddHunterNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddMage(std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddMageNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddPaladin(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddPaladinNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddPriest(std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddPriestNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddRogue(std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddRogueNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddShaman(std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddShamanNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddWarlock(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddWarlockNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddWarrior(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddWarriorNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddDemonHunter(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddDemonHunterNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddNeutral(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddNeutralNonCollect(
+    std::map<std::string, CardDef>& cards)
+{
+}
+
+void Placeholder202204CardsGen::AddAll(std::map<std::string, CardDef>& cards)
+{
+    AddHeroes(cards);
+    AddHeroPowers(cards);
+
+    AddDruid(cards);
+    AddDruidNonCollect(cards);
+
+    AddHunter(cards);
+    AddHunterNonCollect(cards);
+
+    AddMage(cards);
+    AddMageNonCollect(cards);
+
+    AddPaladin(cards);
+    AddPaladinNonCollect(cards);
+
+    AddPriest(cards);
+    AddPriestNonCollect(cards);
+
+    AddRogue(cards);
+    AddRogueNonCollect(cards);
+
+    AddShaman(cards);
+    AddShamanNonCollect(cards);
+
+    AddWarlock(cards);
+    AddWarlockNonCollect(cards);
+
+    AddWarrior(cards);
+    AddWarriorNonCollect(cards);
+
+    AddDemonHunter(cards);
+    AddDemonHunterNonCollect(cards);
+
+    AddNeutral(cards);
+    AddNeutralNonCollect(cards);
+}
+}  // namespace RosettaStone::PlayMode
