6fff841a6332b5918fbc2dad93ef7e0b2359763a
Chris Ohk <utilforever@gmail.com>
2022-04-17 14:30:48 +0900

    feat(core): Delete card 'Menagerie Warden' (CORE_KAR_065)

--- Sources/Rosetta/PlayMode/CardSets/CoreCardsGen.cpp
+++ Sources/Rosetta/PlayMode/CardSets/CoreCardsGen.cpp
@@ -203,31 +203,6 @@ void CoreCardsGen::AddDruid(std::map<std::string, CardDef>& cards)
     cards.emplace("CORE_EX1_573",
                   CardDef(power, ChooseCardIDs{ "EX1_573a", "EX1_573b" }));
 
-    // ----------------------------------------- MINION - DRUID
-    // [CORE_KAR_065] Menagerie Warden - COST:5 [ATK:4/HP:4]
-    // - Set: CORE, Rarity: Common
-    // --------------------------------------------------------
-    // Text: <b>Battlecry:</b> Choose a friendly Beast.
-    //       Summon a copy of it.
-    // --------------------------------------------------------
-    // GameTag:
-    // - BATTLECRY = 1
-    // --------------------------------------------------------
-    // PlayReq:
-    // - REQ_TARGET_IF_AVAILABLE = 0
-    // - REQ_FRIENDLY_TARGET = 0
-    // - REQ_MINION_TARGET = 0
-    // - REQ_TARGET_WITH_RACE = 20
-    // --------------------------------------------------------
-    power.ClearData();
-    power.AddPowerTask(std::make_shared<SummonCopyTask>(EntityType::TARGET));
-    cards.emplace(
-        "CORE_KAR_065",
-        CardDef(power, PlayReqs{ { PlayReq::REQ_TARGET_IF_AVAILABLE, 0 },
-                                 { PlayReq::REQ_FRIENDLY_TARGET, 0 },
-                                 { PlayReq::REQ_MINION_TARGET, 0 },
-                                 { PlayReq::REQ_TARGET_WITH_RACE, 20 } }));
-
     // ------------------------------------------ SPELL - DRUID
     // [CORE_OG_047] Feral Rage - COST:3
     // - Set: CORE, Rarity: Common
