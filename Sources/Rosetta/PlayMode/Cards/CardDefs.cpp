f6c9888211ef071c050d5933e08cdfb3842bfd20
Chris Ohk <utilforever@gmail.com>
2022-04-17 14:05:08 +0900

    feat(core): Add code to load card data of 'PLACEHOLDER_202204'

--- Sources/Rosetta/PlayMode/Cards/CardDefs.cpp
+++ Sources/Rosetta/PlayMode/Cards/CardDefs.cpp
@@ -22,6 +22,7 @@
 #include <Rosetta/PlayMode/CardSets/LootapaloozaCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/NaxxCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/OgCardsGen.hpp>
+#include <Rosetta/PlayMode/CardSets/Placeholder202204CardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/ScholomanceCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/StormwindCardsGen.hpp>
 #include <Rosetta/PlayMode/CardSets/TgtCardsGen.hpp>
@@ -69,6 +70,7 @@ CardDefs::CardDefs()
     TheBarrensCardsGen::AddAll(m_data);
     StormwindCardsGen::AddAll(m_data);
     TheSunkenCityCardsGen::AddAll(m_data);
+    Placeholder202204CardsGen::AddAll(m_data);
 }
 
 CardDefs::~CardDefs()
