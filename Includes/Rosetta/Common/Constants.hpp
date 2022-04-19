b0008db4d6fb5b0a040935d7569ea1daf1dc1dd9
Chris Ohk <utilforever@gmail.com>
2022-04-19 19:04:43 +0900

    feat(core): Update the value of constant 'NUM_PLAY_MODE_CARDS'

--- Includes/Rosetta/Common/Constants.hpp
+++ Includes/Rosetta/Common/Constants.hpp
@@ -71,7 +71,7 @@ constexpr std::array<CardSet, 1> CLASSIC_CARD_SETS = {
 };
 
 //! The number of Play mode cards.
-constexpr int NUM_PLAY_MODE_CARDS = 14391;
+constexpr int NUM_PLAY_MODE_CARDS = 14334;
 
 //! The number of Battlegrounds cards.
 constexpr int NUM_BATTLEGROUNDS_CARDS = 15742;
