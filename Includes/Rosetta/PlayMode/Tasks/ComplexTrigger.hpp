534f6a5bfef0cde7af05b0c54a262568e0c03f0c
Chris Ohk <utilforever@gmail.com>
2022-04-23 20:01:21 +0900

    feat(naga): Add function 'PlayedNagaWhileHoldingThis()'
    - Adds a trigger for checking "If you played a Naga while holding this"

--- Includes/Rosetta/PlayMode/Tasks/ComplexTrigger.hpp
+++ Includes/Rosetta/PlayMode/Tasks/ComplexTrigger.hpp
@@ -6,8 +6,12 @@
 #ifndef ROSETTASTONE_PLAYMODE_COMPLEX_TRIGGER_HPP
 #define ROSETTASTONE_PLAYMODE_COMPLEX_TRIGGER_HPP
 
+#include <Rosetta/PlayMode/Tasks/SimpleTasks.hpp>
+
 namespace RosettaStone::PlayMode
 {
+using SelfCondList = std::vector<std::shared_ptr<SelfCondition>>;
+
 //!
 //! \brief ComplexTrigger class.
 //!
@@ -17,6 +21,21 @@ namespace RosettaStone::PlayMode
 class ComplexTrigger
 {
  public:
+    //! Adds a trigger for checking "If you played a Naga while holding this".
+    //! \param power A power to add the trigger.
+    static void PlayedNagaWhileHoldingThis(Power& power)
+    {
+        power.AddTrigger(std::make_shared<Trigger>(TriggerType::PLAY_CARD));
+        power.GetTrigger()->triggerActivation = TriggerActivation::HAND;
+        power.GetTrigger()->triggerSource = TriggerSource::FRIENDLY;
+        power.GetTrigger()->conditions = SelfCondList{
+            std::make_shared<SelfCondition>(SelfCondition::IsRace(Race::NAGA))
+        };
+        power.GetTrigger()->tasks = {
+            std::make_shared<SimpleTasks::SetGameTagTask>(
+                EntityType::SOURCE, GameTag::TAG_SCRIPT_DATA_NUM_1, 1)
+        };
+    }
 };
 }  // namespace RosettaStone::PlayMode
 
