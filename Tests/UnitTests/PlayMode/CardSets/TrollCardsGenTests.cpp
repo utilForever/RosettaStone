9f3c241ce83338466c43ef9d6f9deb975fc390a2
Chris Ohk <utilforever@gmail.com>
2022-05-23 20:59:13 +0900

    feat(core): Implement card 'Flash of Light' (TRL_307)

--- Tests/UnitTests/PlayMode/CardSets/TrollCardsGenTests.cpp
+++ Tests/UnitTests/PlayMode/CardSets/TrollCardsGenTests.cpp
@@ -217,3 +217,57 @@ TEST_CASE("[Mage : Minion] - TRL_315 : Pyromaniac")
     CHECK_EQ(opField.GetCount(), 0);
     CHECK_EQ(curHand.GetCount(), 7);
 }
+
+// ---------------------------------------- SPELL - PALADIN
+// [TRL_307] Flash of Light - COST:2
+// - Set: Troll, Rarity: Common
+// --------------------------------------------------------
+// Text: Restore 4 Health. Draw a card.
+// --------------------------------------------------------
+// PlayReq:
+// - REQ_TARGET_TO_PLAY = 0
+// --------------------------------------------------------
+TEST_CASE("[Paladin : Spell] - TRL_307 : Flash of Light")
+{
+    GameConfig config;
+    config.player1Class = CardClass::PALADIN;
+    config.player2Class = CardClass::WARRIOR;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = true;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curHand = *(curPlayer->GetHandZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Flash of Light"));
+    const auto card2 =
+        Generic::DrawCard(opPlayer, Cards::FindCardByName("Fireball"));
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(opPlayer,
+                 PlayCardTask::SpellTarget(card2, curPlayer->GetHero()));
+    CHECK_EQ(curPlayer->GetHero()->GetHealth(), 24);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    CHECK_EQ(curHand.GetCount(), 6);
+
+    game.Process(curPlayer,
+                 PlayCardTask::SpellTarget(card1, curPlayer->GetHero()));
+    CHECK_EQ(curPlayer->GetHero()->GetHealth(), 28);
+    CHECK_EQ(curHand.GetCount(), 6);
+}
