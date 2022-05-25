1bb171c427f54f45ad0d16937df68fcb4e698280
Chris Ohk <utilforever@gmail.com>
2022-05-25 14:19:53 +0900

    feat(core): Create unit test file for 'GANGS' cards

---
+++ Tests/UnitTests/PlayMode/CardSets/GangsCardsGenTests.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 Chris Ohk, Youngjoong Kim, SeungHyun Jeon
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <Utils/CardSetUtils.hpp>
+
+TEST_CASE("[GangsCardsGen] - Temp")
+{
+    CHECK(true);
+}
