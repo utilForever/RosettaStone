c149a158b4e8d8a95f826c01b839d93e226de7d6
Chris Ohk <utilforever@gmail.com>
2022-06-10 15:14:31 +0900

    feat(core): Add variable 'turn' and code to assign it

--- Sources/Rosetta/PlayMode/Actions/PlayCard.cpp
+++ Sources/Rosetta/PlayMode/Actions/PlayCard.cpp
@@ -76,7 +76,8 @@ void PlayCard(Player* player, Playable* source, Character* target, int fieldPos,
     // Increase the number of cards played this turn
     const int val = player->GetNumCardsPlayedThisTurn();
     player->SetNumCardsPlayedThisTurn(val + 1);
-    player->playHistory.emplace_back(PlayHistory(source, target, chooseOne));
+    player->playHistory.emplace_back(
+        PlayHistory(source, target, player->game->GetTurn(), chooseOne));
 
     // Record played cards for effect of cards
     // (i.e. Obsidian Shard and Lynessa Sunsorrow)
