c0646fa40f20f10ad055d9d1b0a6e014470f3e13
Chris Ohk <utilforever@gmail.com>
2022-07-08 22:19:57 +0900

    feat(patch): Apply card update for Hearthstone Patch 23.6
    - NUM_PLAY_MODE_CARDS: 15094 -> 15090

--- Includes/Rosetta/Common/Constants.hpp
+++ Includes/Rosetta/Common/Constants.hpp
@@ -71,7 +71,7 @@ constexpr std::array<CardSet, 1> CLASSIC_CARD_SETS = {
 };
 
 //! The number of Play mode cards.
-constexpr int NUM_PLAY_MODE_CARDS = 15074;
+constexpr int NUM_PLAY_MODE_CARDS = 15090;
 
 //! The number of Battlegrounds cards.
 constexpr int NUM_BATTLEGROUNDS_CARDS = 16682;
