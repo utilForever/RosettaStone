ff4fdf86b43e75b54e0a38d7b55a3f9b53823038
Chris Ohk <utilforever@gmail.com>
2022-07-29 20:40:59 +0900

    feat(impl-card): Implement card 'Imp Gang Boss' (BRM_006)

--- Tests/UnitTests/PlayMode/CardSets/BrmCardsGenTests.cpp
+++ Tests/UnitTests/PlayMode/CardSets/BrmCardsGenTests.cpp
@@ -899,3 +899,50 @@ TEST_CASE("[Warlock : Spell] - BRM_005 : Demonwrath")
     CHECK_EQ(opField[1]->GetHealth(), 1);
     CHECK_EQ(opField[2]->GetHealth(), 2);
 }
+
+// --------------------------------------- MINION - WARLOCK
+// [BRM_006] Imp Gang Boss - COST:3 [ATK:2/HP:4]
+// - Race: Demon, Set: CORE, Rarity: Common
+// --------------------------------------------------------
+// Text: Whenever this minion takes damage, summon a 1/1 Imp.
+// --------------------------------------------------------
+// GameTag:
+// - TRIGGER_VISUAL = 1
+// --------------------------------------------------------
+TEST_CASE("[Warlock : Minion] - BRM_006 : Imp Gang Boss")
+{
+    GameConfig config;
+    config.player1Class = CardClass::WARLOCK;
+    config.player2Class = CardClass::MAGE;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = false;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curField = *(curPlayer->GetFieldZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Imp Gang Boss"));
+
+    game.Process(curPlayer, PlayCardTask::Minion(card1));
+    CHECK_EQ(curField.GetCount(), 1);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(opPlayer, HeroPowerTask(card1));
+    CHECK_EQ(curField.GetCount(), 2);
+    CHECK_EQ(curField[1]->card->name, "Imp");
+    CHECK_EQ(curField[1]->GetAttack(), 1);
+    CHECK_EQ(curField[1]->GetHealth(), 1);
+}
