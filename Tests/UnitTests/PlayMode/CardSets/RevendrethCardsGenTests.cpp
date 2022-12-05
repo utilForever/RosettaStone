1841e90a75c5e49c3ec65d28fa967d7b9ce1187a
Chris Ohk <utilforever@gmail.com>
2022-12-05 21:33:41 +0900

    feat(location): Uncomment unit test code of 'Great Hall' (REV_983)

--- Tests/UnitTests/PlayMode/CardSets/RevendrethCardsGenTests.cpp
+++ Tests/UnitTests/PlayMode/CardSets/RevendrethCardsGenTests.cpp
@@ -628,44 +628,42 @@ TEST_CASE("[Hunter : Minion] - REV_356 : Batty Guest")
 // --------------------------------------------------------
 // Text: Set a minion's Attack and Health to 3.
 // --------------------------------------------------------
-//TEST_CASE("[Paladin : Location] - REV_983 : Great Hall")
-//{
-//    GameConfig config;
-//    config.player1Class = CardClass::PALADIN;
-//    config.player2Class = CardClass::MAGE;
-//    config.startPlayer = PlayerType::PLAYER1;
-//    config.doFillDecks = false;
-//    config.autoRun = false;
-//
-//    Game game(config);
-//    game.Start();
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//
-//    Player* curPlayer = game.GetCurrentPlayer();
-//    Player* opPlayer = game.GetOpponentPlayer();
-//    curPlayer->SetTotalMana(10);
-//    curPlayer->SetUsedMana(0);
-//    opPlayer->SetTotalMana(10);
-//    opPlayer->SetUsedMana(0);
-//
-//    auto& curField = *(curPlayer->GetFieldZone());
-//
-//    const auto card1 =
-//        Generic::DrawCard(curPlayer, Cards::FindCardByName("Great Hall"));
-//    const auto card2 =
-//        Generic::DrawCard(curPlayer, Cards::FindCardByName("Wisp"));
-//    const auto card3 =
-//        Generic::DrawCard(opPlayer, Cards::FindCardByName("Blizzard"));
-//    const auto card4 =
-//        Generic::DrawCard(opPlayer, Cards::FindCardByName("Fireball"));
-//
-//    game.Process(curPlayer, PlayCardTask::Minion(card1));
-//    game.Process(curPlayer, PlayCardTask::Minion(card2));
-//    CHECK_EQ(curField.GetCount(), 2);
-//
-//    game.Process(curPlayer, EndTurnTask());
-//    game.ProcessUntil(Step::MAIN_ACTION);
-//}
+TEST_CASE("[Paladin : Location] - REV_983 : Great Hall")
+{
+    GameConfig config;
+    config.player1Class = CardClass::PALADIN;
+    config.player2Class = CardClass::MAGE;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = false;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curField = *(curPlayer->GetFieldZone());
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Great Hall"));
+    const auto card2 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Wisp"));
+
+    game.Process(curPlayer, PlayCardTask::Minion(card1));
+    game.Process(curPlayer, PlayCardTask::Location(card2));
+    CHECK_EQ(curField.GetCount(), 2);
+    CHECK_EQ(curField.GetMinions().size(), 1u);
+    CHECK_EQ(curField.GetLocations().size(), 1u);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+}
 
 // --------------------------------------- MINION - NEUTRAL
 // [REV_012] Bog Beast - COST:6 [ATK:3/HP:6]
