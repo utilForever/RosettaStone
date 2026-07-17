2ff589e14821197866367161aa271be56af44bad
Chris Ohk <utilforever@gmail.com>
2026-07-17 12:36:48 +0900

    refactor: replace manual priority queue nodes

--- Includes/Rosetta/Common/PriorityQueue.hpp
+++ Includes/Rosetta/Common/PriorityQueue.hpp
@@ -7,8 +7,9 @@
 #ifndef ROSETTASTONE_PRIORITY_QUEUE_HPP
 #define ROSETTASTONE_PRIORITY_QUEUE_HPP
 
-#include <algorithm>
+#include <map>
 #include <stdexcept>
+#include <utility>
 
 namespace RosettaStone
 {
@@ -21,84 +22,18 @@ template <typename T>
 class PriorityQueue
 {
  public:
-    //! Constructor.
-    PriorityQueue() : m_head(new Node())
-    {
-        // Do nothing
-    }
-
-    //! Destructor.
-    ~PriorityQueue()
-    {
-        Clear();
-        delete m_head;
-    }
-
-    //! Copy constructor.
-    PriorityQueue(const PriorityQueue& rhs) : m_count(rhs.m_count)
-    {
-        const Node* rhsNode = rhs.m_head;
-        Node* curNode = nullptr;
-
-        if (rhsNode)
-        {
-            m_head = new Node(rhsNode->value, rhsNode->priority);
-            curNode = m_head;
-
-            rhsNode = rhsNode->next;
-        }
-
-        while (rhsNode)
-        {
-            Node* newNode = new Node(rhsNode->value, rhsNode->priority);
-            curNode->next = newNode;
-
-            curNode = curNode->next;
-            rhsNode = rhsNode->next;
-        }
-    }
-
-    //! Move constructor.
-    PriorityQueue(PriorityQueue&& rhs) noexcept : PriorityQueue()
-    {
-        std::swap(m_head, rhs.m_head);
-        std::swap(m_count, rhs.m_count);
-    }
-
-    //! Copy assignment operator.
-    PriorityQueue& operator=(const PriorityQueue& rhs)
-    {
-        if (this == &rhs)
-        {
-            return *this;
-        }
-
-        PriorityQueue<T> temp(rhs);
-        std::swap(temp.m_head, m_head);
-        std::swap(temp.m_count, m_count);
-        return *this;
-    }
-
-    //! Move assignment operator.
-    PriorityQueue& operator=(PriorityQueue&& rhs) noexcept
-    {
-        if (this == &rhs)
-        {
-            return *this;
-        }
-
-        PriorityQueue<T> temp(static_cast<PriorityQueue<T>&&>(rhs));
-        std::swap(m_head, temp.m_head);
-        std::swap(m_count, temp.m_count);
-
-        return *this;
-    }
+    PriorityQueue() = default;
+    ~PriorityQueue() = default;
+    PriorityQueue(const PriorityQueue&) = default;
+    PriorityQueue(PriorityQueue&&) noexcept = default;
+    PriorityQueue& operator=(const PriorityQueue&) = default;
+    PriorityQueue& operator=(PriorityQueue&&) noexcept = default;
 
     //! Gets the count of the elements.
     //! \return The count of the elements.
     std::size_t GetCount() const
     {
-        return m_count;
+        return m_elements.size();
     }
 
     //! Inserts element and sorts the underlying container.
@@ -106,26 +41,7 @@ class PriorityQueue
     //! \param priority The priority of the element to push.
     void Push(T value, int priority)
     {
-        Node* node = m_head;
-
-        while (node->next)
-        {
-            if (node->next->priority <= priority)
-            {
-                node = node->next;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        Node* temp = node->next;
-        Node* newNode = new Node(value, priority);
-        node->next = newNode;
-        newNode->next = temp;
-
-        m_count++;
+        m_elements.emplace(priority, std::move(value));
     }
 
     //! Removes the top element from the priority queue.
@@ -137,31 +53,21 @@ class PriorityQueue
             throw std::out_of_range("Cannot pop from an empty priority queue");
         }
 
-        Node* node = m_head->next;
-        m_head->next = node->next;
-        m_count--;
-
-        T value = node->value;
-        delete node;
-
-        return value;
+        auto node = m_elements.extract(m_elements.begin());
+        return std::move(node.mapped());
     }
 
     //! Checks if the value of the element exists.
     //! \param value The value of the element to check.
     //! \return true if the the value of the element exists, false otherwise.
-    bool IsExist(T value)
+    bool IsExist(const T& value) const
     {
-        Node* node = m_head->next;
-
-        while (node)
+        for (const auto& [priority, element] : m_elements)
         {
-            if (node->value == value)
+            if (element == value)
             {
                 return true;
             }
-
-            node = node->next;
         }
 
         return false;
@@ -171,62 +77,11 @@ class PriorityQueue
     //! \return true if the underlying container is empty, false otherwise.
     bool IsEmpty() const
     {
-        return m_count == 0;
+        return m_elements.empty();
     }
 
  private:
-    //!
-    //! \brief Node struct.
-    //!
-    //! This struct stores the value and the priority of the element.
-    //! Also, it contains a pointer that points to the next element.
-    //!
-    struct Node
-    {
-        //! Default constructor.
-        Node() = default;
-
-        //! Constructs node with given \p _value and \p _priority.
-        //! \param _value The value of the element.
-        //! \param _priority The priority of the element.
-        Node(T _value, int _priority) : value(_value), priority(_priority)
-        {
-            // Do nothing
-        }
-
-        T value = T();
-        int priority = 0;
-
-        Node* next = nullptr;
-    };
-
-    //!
-    //! \brief Clears the priority queue.
-    //!
-    //! This function deletes all the nodes in the priority queue and resets the
-    //! count to 0.
-    //!
-    void Clear()
-    {
-        if (!m_head)
-        {
-            return;
-        }
-
-        Node* node = m_head->next;
-        while (node)
-        {
-            Node* next = node->next;
-            delete node;
-            node = next;
-        }
-
-        m_head->next = nullptr;
-        m_count = 0;
-    }
-
-    Node* m_head = nullptr;
-    std::size_t m_count = 0;
+    std::multimap<int, T> m_elements;
 };
 }  // namespace RosettaStone
 
