8bdae4c6e4580fa2a3758823c749abb164a7dfd5
Chris Ohk <utilforever@gmail.com>
2026-07-30 12:03:57 +0900

    feat(impl-card): implement card 'Great Hall' (REV_983)

--- Tests/UnitTests/PlayMode/CardSets/RevendrethCardsGenTests.cpp
+++ Tests/UnitTests/PlayMode/CardSets/RevendrethCardsGenTests.cpp
@@ -622,6 +622,94 @@ TEST_CASE("[Hunter : Minion] - REV_356 : Batty Guest")
     CHECK_EQ(curField[0]->GetHealth(), 1);
 }
 
+// ------------------------------------- LOCATION - PALADIN
+// [REV_983] Great Hall - COST:2
+// - Set: REVENDRETH, Rarity: Rare
+// --------------------------------------------------------
+// Text: Set a minion's Attack and Health to 3.
+// --------------------------------------------------------
+TEST_CASE("[Paladin : Location] - REV_983 : Great Hall")
+{
+    GameConfig config;
+    config.player1Class = CardClass::PALADIN;
+    config.player2Class = CardClass::MAGE;
+    config.startPlayer = PlayerType::PLAYER1;
+    config.doFillDecks = false;
+    config.autoRun = false;
+
+    Game game(config);
+    game.Start();
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    Player* curPlayer = game.GetCurrentPlayer();
+    Player* opPlayer = game.GetOpponentPlayer();
+    curPlayer->SetTotalMana(10);
+    curPlayer->SetUsedMana(0);
+    opPlayer->SetTotalMana(10);
+    opPlayer->SetUsedMana(0);
+
+    auto& curField = *curPlayer->GetFieldZone();
+
+    const auto card1 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Great Hall"));
+    const auto card2 =
+        Generic::DrawCard(curPlayer, Cards::FindCardByName("Wisp"));
+
+    game.Process(curPlayer, PlayCardTask::Location(card1));
+    game.Process(curPlayer, PlayCardTask::Minion(card2));
+
+    const auto location = dynamic_cast<Location*>(card1);
+    const auto minion = dynamic_cast<Minion*>(card2);
+
+    REQUIRE(location);
+    REQUIRE(minion);
+    CHECK_EQ(curField.GetCount(), 2);
+    CHECK_EQ(curField.GetAll().size(), 2u);
+    CHECK_EQ(curField.GetMinionCount(), 1);
+    CHECK_EQ(curField.GetLocations().size(), 1u);
+    CHECK_EQ(location->GetHealth(), 3);
+    CHECK(location->IsOnCooldown());
+
+    game.Process(curPlayer, PlayLocationTask(location, minion));
+    CHECK_EQ(minion->GetAttack(), 1);
+    CHECK_EQ(location->GetHealth(), 3);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, PlayLocationTask(location, minion));
+    CHECK_EQ(minion->GetAttack(), 3);
+    CHECK_EQ(minion->GetHealth(), 3);
+    CHECK_EQ(location->GetHealth(), 2);
+    CHECK(location->IsOnCooldown());
+
+    game.Process(curPlayer, PlayLocationTask(location, minion));
+    CHECK_EQ(location->GetHealth(), 2);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, PlayLocationTask(location, minion));
+    CHECK_EQ(location->GetHealth(), 1);
+
+    game.Process(curPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(opPlayer, EndTurnTask());
+    game.ProcessUntil(Step::MAIN_ACTION);
+
+    game.Process(curPlayer, PlayLocationTask(location, minion));
+    CHECK(curField.GetLocations().empty());
+    CHECK_EQ(curField.GetCount(), 1);
+    CHECK_EQ(location->GetZoneType(), ZoneType::GRAVEYARD);
+}
+
 // --------------------------------------- MINION - NEUTRAL
 // [REV_012] Bog Beast - COST:6 [ATK:3/HP:6]
 // - Set: REVENDRETH, Rarity: Common
